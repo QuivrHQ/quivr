a22bcf31baebf483f6f1eb74fe18baaa0151f8a1
Stan Girard <girard.stanislas@gmail.com>
2023-05-14 21:12:30 +0200

    feat(website): first iteration

---
+++ website/app/page.js
@@ -0,0 +1,51 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Welcome to Quivr, your second brain with GenerativeAI 🧠
+        </p>
+      </div>
+      <div className={styles.center}>
+        <iframe 
+          src="https://github.com/StanGirard/quiver/assets/19614572/a3cddc6a-ca28-44ad-9ede-3122fa918b51" 
+          width="1000" 
+          height="480" 
+          frameborder="0" 
+          allow="autoplay; fullscreen; picture-in-picture" 
+          allowfullscreen
+        ></iframe>
+      </div>
+      <div className={styles.grid}>
+  <a
+    href="https://github.com/StanGirard"
+    className={styles.card}
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <h2>
+      GitHub <span>-&gt;</span>
+    </h2>
+    <p>Check out the source code on GitHub.</p>
+  </a>
+
+  <a
+    href="https://twitter.com/_StanGirard"
+    className={styles.card}
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <h2>
+      Twitter <span>-&gt;</span>
+    </h2>
+    <p>Follow us on Twitter for updates.</p>
+  </a>
+
+
+</div>
+    </main>
+  )
+}
