a0a97d347d14ea59c083e6b7455fa6ef1e721af6
Stan Girard <girard.stanislas@gmail.com>
2023-05-16 12:02:10 +0200

    feat(website): added analytics

--- website/app/layout.js
+++ website/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   )
 }
