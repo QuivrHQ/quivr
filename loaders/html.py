818989827e34f5211bec55f463373200af62d0e2
Stan Girard <girard.stanislas@gmail.com>
2023-05-16 09:56:12 +0200

    feat(url): added html loader instead of text

--- loaders/html.py
+++ loaders/html.py
@@ -1,5 +1,5 @@
 from .common import process_file
-from langchain.document_loaders import TextLoader
+from langchain.document_loaders import UnstructuredHTMLLoader
 
 def process_html(vector_store, file):
-    return process_file(vector_store, file, TextLoader, ".html")
+    return process_file(vector_store, file, UnstructuredHTMLLoader, ".html")
