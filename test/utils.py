21dd3af2e817b561e80d827b92285eb19abf7347
Stan Girard <girard.stanislas@gmail.com>
2023-05-18 01:22:13 +0200

    feat(api): new api init

---
+++ test/utils.py
@@ -0,0 +1,11 @@
+import hashlib
+
+def compute_sha1_from_file(file_path):
+    with open(file_path, "rb") as file:
+        bytes = file.read() 
+        readable_hash = hashlib.sha1(bytes).hexdigest()
+    return readable_hash
+
+def compute_sha1_from_content(content):
+    readable_hash = hashlib.sha1(content).hexdigest()
+    return readable_hash
