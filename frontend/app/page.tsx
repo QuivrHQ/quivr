58630f7207e13f891bd768e92668e7d37cf15f0a
Stan Girard <girard.stanislas@gmail.com>
2023-05-18 01:22:13 +0200

    feat(api): new api init

---
+++ frontend/app/page.tsx
@@ -0,0 +1,24 @@
+'use client';
+import Link from 'next/link';
+
+export default function HomePage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="m-4 p-6 max-w-md mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        <h1 className="mb-4 text-xl font-bold text-gray-900">Welcome!</h1>
+        <div className="flex flex-col space-y-4">
+          <Link href="/chat">
+            <a className="px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
+              Go to Chat
+            </a>
+          </Link>
+          <Link href="/upload">
+            <a className="px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
+              Go to Upload
+            </a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/app/upload/page.tsx
@@ -0,0 +1,49 @@
+'use client';
+import { useCallback, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import axios from 'axios';
+
+export default function UploadPage() {
+  const [message, setMessage] = useState(null);
+
+  const onDrop = useCallback(async (acceptedFiles) => {
+    const file = acceptedFiles[0];
+    const formData = new FormData();
+    formData.append('file', file);
+    try {
+      const response = await axios.post('http://localhost:8000/upload', formData);
+      setMessage({
+        type: 'success',
+        text: 'File uploaded successfully: ' + JSON.stringify(response.data)
+      });
+    } catch (error) {
+      setMessage({
+        type: 'error',
+        text: 'Failed to upload file: ' + error.toString()
+      });
+    }
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div 
+      {...getRootProps()} 
+      className="flex flex-col items-center justify-center h-screen bg-gray-100"
+    >
+      <input {...getInputProps()} />
+      <div className="mt-2 p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        {
+          isDragActive
+            ? <p className="text-blue-600">Drop the files here...</p>
+            : <p className="text-gray-500">Drag 'n' drop some files here, or click to select files</p>
+        }
+      </div>
+      {message && (
+        <div className={`mt-4 p-2 rounded ${message.type === 'success' ? 'bg-green-500' : 'bg-red-500'}`}>
+          <p className="text-white">{message.text}</p>
+        </div>
+      )}
+    </div>
+  );
+}
