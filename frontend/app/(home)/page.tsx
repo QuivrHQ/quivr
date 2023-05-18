f042ff15f098df56b4219a011aa0dd4ca229f777
Stan Girard <girard.stanislas@gmail.com>
2023-05-18 13:49:53 +0200

    feat(vercel): added analytics

--- frontend/app/(home)/page.tsx
+++ frontend/app/(home)/page.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import Hero from "./Hero";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function HomePage() {
   return (
     <main className="">
       <Hero />
+      <Analytics />
     </main>
   );
 }
