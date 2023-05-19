087771d77c43f8c4fd200fc1bdfead2d63c576db
rafalohaki <rafalohaki@gmail.com>
2023-05-19 17:37:07 +0200

    explore
    show explored content

--- explorer.py
+++ explorer.py
@@ -1,12 +1,21 @@
 import streamlit as st
 
+def show_document(document):
+    st.write("**Document Content**")
+    st.write(document)
 
 def view_document(supabase):
     # Get the document from the database
     response = supabase.table("documents").select("content").execute()
     st.write("**This feature is in active development**")
+    st.write("**Document List**")
+
     # Display a list of elements from the documents
     # If the user clicks on an element, display the content of the document
-    for document in response.data:
-        if st.button(document['content'][:50].replace("\n", " ")):
-            continue
+    selected_document = None
+    for index, document in enumerate(response.data):
+        if st.button(document['content'][:50].replace("\n", " "), key=f"document_button_{index}"):
+            selected_document = document['content']
+    
+    if selected_document:
+        show_document(selected_document)
