3d42894d3641db447cfc34eb6448d8977769ba61
dheerapat <61280196+dheerapat@users.noreply.github.com>
2023-05-20 21:19:00 +0700

    tiangolo domain is not necessary (#72)

--- v2/api.py
+++ v2/api.py
@@ -17,8 +17,6 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
     "http://localhost",
     "http://localhost:3000",
     "http://localhost:8080",
