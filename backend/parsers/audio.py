c38265a5f587db4de2200a1584ae35bd84bd622c
shaun <shaunweix@gmail.com>
2023-05-21 23:39:55 -0700

    add summarization backend

--- backend/parsers/audio.py
+++ backend/parsers/audio.py
@@ -7,7 +7,7 @@ import openai
 from langchain.document_loaders import TextLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from utils import compute_sha1_from_content
+from utils import compute_sha1_from_content, documents_vector_store
 from langchain.schema import Document
 from fastapi import UploadFile
 
@@ -15,27 +15,27 @@ from fastapi import UploadFile
 # def _transcribe_audio(api_key, audio_file, stats_db):
 #     openai.api_key = api_key
 #     transcript = ""
-    
+
 #     with BytesIO(audio_file.read()) as audio_bytes:
 #         # Get the extension of the uploaded file
 #         file_extension = os.path.splitext(audio_file.name)[-1]
-        
+
 #         # Create a temporary file with the uploaded audio data and the correct extension
 #         with tempfile.NamedTemporaryFile(delete=True, suffix=file_extension) as temp_audio_file:
 #             temp_audio_file.write(audio_bytes.read())
 #             temp_audio_file.seek(0)  # Move the file pointer to the beginning of the file
-            
+
 #             transcript = openai.Audio.translate("whisper-1", temp_audio_file)
 
 #     return transcript
 
-async def process_audio(vector_store, upload_file: UploadFile, stats_db):
-   
+
+async def process_audio(upload_file: UploadFile, stats_db):
+
     file_sha = ""
     dateshort = time.strftime("%Y%m%d-%H%M%S")
     file_meta_name = f"audiotranscript_{dateshort}.txt"
-    ### uploaded file to file object
-
+    # uploaded file to file object
 
     openai_api_key = os.environ.get("OPENAI_API_KEY")
 
@@ -46,31 +46,29 @@ async def process_audio(vector_store, upload_file: UploadFile, stats_db):
         tmp_file.write(content)
         tmp_file.flush()
         tmp_file.close()
-       
 
         with open(tmp_file.name, "rb") as audio_file:
             transcript = openai.Audio.transcribe("whisper-1", audio_file)
 
-        
-    
     file_sha = compute_sha1_from_content(transcript.text.encode("utf-8"))
     file_size = len(transcript.text.encode("utf-8"))
     print(file_size)
 
-    ## Load chunk size and overlap from sidebar
+    # Load chunk size and overlap from sidebar
     chunk_size = 500
     chunk_overlap = 0
 
-    text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     print(transcript)
     print("#########")
     texts = text_splitter.split_text(transcript)
 
-    docs_with_metadata = [Document(page_content=text, metadata={"file_sha1": file_sha,"file_size": file_size, "file_name": file_meta_name, "chunk_size": chunk_size, "chunk_overlap": chunk_overlap, "date": dateshort}) for text in texts]
+    docs_with_metadata = [Document(page_content=text, metadata={"file_sha1": file_sha, "file_size": file_size, "file_name": file_meta_name,
+                                   "chunk_size": chunk_size, "chunk_overlap": chunk_overlap, "date": dateshort}) for text in texts]
 
     # if st.secrets.self_hosted == "false":
     #     add_usage(stats_db, "embedding", "audio", metadata={"file_name": file_meta_name,"file_type": ".txt", "chunk_size": chunk_size, "chunk_overlap": chunk_overlap})
-    vector_store.add_documents(docs_with_metadata)
+    documents_vector_store.add_documents(docs_with_metadata)
 
-    return vector_store
-
+    return documents_vector_store
--- backend/parsers/txt.py
+++ backend/parsers/txt.py
@@ -2,5 +2,6 @@ from .common import process_file
 from langchain.document_loaders import TextLoader
 from fastapi import UploadFile
 
-async def process_txt(vector_store, file: UploadFile, stats_db):
-    return await process_file(vector_store, file, TextLoader,".txt", stats_db=stats_db)
+
+async def process_txt(file: UploadFile, enable_summarization):
+    return await process_file(file, TextLoader, ".txt", enable_summarization)
