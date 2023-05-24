0bed861c1f91479c36134fb9f1ea348389ecee83
Stan Girard <girard.stanislas@gmail.com>
2023-05-24 13:07:04 +0200

    feat(supabase): added auth

---
+++ frontend/app/supabase-provider.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { createContext, useContext, useEffect, useState } from 'react'
+import { Session, createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { useRouter } from 'next/navigation'
+
+import type { SupabaseClient } from '@supabase/auth-helpers-nextjs'
+import type { Database } from '@/lib/database.types'
+
+type MaybeSession = Session | null
+
+type SupabaseContext = {
+  supabase: SupabaseClient<Database>
+  session: MaybeSession
+}
+
+const Context = createContext<SupabaseContext | undefined>(undefined)
+
+export default function SupabaseProvider({
+  children,
+  session,
+}: {
+  children: React.ReactNode
+  session: MaybeSession
+}) {
+  const [supabase] = useState(() => createBrowserSupabaseClient())
+  const router = useRouter()
+
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange(() => {
+      router.refresh()
+    })
+
+    return () => {
+      subscription.unsubscribe()
+    }
+  }, [router, supabase])
+
+  return (
+    <Context.Provider value={{ supabase, session }}>
+      <>{children}</>
+    </Context.Provider>
+  )
+}
+
+export const useSupabase = () => {
+  const context = useContext(Context)
+
+  if (context === undefined) {
+    throw new Error('useSupabase must be used inside SupabaseProvider')
+  }
+
+  return context
+}
