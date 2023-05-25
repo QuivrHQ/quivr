f171f5eb45342a142a5771e6fcda432b4b3c0fbd
iMADi-ARCH <nandanaditya985@gmail.com>
2023-05-25 13:21:24 +0530

    fix: field dark mode

--- frontend/app/components/ui/Field.tsx
+++ frontend/app/components/ui/Field.tsx
@@ -12,14 +12,14 @@ interface FieldProps
 
 const Field: FC<FieldProps> = ({ label, className, name, id, ...props }) => {
   return (
-    <fieldset className={cn("flex flex-col", className)} name={name}>
+    <fieldset className={cn("flex flex-col w-full", className)} name={name}>
       {label && (
         <label htmlFor={name} className="text-sm">
           {label}
         </label>
       )}
       <input
-        className="px-4 py-2 border rounded-md border-black/10 dark:border-white/25"
+        className="w-full bg-gray-50 dark:bg-gray-900 px-4 py-2 border rounded-md border-black/10 dark:border-white/25"
         name={name}
         id={name}
         {...props}
