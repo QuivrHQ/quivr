a892bf4daf55dc1131ad1c0884fe3910add7176b
iMADi-ARCH <nandanaditya985@gmail.com>
2023-05-25 15:32:24 +0530

    fix: forward ref

--- frontend/app/components/ui/Field.tsx
+++ frontend/app/components/ui/Field.tsx
@@ -1,5 +1,11 @@
 import { cn } from "@/lib/utils";
-import { DetailedHTMLProps, FC, InputHTMLAttributes } from "react";
+import {
+  DetailedHTMLProps,
+  FC,
+  InputHTMLAttributes,
+  RefObject,
+  forwardRef,
+} from "react";
 
 interface FieldProps
   extends DetailedHTMLProps<
@@ -10,22 +16,25 @@ interface FieldProps
   name: string;
 }
 
-const Field: FC<FieldProps> = ({ label, className, name, id, ...props }) => {
-  return (
-    <fieldset className={cn("flex flex-col w-full", className)} name={name}>
-      {label && (
-        <label htmlFor={name} className="text-sm">
-          {label}
-        </label>
-      )}
-      <input
-        className="w-full bg-gray-50 dark:bg-gray-900 px-4 py-2 border rounded-md border-black/10 dark:border-white/25"
-        name={name}
-        id={name}
-        {...props}
-      />
-    </fieldset>
-  );
-};
+const Field = forwardRef(
+  ({ label, className, name, id, ...props }: FieldProps, forwardedRef) => {
+    return (
+      <fieldset className={cn("flex flex-col w-full", className)} name={name}>
+        {label && (
+          <label htmlFor={name} className="text-sm">
+            {label}
+          </label>
+        )}
+        <input
+          ref={forwardedRef as RefObject<HTMLInputElement>}
+          className="w-full bg-gray-50 dark:bg-gray-900 px-4 py-2 border rounded-md border-black/10 dark:border-white/25"
+          name={name}
+          id={name}
+          {...props}
+        />
+      </fieldset>
+    );
+  }
+);
 
 export default Field;
