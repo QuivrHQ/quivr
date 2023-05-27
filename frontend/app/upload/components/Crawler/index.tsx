f634344f32ddbc753999cced1b125b43af98a85a
iMADi-ARCH <nandanaditya985@gmail.com>
2023-05-27 19:15:21 +0530

    feat(upload): responsive + consistent spacing

--- frontend/app/upload/components/Crawler/index.tsx
+++ frontend/app/upload/components/Crawler/index.tsx
@@ -8,11 +8,11 @@ export const Crawler = (): JSX.Element => {
   const { urlInputRef, isCrawling, crawlWebsite } = useCrawler();
   return (
     <div className="w-full">
-      <div className="flex justify-center gap-5">
-        <div className="w-1/2">
-          <div className="flex-column justify-center gap-5">
-            <Card className="h-32 flex justify-center items-center">
-              <div className="text-center mt-2 p-6 max-w-sm w-full flex flex-col gap-5 items-center">
+      <div className="flex justify-center gap-5 px-6">
+        <div className="max-w-xl w-full">
+          <div className="flex-col justify-center gap-5">
+            <Card className="h-32 flex gap-5 justify-center items-center px-5">
+              <div className="text-center max-w-sm w-full flex flex-col gap-5 items-center">
                 <Field
                   name="crawlurl"
                   ref={urlInputRef}
