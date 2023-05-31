99e239679aa84cdfcec450dd8a8d556d22ba0b7e
Mamadou DICKO <mamadoudicko100@gmail.com>
2023-05-31 13:18:03 +0200

    feat(axios): add backendUrl overwrite

--- frontend/lib/useAxios.ts
+++ frontend/lib/useAxios.ts
@@ -1,16 +1,20 @@
 import { useSupabase } from "@/app/supabase-provider";
 import axios from "axios";
+import { useBrainConfig } from "./context/BrainConfigProvider/hooks/useBrainConfig";
+
+const axiosInstance = axios.create({
+  baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}`,
+});
+
 export const useAxios = () => {
   const { session } = useSupabase();
-
-  const axiosInstance = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}`,
-  });
-
+  const {
+    config: { backendUrl },
+  } = useBrainConfig();
   axiosInstance.interceptors.request.use(
     async (config) => {
       config.headers["Authorization"] = "Bearer " + session?.access_token;
-
+      config.baseURL = backendUrl ?? config.baseURL;
       return config;
     },
     (error) => {
