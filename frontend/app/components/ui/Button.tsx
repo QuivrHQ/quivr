4ab1f5c21f0e65ff6fc35490d6d2683bab0a8cfb
Aditya Nandan <61308761+iMADi-ARCH@users.noreply.github.com>
2023-06-02 19:31:24 +0530

    fix(chat): only allow speech recognition if supported (#229)

--- frontend/app/components/ui/Button.tsx
+++ frontend/app/components/ui/Button.tsx
@@ -10,7 +10,8 @@ const ButtonVariants = cva(
       variant: {
         primary:
           "bg-black border border-black dark:border-white disabled:bg-gray-500 disabled:hover:bg-gray-500 text-white dark:bg-white dark:text-black hover:bg-gray-700 dark:hover:bg-gray-200 transition-colors",
-        tertiary: "text-black dark:text-white bg-transparent py-2 px-4",
+        tertiary:
+          "text-black dark:text-white bg-transparent py-2 px-4 disabled:opacity-25",
         secondary:
           "border border-black dark:border-white bg-white dark:bg-black text-black dark:text-white focus:bg-black dark:focus:bg-white hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black focus:text-white dark:focus:text-black transition-colors py-2 px-4 shadow-none",
         danger:
