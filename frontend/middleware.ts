bd673999cc2a4bcccb327307b7292cbe456fa2ed
0xflo <floriankluge@gmail.com>
2023-06-04 12:05:51 +0100

    feat(middleware): Handle 'Invalid Refresh Token: Already Used' Error

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,12 +1,26 @@
 import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { NextResponse } from 'next/server'
-
 import type { NextRequest } from 'next/server'
-// import type { Database } from '@/lib/database.types'
+import { NextResponse } from 'next/server'
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
   const supabase = createMiddlewareSupabaseClient({ req, res })
-  await supabase.auth.getSession()
+
+  try {
+    await supabase.auth.getSession()
+  } catch (error) {
+    if (error instanceof Error && error.message === 'Invalid Refresh Token: Already Used') {
+      // Attempt to sign the user out
+      const { error: signOutError } = await supabase.auth.signOut()
+
+      if (signOutError) {
+        console.error("Error logging out:", signOutError.message)
+      } else {
+        console.log("Logged out successfully")
+        return NextResponse.redirect('/')
+      }
+    }
+  }
+
   return res
 }
