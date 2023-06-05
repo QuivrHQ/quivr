963fb05682d13ec95076ca2e33ff6af473b5aead
Stan Girard <girard.stanislas@gmail.com>
2023-06-06 00:38:15 +0200

    feat(github): now github loader (#264)

--- backend/main.py
+++ backend/main.py
@@ -13,6 +13,7 @@ from logger import get_logger
 from middlewares.cors import add_cors_middleware
 from models.chats import ChatMessage
 from models.users import User
+from parsers.github import process_github
 from utils.file import convert_bytes, get_file_size
 from utils.processors import filter_file
 from utils.vectors import (CommonsDep, create_user, similarity_search,
@@ -114,18 +115,42 @@ async def chat_endpoint(commons: CommonsDep, chat_message: ChatMessage, credenti
 
 @app.post("/crawl/", dependencies=[Depends(JWTBearer())])
 async def crawl_endpoint(commons: CommonsDep, crawl_website: CrawlWebsite, enable_summarization: bool = False, credentials: dict = Depends(JWTBearer())):
+    max_brain_size = os.getenv("MAX_BRAIN_SIZE")
+   
     user = User(email=credentials.get('email', 'none'))
-    file_path, file_name = crawl_website.process()
+    user_vectors_response = commons['supabase'].table("vectors").select(
+        "name:metadata->>file_name, size:metadata->>file_size", count="exact") \
+            .filter("user_id", "eq", user.email)\
+            .execute()
+    documents = user_vectors_response.data  # Access the data from the response
+    # Convert each dictionary to a tuple of items, then to a set to remove duplicates, and then back to a dictionary
+    user_unique_vectors = [dict(t) for t in set(tuple(d.items()) for d in documents)]
 
-    # Create a SpooledTemporaryFile from the file_path
-    spooled_file = SpooledTemporaryFile()
-    with open(file_path, 'rb') as f:
-        shutil.copyfileobj(f, spooled_file)
+    current_brain_size = sum(float(doc['size']) for doc in user_unique_vectors)
 
-    # Pass the SpooledTemporaryFile to UploadFile
-    file = UploadFile(file=spooled_file, filename=file_name)
-    message = await filter_file(file, enable_summarization, commons['supabase'], user=user)
-    return message
+    file_size = 1000000
+
+    remaining_free_space =  float(max_brain_size) - (current_brain_size)
+
+    if remaining_free_space - file_size < 0:
+        message = {"message": f"❌ User's brain will exceed maximum capacity with this upload. Maximum file allowed is : {convert_bytes(remaining_free_space)}", "type": "error"}
+    else: 
+        user = User(email=credentials.get('email', 'none'))
+        if not crawl_website.checkGithub():
+
+            file_path, file_name = crawl_website.process()
+
+            # Create a SpooledTemporaryFile from the file_path
+            spooled_file = SpooledTemporaryFile()
+            with open(file_path, 'rb') as f:
+                shutil.copyfileobj(f, spooled_file)
+
+            # Pass the SpooledTemporaryFile to UploadFile
+            file = UploadFile(file=spooled_file, filename=file_name)
+            message = await filter_file(file, enable_summarization, commons['supabase'], user=user)
+            return message
+        else:
+            message = await process_github(crawl_website.url, "false", user=user, supabase=commons['supabase'])
 
 
 @app.get("/explore", dependencies=[Depends(JWTBearer())])
