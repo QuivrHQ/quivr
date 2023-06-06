c8cdd910e748ce24031745ee3c85ca42d906c5d7
Stan Girard <girard.stanislas@gmail.com>
2023-06-06 11:18:33 +0200

    feat(pdf): added new pdf miner that works

--- backend/parsers/pdf.py
+++ backend/parsers/pdf.py
@@ -1,8 +1,8 @@
 from fastapi import UploadFile
-from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import PyMuPDFLoader
 
 from .common import process_file
 
 
 def process_pdf(file: UploadFile, enable_summarization, user):
-    return process_file(file, PyPDFLoader, ".pdf", enable_summarization, user)
+    return process_file(file, PyMuPDFLoader, ".pdf", enable_summarization, user)
