a7078f7557be2b3f15a8dba0ec653c508a1df4ab
gozineb <zinebe@theodo.fr>
2023-06-08 17:49:05 +0200

    ✨ add chatId to useQuestion

--- frontend/app/chat/hooks/useQuestion.ts
+++ frontend/app/chat/hooks/useQuestion.ts
@@ -1,37 +1,44 @@
-import { useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 
 import { useSupabase } from "@/app/supabase-provider";
 import { useBrainConfig } from "@/lib/context/BrainConfigProvider/hooks/useBrainConfig";
 import { useAxios } from "@/lib/useAxios";
+import { UUID } from "crypto";
 import { redirect } from "next/navigation";
-export const useQuestion = () => {
+import { Chat } from "../types";
+
+interface QuestionParams {
+  chatId: string | undefined;
+  history?: Array<[string, string]> | undefined;
+  setChats: Dispatch<SetStateAction<Chat[]>>;
+}
+
+export const useQuestion = (params: QuestionParams) => {
   const [question, setQuestion] = useState("");
-  const [history, setHistory] = useState<Array<[string, string]>>([]);
+  const [chatId, setChatId] = useState(params?.chatId);
+  const [history, setHistory] = useState<Array<[string, string]>>(
+    params?.history ?? []
+  );
   const [isPending, setIsPending] = useState(false);
   const { session } = useSupabase();
   const { axiosInstance } = useAxios();
   const {
     config: { maxTokens, model, temperature },
   } = useBrainConfig();
+
   if (session === null) {
     redirect("/login");
   }
 
-  useEffect(()=>{
-    // Check if history exists in local storage. If it does, fetch it and set it as history
-    (async ()=>{
-      const localHistory = await localStorage.getItem('history')
-      if(localHistory){
-        setHistory(JSON.parse(localHistory))
-      }
-    })()
-  }, [])
-  
   const askQuestion = async () => {
     setHistory((hist) => [...hist, ["user", question]]);
     setIsPending(true);
 
-    const response = await axiosInstance.post(`/chat/`, {
+    const response = await axiosInstance.post<{
+      chatId: UUID;
+      history: Array<[string, string]>;
+    }>(`/chat`, {
+      ...(chatId && { chat_id: chatId }),
       model,
       question,
       history,
@@ -39,15 +46,24 @@ export const useQuestion = () => {
       max_tokens: maxTokens,
     });
     setHistory(response.data.history);
-    localStorage.setItem('history', JSON.stringify(response.data.history))
+    localStorage.setItem("history", JSON.stringify(response.data.history));
     setQuestion("");
     setIsPending(false);
+
+    if (chatId === undefined) {
+      params.setChats((chats) => [
+        ...chats,
+        { chatId: response.data.chatId, history },
+      ]);
+    }
+
+    setChatId(response.data.chatId);
   };
 
   const resetHistory = () => {
-    localStorage.setItem('history', JSON.stringify([]))
-    setHistory([])
-  }
+    localStorage.setItem("history", JSON.stringify([]));
+    setHistory([]);
+  };
 
   return {
     isPending,
@@ -56,5 +72,7 @@ export const useQuestion = () => {
     setQuestion,
     resetHistory,
     askQuestion,
+    setChatId,
+    setHistory,
   };
 };
