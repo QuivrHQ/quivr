80dcef48e3e730f0c73b9a6c6322c177e36e0e92
gozineb <zinebe@theodo.fr>
2023-06-08 17:48:05 +0200

    ♻️ extract components from chat page

---
+++ frontend/app/chat/components/NewChatButton.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import { BsPlusSquare } from "react-icons/bs";
+
+export const NewChatButton = () => (
+  <Link href="/chat">
+    <button
+      type="button"
+      className="w-full block mb-4 pl-4 pr-4 py-2 bg-gray-400 text-white rounded-lg flex items-center justify-center"
+    >
+      <BsPlusSquare className="h-6 w-6 mr-2" /> New Chat
+    </button>
+  </Link>
+);
