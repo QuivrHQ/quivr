dfe887ea8eded97b83d1e5db8148a331ddc0fc02
iMADi-ARCH <nandanaditya985@gmail.com>
2023-06-11 12:24:28 +0530

    fix(chat): screen height chatlist

--- frontend/app/chat/components/ChatsList/index.tsx
+++ frontend/app/chat/components/ChatsList/index.tsx
@@ -6,7 +6,7 @@ export function ChatsList() {
   const { allChats, deleteChat } = useChats();
   return (
     <div className="sticky top-0 max-h-screen overflow-auto scrollbar">
-      <aside className="relative bg-white dark:bg-black max-w-xs w-full border-r border-black/10 dark:border-white/25 ">
+      <aside className="relative bg-white dark:bg-black max-w-xs w-full border-r border-black/10 dark:border-white/25 h-screen">
         <NewChatButton />
         <div className="flex flex-col gap-0">
           {allChats.map((chat) => (
