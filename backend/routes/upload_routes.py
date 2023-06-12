917f017e6ab8d50c40c09e9b0800dea7e92ff9c7
Matt <77928207+mattzcarey@users.noreply.github.com>
2023-06-12 12:30:50 +0100

    refactor: upload_routes

--- backend/routes/upload_routes.py
+++ backend/routes/upload_routes.py
@@ -9,13 +9,7 @@ from utils.vectors import CommonsDep
 
 upload_router = APIRouter()
 
-@upload_router.post("/upload", dependencies=[Depends(JWTBearer())])
-async def upload_file(request: Request,commons: CommonsDep,  file: UploadFile, enable_summarization: bool = False, credentials: dict = Depends(JWTBearer())):
-    max_brain_size = os.getenv("MAX_BRAIN_SIZE")
-    max_brain_size_with_own_key = os.getenv("MAX_BRAIN_SIZE_WITH_KEY",209715200)
-    remaining_free_space = 0
-    
-    user = User(email=credentials.get('email', 'none'))
+def get_user_vectors(commons, user):
     user_vectors_response = commons['supabase'].table("vectors").select(
         "name:metadata->>file_name, size:metadata->>file_size", count="exact") \
             .filter("user_id", "eq", user.email)\
@@ -23,13 +17,23 @@ async def upload_file(request: Request,commons: CommonsDep,  file: UploadFile, e
     documents = user_vectors_response.data  # Access the data from the response
     # Convert each dictionary to a tuple of items, then to a set to remove duplicates, and then back to a dictionary
     user_unique_vectors = [dict(t) for t in set(tuple(d.items()) for d in documents)]
+    return user_unique_vectors
 
+def calculate_remaining_space(request, max_brain_size, max_brain_size_with_own_key, current_brain_size):
+    remaining_free_space = float(max_brain_size_with_own_key) - current_brain_size if request.headers.get('Openai-Api-Key') else float(max_brain_size) - current_brain_size
+    return remaining_free_space
+
+@upload_router.post("/upload", dependencies=[Depends(JWTBearer())])
+async def upload_file(request: Request, commons: CommonsDep,  file: UploadFile, enable_summarization: bool = False, credentials: dict = Depends(JWTBearer())):
+    max_brain_size = os.getenv("MAX_BRAIN_SIZE")
+    max_brain_size_with_own_key = os.getenv("MAX_BRAIN_SIZE_WITH_KEY",209715200)
+    
+    user = User(email=credentials.get('email', 'none'))
+    user_unique_vectors = get_user_vectors(commons, user)
     current_brain_size = sum(float(doc['size']) for doc in user_unique_vectors)
 
-    if request.headers.get('Openai-Api-Key'):
-        remaining_free_space =  float(max_brain_size_with_own_key) - (current_brain_size)
-    else:
-        remaining_free_space = float(max_brain_size) - (current_brain_size)
+    remaining_free_space = calculate_remaining_space(request, max_brain_size, max_brain_size_with_own_key, current_brain_size)
+
     file_size = get_file_size(file)
 
     if remaining_free_space - file_size < 0:
