1fb6eb6822850e7f18e293cde6053e5795f2835b
gozineb <zinebe@theodo.fr>
2023-06-13 13:14:19 +0200

    🧑‍💻  add and run prettier

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,100 +1,101 @@
 /* eslint-disable max-lines */
 module.exports = {
-  plugins: ['prefer-arrow', 'import'],
-  extends: ['next/core-web-vitals','eslint:recommended', 'plugin:import/recommended'],
-  ignorePatterns: [
-    '**/node_modules/',
-    '**/.next/',
+  plugins: ["prefer-arrow", "import"],
+  extends: [
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:import/recommended",
   ],
+  ignorePatterns: ["**/node_modules/", "**/.next/"],
   rules: {
-    'import/extensions': 0,
-    'import/no-unresolved': 0,
-    'import/prefer-default-export': 0,
-    'import/no-duplicates': 'error',
-    complexity: ['error', 8],
-    'max-lines': ['error', 100],
-    'max-depth': ['error', 3],
-    'max-params': ['error', 4],
-    eqeqeq: ['error', 'smart'],
-    'import/no-extraneous-dependencies': [
-      'error',
+    "import/extensions": 0,
+    "import/no-unresolved": 0,
+    "import/prefer-default-export": 0,
+    "import/no-duplicates": "error",
+    complexity: ["error", 8],
+    "max-lines": ["error", 100],
+    "max-depth": ["error", 3],
+    "max-params": ["error", 4],
+    eqeqeq: ["error", "smart"],
+    "import/no-extraneous-dependencies": [
+      "error",
       {
         devDependencies: true,
         optionalDependencies: false,
         peerDependencies: false,
       },
     ],
-    'no-shadow': [
-      'error',
+    "no-shadow": [
+      "error",
       {
-        hoist: 'all',
+        hoist: "all",
       },
     ],
-    'prefer-const': 'error',
-    'import/order': [
-      'error',
+    "prefer-const": "error",
+    "import/order": [
+      "error",
       {
-        pathGroups: [{ pattern: '@lib/**', group: 'unknown' }],
+        pathGroups: [{ pattern: "@lib/**", group: "unknown" }],
         groups: [
-          ['external', 'builtin'],
-          'unknown',
-          'internal',
-          ['parent', 'sibling', 'index'],
+          ["external", "builtin"],
+          "unknown",
+          "internal",
+          ["parent", "sibling", "index"],
         ],
         alphabetize: {
-          order: 'asc',
+          order: "asc",
           caseInsensitive: false,
         },
-        'newlines-between': 'always',
-        pathGroupsExcludedImportTypes: ['builtin'],
+        "newlines-between": "always",
+        pathGroupsExcludedImportTypes: ["builtin"],
       },
     ],
-    'import/namespace': 'off',
-    'sort-imports': [
-      'error',
+    "import/namespace": "off",
+    "sort-imports": [
+      "error",
       {
         ignoreCase: true,
         ignoreDeclarationSort: true,
         ignoreMemberSort: false,
-        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
       },
     ],
-    'padding-line-between-statements': [
-      'error',
+    "padding-line-between-statements": [
+      "error",
       {
-        blankLine: 'always',
-        prev: '*',
-        next: 'return',
+        blankLine: "always",
+        prev: "*",
+        next: "return",
       },
     ],
-    'prefer-arrow/prefer-arrow-functions': [
-      'error',
+    "prefer-arrow/prefer-arrow-functions": [
+      "error",
       {
         disallowPrototype: true,
         singleReturnOnly: false,
         classPropertiesAllowed: false,
       },
     ],
-    'no-restricted-imports': [
-      'error',
+    "no-restricted-imports": [
+      "error",
       {
         paths: [
           {
-            name: 'lodash',
-            message: 'Please use lodash/{module} import instead',
+            name: "lodash",
+            message: "Please use lodash/{module} import instead",
           },
           {
-            name: 'aws-sdk',
-            message: 'Please use aws-sdk/{module} import instead',
+            name: "aws-sdk",
+            message: "Please use aws-sdk/{module} import instead",
           },
           {
-            name: '.',
-            message: 'Please use explicit import file',
+            name: ".",
+            message: "Please use explicit import file",
           },
         ],
       },
     ],
-    curly: ['error', 'all'],
+    curly: ["error", "all"],
   },
   root: true,
   env: {
@@ -104,72 +105,72 @@ module.exports = {
   },
   parserOptions: {
     ecmaVersion: 9,
-    sourceType: 'module',
+    sourceType: "module",
   },
   overrides: [
     {
-      files: ['**/*.ts?(x)'],
+      files: ["**/*.ts?(x)"],
       extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        'plugin:import/typescript',
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:import/typescript",
       ],
-      parser: '@typescript-eslint/parser',
+      parser: "@typescript-eslint/parser",
       parserOptions: {
-        project: 'tsconfig.eslint.json',
+        project: "tsconfig.eslint.json",
       },
       rules: {
-        '@typescript-eslint/prefer-optional-chain': 'error',
-        'no-shadow': 'off',
-        '@typescript-eslint/no-shadow': 'error',
-        '@typescript-eslint/prefer-nullish-coalescing': 'error',
-        '@typescript-eslint/strict-boolean-expressions': [
-          'error',
+        "@typescript-eslint/prefer-optional-chain": "error",
+        "no-shadow": "off",
+        "@typescript-eslint/no-shadow": "error",
+        "@typescript-eslint/prefer-nullish-coalescing": "error",
+        "@typescript-eslint/strict-boolean-expressions": [
+          "error",
           {
             allowString: false,
             allowNumber: false,
             allowNullableObject: true,
           },
         ],
-        '@typescript-eslint/ban-ts-comment': [
-          'error',
+        "@typescript-eslint/ban-ts-comment": [
+          "error",
           {
-            'ts-ignore': 'allow-with-description',
+            "ts-ignore": "allow-with-description",
             minimumDescriptionLength: 10,
           },
         ],
-        '@typescript-eslint/explicit-function-return-type': 0,
-        '@typescript-eslint/explicit-member-accessibility': 0,
-        '@typescript-eslint/camelcase': 0,
-        '@typescript-eslint/interface-name-prefix': 0,
-        '@typescript-eslint/explicit-module-boundary-types': 'error',
-        '@typescript-eslint/no-explicit-any': 'error',
-        '@typescript-eslint/no-unused-vars': 'error',
-        '@typescript-eslint/ban-types': [
-          'error',
+        "@typescript-eslint/explicit-function-return-type": 0,
+        "@typescript-eslint/explicit-member-accessibility": 0,
+        "@typescript-eslint/camelcase": 0,
+        "@typescript-eslint/interface-name-prefix": 0,
+        "@typescript-eslint/explicit-module-boundary-types": "error",
+        "@typescript-eslint/no-explicit-any": "error",
+        "@typescript-eslint/no-unused-vars": "error",
+        "@typescript-eslint/ban-types": [
+          "error",
           {
             types: {
-              FC: 'Use `const MyComponent = (props: Props): JSX.Element` instead',
-              SFC: 'Use `const MyComponent = (props: Props): JSX.Element` instead',
+              FC: "Use `const MyComponent = (props: Props): JSX.Element` instead",
+              SFC: "Use `const MyComponent = (props: Props): JSX.Element` instead",
               FunctionComponent:
-                'Use `const MyComponent = (props: Props): JSX.Element` instead',
-              'React.FC':
-                'Use `const MyComponent = (props: Props): JSX.Element` instead',
-              'React.SFC':
-                'Use `const MyComponent = (props: Props): JSX.Element` instead',
-              'React.FunctionComponent':
-                'Use `const MyComponent = (props: Props): JSX.Element` instead',
+                "Use `const MyComponent = (props: Props): JSX.Element` instead",
+              "React.FC":
+                "Use `const MyComponent = (props: Props): JSX.Element` instead",
+              "React.SFC":
+                "Use `const MyComponent = (props: Props): JSX.Element` instead",
+              "React.FunctionComponent":
+                "Use `const MyComponent = (props: Props): JSX.Element` instead",
             },
             extendDefaults: true,
           },
         ],
-        '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
-        '@typescript-eslint/no-unnecessary-condition': 'error',
-        '@typescript-eslint/no-unnecessary-type-arguments': 'error',
-        '@typescript-eslint/prefer-string-starts-ends-with': 'error',
-        '@typescript-eslint/switch-exhaustiveness-check': 'error',
-        '@typescript-eslint/restrict-template-expressions': [
-          'error',
+        "@typescript-eslint/no-unnecessary-boolean-literal-compare": "error",
+        "@typescript-eslint/no-unnecessary-condition": "error",
+        "@typescript-eslint/no-unnecessary-type-arguments": "error",
+        "@typescript-eslint/prefer-string-starts-ends-with": "error",
+        "@typescript-eslint/switch-exhaustiveness-check": "error",
+        "@typescript-eslint/restrict-template-expressions": [
+          "error",
           {
             allowNumber: true,
             allowBoolean: true,
--- frontend/pages/about.tsx
+++ frontend/pages/about.tsx
@@ -1,3 +1,3 @@
 export default function About() {
-    return <div>About</div>;
-  }
+  return <div>About</div>;
+}
