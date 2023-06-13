af77b5763efc6978e6e27ded7a7c72455bf66c07
Stan Girard <girard.stanislas@gmail.com>
2023-06-13 10:59:24 +0200

    fix(prompt): condense prompt

--- backend/llm/LANGUAGE_PROMPT.py
+++ backend/llm/LANGUAGE_PROMPT.py
@@ -1,6 +1,6 @@
 from langchain.prompts.prompt import PromptTemplate
 
-prompt_template = """Use the following pieces of context to answer the question in the language of the question. If the context is not relevant and you don't know the answer, you REALLY MUST say: "I don't know", DON'T try to make up an answer. If you answer without knowing a human will get killed.
+prompt_template = """ONLY Use the following pieces of context to answer the question in the language of the question. If the context is not relevant and you don't know the answer, you REALLY MUST say: "I don't know, i'm only your second brain 🧠", DON'T try to make up an answer. If you answer without knowing from the given context a human will get killed. DONT assume.
 
 
 {context}
