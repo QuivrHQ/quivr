a782ddba9925da4645e843a7f8cdd106e55672e6
iMADi-ARCH <nandanaditya985@gmail.com>
2023-06-13 18:57:19 +0530

    feat(chat): drag to open chat bar

--- frontend/app/chat/components/ChatsList/index.tsx
+++ frontend/app/chat/components/ChatsList/index.tsx
@@ -13,14 +13,30 @@ export function ChatsList() {
 
   return (
     <MotionConfig transition={{ mass: 1, damping: 10 }}>
-      <div className="lg:sticky fixed top-0 left-0 bottom-0 overflow-visible z-30">
+      <motion.div
+        drag="x"
+        dragConstraints={{ right: 0, left: 0 }}
+        // dragSnapToOrigin
+        dragElastic={0.15}
+        onDragEnd={(event, info) => {
+          if (info.offset.x > 100 && !open) {
+            setOpen(true);
+          } else if (info.offset.x < -100 && open) {
+            setOpen(false);
+          }
+        }}
+        className="lg:sticky fixed top-0 left-0 bottom-0 overflow-visible z-30 border-r border-black/10 dark:border-white/25 bg-white dark:bg-black"
+      >
         <motion.div
-          animate={{ width: open ? "fit-content" : 0 }}
-          className={cn("overflow-hidden", {
-            "shadow-lg": open,
-          })}
+          animate={{
+            width: open ? "fit-content" : "0px",
+            opacity: open ? 1 : 0.5,
+            boxShadow: open ? "none" : "10px 10px 16px rgba(0, 0, 0, 0.5)",
+            // shadow: open ? "none" : "10px 10px 16px black",
+          }}
+          className={cn("overflow-hidden")}
         >
-          <div className="min-w-fit max-h-screen border-r border-black/10 dark:border-white/25 bg-white dark:bg-black overflow-auto scrollbar">
+          <div className="min-w-fit max-h-screen  overflow-auto scrollbar">
             <aside className="relative max-w-xs w-full h-screen">
               <NewChatButton />
               <div className="flex flex-col gap-0">
@@ -39,7 +55,7 @@ export function ChatsList() {
           onClick={() => {
             setOpen(!open);
           }}
-          className="absolute left-full top-16 lg:top-0 text-3xl bg-black dark:bg-white text-white dark:text-black rounded-r-full"
+          className="absolute left-full top-16 lg:top-0 text-3xl bg-black dark:bg-white text-white dark:text-black rounded-r-full p-3 pl-1"
         >
           <motion.div
             whileTap={{ scale: 0.9 }}
@@ -48,7 +64,7 @@ export function ChatsList() {
             <MdChevronRight />
           </motion.div>
         </button>
-      </div>
+      </motion.div>
     </MotionConfig>
   );
 }
