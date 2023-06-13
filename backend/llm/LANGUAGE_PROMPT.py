4546e8bc1d4403f298e8e8898b9e9be586ad571e
Arthur Busser <22616578+busser@users.noreply.github.com>
2023-06-13 16:24:07 +0200

    ✨ Improve the prompt sent to the LLM (#318)
    This is a more detailed prompt that explains what Quivr is and what it
    does.

--- backend/llm/LANGUAGE_PROMPT.py
+++ backend/llm/LANGUAGE_PROMPT.py
@@ -1,6 +1,6 @@
 from langchain.prompts.prompt import PromptTemplate
 
-prompt_template = """Use the following pieces of context to answer the question in the language of the question. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+prompt_template = """Your name is Quivr. You are a second brain. A person will ask you a question and you will provide a helpful answer. Write the answer in the same language as the question. If you don't know the answer, just say that you don't know. Don't try to make up an answer. Use the following context to answer the question:
 
 
 {context}
