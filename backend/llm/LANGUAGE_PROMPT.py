4c382ec85e6ce967c95f61cf96e3401861b3593d
Stan Girard <girard.stanislas@gmail.com>
2023-06-13 10:35:06 +0200

    fix(prompt): fixed qa prompt

--- backend/llm/LANGUAGE_PROMPT.py
+++ backend/llm/LANGUAGE_PROMPT.py
@@ -1,14 +1,7 @@
 from langchain.prompts.prompt import PromptTemplate
 
-_template = """Given the following conversation and a follow up question, answer the follow up question in the initial language of the question. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+prompt_template = """Use the following pieces of context to answer the question in the language of the question. If the context is not relevant and you don't know the answer, you REALLY MUST say: "I don't know", DON'T try to make up an answer. If you answer without knowing a human will get killed.
 
-Chat History:
-{chat_history}
-Follow Up Input: {question}
-Standalone question:"""
-CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
-
-prompt_template = """Use the following pieces of context to answer the question in the language of the question. If you don't know the answer, just say that you don't know, don't try to make up an answer. 
 
 {context}
 
