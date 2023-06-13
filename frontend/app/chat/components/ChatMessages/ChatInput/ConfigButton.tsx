cb24730c4314312691cb29c393f8015d6c86e911
iMADi-ARCH <nandanaditya985@gmail.com>
2023-06-13 19:08:38 +0530

    fix(chat): make chat input a little responsive

--- frontend/app/chat/components/ChatMessages/ChatInput/ConfigButton.tsx
+++ frontend/app/chat/components/ChatMessages/ChatInput/ConfigButton.tsx
@@ -6,8 +6,8 @@ import Button from "../../../../components/ui/Button";
 export function ConfigButton() {
   return (
     <Link href={"/config"}>
-      <Button className="px-3" variant={"tertiary"}>
-        <MdSettings className="text-2xl" />
+      <Button className="p-2 sm:px-3" variant={"tertiary"}>
+        <MdSettings className="text-lg sm:text-xl lg:text-2xl" />
       </Button>
     </Link>
   );
