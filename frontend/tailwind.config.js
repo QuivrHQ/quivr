4c6beea626337d5ae1a4b9e1ef8eaeaf35c083d5
Zineb El Bachiri <100568984+gozineb@users.noreply.github.com>
2023-06-14 15:00:05 +0200

    Refactor/front fix navba (#328)

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './lib/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
