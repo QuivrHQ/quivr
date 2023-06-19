78ac21ad3ace60a77f7a73dddd20b48bc97d3b1d
Stan Girard <girard.stanislas@gmail.com>
2023-06-19 17:53:07 +0200

    fix(llm): changed to stuff

--- backend/parsers/common.py
+++ backend/parsers/common.py
@@ -32,7 +32,7 @@ async def process_file(commons: CommonsDep, file: UploadFile, loader_class, file
         file_sha1 = compute_sha1_from_file(tmp_file.name)
 
     os.remove(tmp_file.name)
-    chunk_size = 1000
+    chunk_size = 500
     chunk_overlap = 0
 
     text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
