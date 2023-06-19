2ab32dbe997e78352cbcfa460aff44461d5b8842
gozineb <zinebe@theodo.fr>
2023-06-19 18:26:32 +0200

    ♻️ refacto brain routes

--- backend/models/brains.py
+++ backend/models/brains.py
@@ -6,7 +6,15 @@ from pydantic import BaseModel
 
 
 class Brain(BaseModel):
-    brain_id: Optional[UUID]
+    brain_id: Optional[UUID] = None
+    name: str = "New Brain"
+    status: Optional[str]= "public"
+    model: Optional[str] = "gpt-3.5-turbo-0613"
+    temperature: Optional[float] = 0.0
+    max_tokens: Optional[int] = 256
+    
+class BrainToUpdate(BaseModel): 
+    brain_id: UUID
     name: Optional[str] = "New Brain"
     status: Optional[str] = "public"
     model: Optional[str] = "gpt-3.5-turbo-0613"
