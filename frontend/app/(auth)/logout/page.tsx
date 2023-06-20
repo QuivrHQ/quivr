ac9ea20743a24089df22bbeae00f809370e439f0
chshji666666 <chshji666666@163.com>
2023-06-20 20:43:00 +0800

    cc

--- frontend/app/(auth)/logout/page.tsx
+++ frontend/app/(auth)/logout/page.tsx
@@ -40,7 +40,7 @@ export default function Logout() {
   return (
     <main>
       <section className="w-full min-h-[80vh] h-full outline-none flex flex-col gap-5 items-center justify-center p-6">
-        <PageHeading title="Logout" subtitle="See you next time" />
+        <PageHeading title="退出" subtitle="期待下次再见" />
         <Card className="max-w-md w-full p-5 sm:p-10 text-center flex flex-col items-center gap-5">
           <h2 className="text-lg">确认退出?</h2>
           <div className="flex gap-5 items-center justify-center">
