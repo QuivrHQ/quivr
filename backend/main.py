3e753f2d56cb1c1c9f06b61c1cc2f110cbee5f01
Matt <77928207+mattzcarey@users.noreply.github.com>
2023-06-20 20:53:04 +0100

    feat: stream router (#353)
    * wip: stream router
    
    * feat: chatai streaming
    
    * chore: add comments
    
    * feat: streaming for chains
    
    * chore: comments

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,7 @@ from routes.chat_routes import chat_router
 from routes.crawl_routes import crawl_router
 from routes.explore_routes import explore_router
 from routes.misc_routes import misc_router
+from routes.stream_routes import stream_router
 from routes.upload_routes import upload_router
 from routes.user_routes import user_router
 
@@ -19,11 +20,13 @@ app = FastAPI()
 
 add_cors_middleware(app)
 max_brain_size = os.getenv("MAX_BRAIN_SIZE")
-max_brain_size_with_own_key = os.getenv("MAX_BRAIN_SIZE_WITH_KEY",209715200)
+max_brain_size_with_own_key = os.getenv("MAX_BRAIN_SIZE_WITH_KEY", 209715200)
+
 
 @app.on_event("startup")
 async def startup_event():
     pypandoc.download_pandoc()
+
 
 app.include_router(brain_router)
 app.include_router(chat_router)
@@ -33,3 +36,4 @@ app.include_router(misc_router)
 app.include_router(upload_router)
 app.include_router(user_router)
 app.include_router(api_key_router)
+app.include_router(stream_router)
