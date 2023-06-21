3d76f36e0b2cd439af96dd5f322e84e29e27180c
Luis Eduardo Mendez Tafolla <luismendeztaf3@gmail.com>
2023-06-21 14:46:04 -0600

    Se elimino exp del token @thefrikz18

--- backend/auth/jwt_token_handler.py
+++ backend/auth/jwt_token_handler.py
@@ -11,11 +11,6 @@ ALGORITHM = "HS256"
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
