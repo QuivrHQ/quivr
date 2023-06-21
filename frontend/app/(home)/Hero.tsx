feb938057844da35dd7a204a172677b6f5fd3e1f
chshji666666 <chshji666666@163.com>
2023-06-21 10:31:22 +0800

    cc

--- frontend/app/(home)/Hero.tsx
+++ frontend/app/(home)/Hero.tsx
@@ -40,7 +40,7 @@ const Hero = (): JSX.Element => {
         className="top-24 -z-0 flex flex-col gap-2 items-center justify-center pt-24"
       >
         <h1 className="text-5xl sm:text-7xl font-bold max-w-lg sm:max-w-xl">
-          Get a Second Brain with <span className="text-primary">Quivr</span>
+          专有知识库聊天机器人  <span className="text-primary">Vantoo</span>
         </h1>
         <p className="text-base max-w-sm text-gray-500 mb-5 sm:mb-10">
           Quivr is your second brain in the cloud, designed to easily store and
