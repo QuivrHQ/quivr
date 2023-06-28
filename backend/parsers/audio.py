d94c996d782e54f289574d464b7d963caed8004f
gozineb <zinebe@theodo.fr>
2023-06-28 10:28:20 +0200

    ⚰️ remove comments

--- backend/parsers/audio.py
+++ backend/parsers/audio.py
@@ -9,25 +9,6 @@ from models.files import File
 from models.settings import CommonsDep
 from utils.file import compute_sha1_from_content
 
-# # Create a function to transcribe audio using Whisper
-# def _transcribe_audio(api_key, audio_file, stats_db):
-#     openai.api_key = api_key
-#     transcript = ""
-
-#     with BytesIO(audio_file.read()) as audio_bytes:
-#         # Get the extension of the uploaded file
-#         file_extension = os.path.splitext(audio_file.name)[-1]
-
-#         # Create a temporary file with the uploaded audio data and the correct extension
-#         with tempfile.NamedTemporaryFile(delete=True, suffix=file_extension) as temp_audio_file:
-#             temp_audio_file.write(audio_bytes.read())
-#             temp_audio_file.seek(0)  # Move the file pointer to the beginning of the file
-
-#             transcript = openai.Audio.translate("whisper-1", temp_audio_file)
-
-#     return transcript
-
-# async def process_audio(upload_file: File, stats_db):
 
 async def process_audio(commons: CommonsDep, file: File, enable_summarization: bool, user, user_openai_api_key):
 
@@ -35,15 +16,14 @@ async def process_audio(commons: CommonsDep, file: File, enable_summarization: b
     file_sha = ""
     dateshort = time.strftime("%Y%m%d-%H%M%S")
     file_meta_name = f"audiotranscript_{dateshort}.txt"
-    # uploaded file to file object
 
+    # use this for whisper
     openai_api_key = os.environ.get("OPENAI_API_KEY")
     if user_openai_api_key:
         openai_api_key = user_openai_api_key
 
     try:
         upload_file = file.file
-        # Here, we're writing the uploaded file to a temporary file, so we can use it with your existing code.
         with tempfile.NamedTemporaryFile(delete=False, suffix=upload_file.filename) as tmp_file:
             await upload_file.seek(0)
             content = await upload_file.read()
@@ -59,7 +39,6 @@ async def process_audio(commons: CommonsDep, file: File, enable_summarization: b
         file_sha = compute_sha1_from_content(transcript.text.encode("utf-8"))
         file_size = len(transcript.text.encode("utf-8"))
 
-        # Load chunk size and overlap from sidebar
         chunk_size = 500
         chunk_overlap = 0
 
@@ -70,11 +49,8 @@ async def process_audio(commons: CommonsDep, file: File, enable_summarization: b
         docs_with_metadata = [Document(page_content=text, metadata={"file_sha1": file_sha, "file_size": file_size, "file_name": file_meta_name,
                                     "chunk_size": chunk_size, "chunk_overlap": chunk_overlap, "date": dateshort}) for text in texts]
 
-        # if st.secrets.self_hosted == "false":
-        #     add_usage(stats_db, "embedding", "audio", metadata={"file_name": file_meta_name,"file_type": ".txt", "chunk_size": chunk_size, "chunk_overlap": chunk_overlap})
         commons.documents_vector_store.add_documents(docs_with_metadata)
 
     finally:
         if temp_filename and os.path.exists(temp_filename):
-            os.remove(temp_filename)
-
+             os.remove(temp_filename)
