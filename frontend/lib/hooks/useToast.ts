8b9c48956897257ea375774ab80188ae65c04515
chshji666666 <chshji666666@163.com>
2023-06-28 13:17:11 +0800

    same

--- frontend/lib/hooks/useToast.ts
+++ frontend/lib/hooks/useToast.ts
@@ -3,11 +3,20 @@ import { useContext } from "react";
 import { ToastContext } from "../components/ui/Toast/domain/ToastContext";
 import { ToastPublisher } from "../components/ui/Toast/domain/types";
 
-export const useToast = (): { publish: ToastPublisher; setTop:() =>void } => {
+export const useToast = (): { publish: ToastPublisher, setTop: () => void } => {
     const { publish, setTop } = useContext(ToastContext);
 
-  return {
-      publish,
-      setTop: setTop as () => void,
-  };
+    const setToastOnTop = () => {
+        // 在这里添加代码以将提示框设置在页面最上层
+        // 将提示框的位置设置为页面布局的最上层
+        const toastContainer = document.getElementById("toast-container");
+        if (toastContainer) {
+            toastContainer.style.zIndex = "9999";
+        }
+    };
+
+    return {
+        publish,
+        setTop: setToastOnTop,
+    };
 };
