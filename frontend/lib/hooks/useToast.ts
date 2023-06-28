d18b1379ee712839b72de0e536a2e6d731000152
chshji666666 <chshji666666@163.com>
2023-06-28 13:06:37 +0800

    same

--- frontend/lib/hooks/useToast.ts
+++ frontend/lib/hooks/useToast.ts
@@ -3,7 +3,7 @@ import { useContext } from "react";
 import { ToastContext } from "../components/ui/Toast/domain/ToastContext";
 import { ToastPublisher } from "../components/ui/Toast/domain/types";
 
-export const useToast = (): { publish: ToastPublisher,setTop:() =>void } => {
+export const useToast = (): { publish: ToastPublisher; setTop:() =>void } => {
     const { publish, setTop } = useContext(ToastContext);
 
   return {
