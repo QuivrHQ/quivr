1e1b63df8b46297716877aefb2d2596cb319c0d6
Zineb El Bachiri <100568984+gozineb@users.noreply.github.com>
2023-06-30 09:08:40 +0200

    ✏️ add / in all explore url (#415)
    * ✏️ add / in all explore url
    
    * 🐛 fix user routes
    
    * ⚰️ remove dead code
    
    * ✨ add name to active brain

--- backend/routes/upload_routes.py
+++ backend/routes/upload_routes.py
@@ -12,16 +12,6 @@ from utils.processors import filter_file
 
 upload_router = APIRouter()
 
-def get_user_vectors(commons, user):
-    user_vectors_response = commons['supabase'].table("vectors").select(
-        "name:metadata->>file_name, size:metadata->>file_size", count="exact") \
-            .filter("user_id", "eq", user.email)\
-            .execute()
-    documents = user_vectors_response.data  # Access the data from the response
-    # Convert each dictionary to a tuple of items, then to a set to remove duplicates, and then back to a dictionary
-    user_unique_vectors = [dict(t) for t in set(tuple(d.items()) for d in documents)]
-    return user_unique_vectors
-
 
 @upload_router.post("/upload", dependencies=[Depends(AuthBearer())], tags=["Upload"])
 async def upload_file(request: Request,  uploadFile: UploadFile, brain_id: UUID = Query(..., description="The ID of the brain"), enable_summarization: bool = False, current_user: User = Depends(get_current_user)):
@@ -49,8 +39,6 @@ async def upload_file(request: Request,  uploadFile: UploadFile, brain_id: UUID 
         brain.max_brain_size = os.getenv("MAX_BRAIN_SIZE_WITH_KEY",209715200)
     remaining_free_space =  brain.remaining_brain_size
 
-
-    
     file_size = get_file_size(uploadFile)
 
     file = File(file=uploadFile)
