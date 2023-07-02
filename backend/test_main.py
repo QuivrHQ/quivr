ce18e033c1a994e6dd608dc7d55279d1f6250dfe
Stan Girard <girard.stanislas@gmail.com>
2023-07-02 15:37:23 +0200

    ci(test): added test on endpoints (#454)

--- backend/test_main.py
+++ backend/test_main.py
@@ -214,3 +214,140 @@ def test_create_chat_and_talk():
         "/chat/" + chat_id, headers={"Authorization": "Bearer " + API_KEY}
     )
     assert delete_response.status_code == 200
+
+
+def test_explore_with_default_brain():
+    # Retrieve the default brain
+    brain_response = client.get(
+        "/brains/default", headers={"Authorization": "Bearer " + API_KEY}
+    )
+    assert brain_response.status_code == 200
+    default_brain_id = brain_response.json()["brain_id"]
+
+    # Now use the default brain_id as parameter in the /explore/ endpoint
+    response = client.get(
+        f"/explore/{default_brain_id}",
+        headers={"Authorization": "Bearer " + API_KEY},
+    )
+
+    # Assert that the response status code is 200 (HTTP OK)
+    assert response.status_code == 200
+
+    # Optionally, you can assert on specific fields in the response data
+    response_data = response.json()
+    # e.g., assert that the response contains a 'results' field
+    assert "documents" in response_data
+
+
+def test_upload_and_delete_file():
+    # Retrieve the default brain
+    brain_response = client.get(
+        "/brains/default", headers={"Authorization": "Bearer " + API_KEY}
+    )
+    assert brain_response.status_code == 200
+    default_brain_id = brain_response.json()["brain_id"]
+
+    # File to upload
+    file_path = "test.txt"
+    file_name = "test.txt"  # Assuming the name of the file on the server is the same as the local file name
+
+    # Set enable_summarization flag
+    enable_summarization = False
+
+    # Upload the file
+    with open(file_path, "rb") as file:
+        upload_response = client.post(
+            f"/upload?brain_id={default_brain_id}&enable_summarization={enable_summarization}",
+            headers={"Authorization": "Bearer " + API_KEY},
+            files={"uploadFile": file},
+        )
+
+    # Assert that the upload response status code is 200 (HTTP OK)
+    assert upload_response.status_code == 200
+
+    # Optionally, you can assert on specific fields in the upload response data
+    upload_response_data = upload_response.json()
+    assert "message" in upload_response_data
+
+    # Delete the file
+    delete_response = client.delete(
+        f"/explore/{file_name}",
+        headers={"Authorization": "Bearer " + API_KEY},
+        params={"brain_id": default_brain_id},
+    )
+
+    # Assert that the delete response status code is 200 (HTTP OK)
+    assert delete_response.status_code == 200
+
+    # Optionally, you can assert on specific fields in the delete response data
+    delete_response_data = delete_response.json()
+    assert "message" in delete_response_data
+
+
+def test_upload_explore_and_delete_file():
+    # Retrieve the default brain
+    brain_response = client.get(
+        "/brains/default", headers={"Authorization": "Bearer " + API_KEY}
+    )
+    assert brain_response.status_code == 200
+    default_brain_id = brain_response.json()["brain_id"]
+
+    # File to upload
+    file_path = "test.txt"
+    file_name = "test.txt"  # Assuming the name of the file on the server is the same as the local file name
+
+    # Set enable_summarization flag
+    enable_summarization = False
+
+    # Upload the file
+    with open(file_path, "rb") as file:
+        upload_response = client.post(
+            f"/upload?brain_id={default_brain_id}&enable_summarization={enable_summarization}",
+            headers={"Authorization": "Bearer " + API_KEY},
+            files={"uploadFile": file},
+        )
+
+    # Assert that the upload response status code is 200 (HTTP OK)
+    assert upload_response.status_code == 200
+
+    # Optionally, you can assert on specific fields in the upload response data
+    upload_response_data = upload_response.json()
+    assert "message" in upload_response_data
+
+    # Explore (Download) the file
+    explore_response = client.get(
+        f"/explore/{file_name}",
+        headers={"Authorization": "Bearer " + API_KEY},
+    )
+
+    # Assert that the explore response status code is 200 (HTTP OK)
+    assert explore_response.status_code == 200
+
+    # Delete the file
+    delete_response = client.delete(
+        f"/explore/{file_name}",
+        headers={"Authorization": "Bearer " + API_KEY},
+        params={"brain_id": default_brain_id},
+    )
+
+    # Assert that the delete response status code is 200 (HTTP OK)
+    assert delete_response.status_code == 200
+
+    # Optionally, you can assert on specific fields in the delete response data
+    delete_response_data = delete_response.json()
+    assert "message" in delete_response_data
+
+
+def test_get_user_info():
+    # Send a request to get user information
+    response = client.get("/user", headers={"Authorization": "Bearer " + API_KEY})
+
+    # Assert that the response status code is 200 (HTTP OK)
+    assert response.status_code == 200
+
+    # Assert that the response contains the expected fields
+    user_info = response.json()
+    assert "email" in user_info
+    assert "max_brain_size" in user_info
+    assert "current_brain_size" in user_info
+    assert "date" in user_info
