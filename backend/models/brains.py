1cb9dd19eb453dc749258b72bfbf17c523c1c45e
Matt <77928207+mattzcarey@users.noreply.github.com>
2023-07-14 20:02:26 +0100

    fix: bug if no brain (#635)

--- backend/models/brains.py
+++ backend/models/brains.py
@@ -3,11 +3,10 @@ from typing import Any, List, Optional
 from uuid import UUID
 
 from logger import get_logger
-from pydantic import BaseModel
-from utils.vectors import get_unique_files_from_vector_ids
-
 from models.settings import CommonsDep, common_dependencies
 from models.users import User
+from pydantic import BaseModel
+from utils.vectors import get_unique_files_from_vector_ids
 
 logger = get_logger(__name__)
 
@@ -153,7 +152,7 @@ class Brain(BaseModel):
         self.id = response.data[0]["brain_id"]
         return response.data
 
-    def create_brain_user(self, user_id: UUID, rights, default_brain):
+    def create_brain_user(self, user_id: UUID, rights, default_brain: bool):
         commons = common_dependencies()
         response = (
             commons["supabase"]
@@ -279,7 +278,6 @@ def get_default_user_brain(user: User):
         .execute()
     )
 
-    logger.info("Default brain response:", response.data)
     default_brain_id = response.data[0]["brain_id"] if response.data else None
 
     logger.info(f"Default brain id: {default_brain_id}")
@@ -295,4 +293,14 @@ def get_default_user_brain(user: User):
 
         return brain_response.data[0] if brain_response.data else None
 
-    return None
+
+def get_default_user_brain_or_create_new(user: User) -> Brain:
+    default_brain = get_default_user_brain(user)
+
+    if default_brain:
+        return Brain.create(**default_brain)
+    else:
+        brain = Brain.create()
+        brain.create_brain()
+        brain.create_brain_user(user.id, "Owner", True)
+        return brain
