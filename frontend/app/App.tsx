95c9f598e9783acb2f4b27b53dcf916fdb4cd403
mamadoudicko <mamadoudicko100@gmail.com>
2023-07-17 12:28:38 +0200

    fix(Brain): fecth brains on auth status change

--- frontend/app/App.tsx
+++ frontend/app/App.tsx
@@ -6,15 +6,17 @@ import Footer from "@/lib/components/Footer";
 import { NavBar } from "@/lib/components/NavBar";
 import { TrackingWrapper } from "@/lib/components/TrackingWrapper";
 import { useBrainContext } from "@/lib/context/BrainProvider/hooks/useBrainContext";
+import { useSupabase } from "@/lib/context/SupabaseProvider";
 
 // This wrapper is used to make effect calls at a high level in app rendering.
 export const App = ({ children }: PropsWithChildren): JSX.Element => {
   const { fetchAllBrains, fetchAndSetActiveBrain } = useBrainContext();
+  const { session } = useSupabase();
 
   useEffect(() => {
     void fetchAllBrains();
     void fetchAndSetActiveBrain();
-  }, []);
+  }, [session?.user]);
 
   return (
     <>
