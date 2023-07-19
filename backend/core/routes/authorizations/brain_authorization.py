4b42570c1a703bbd24bd4aea42af34a2b4aa3184
mamadoudicko <mamadoudicko100@gmail.com>
2023-07-19 11:53:18 +0200

    feat(RBAC): add role enum and supports multiple roles check

--- backend/core/routes/authorizations/brain_authorization.py
+++ backend/core/routes/authorizations/brain_authorization.py
@@ -1,4 +1,5 @@
-from typing import Optional
+from enum import Enum
+from typing import List, Optional, Union
 from uuid import UUID
 
 from auth.auth_bearer import get_current_user
@@ -7,16 +8,27 @@ from models.brains import Brain
 from models.users import User
 
 
-def has_brain_authorization(required_role: Optional[str] = "Owner"):
+class RoleEnum(str, Enum):
+    Viewer = "Viewer"
+    Editor = "Editor"
+    Owner = "Owner"
+
+
+def has_brain_authorization(
+    required_roles: Optional[Union[RoleEnum, List[RoleEnum]]] = RoleEnum.Owner
+):
     """
-    Decorator to check if the user has the required role for the brain
-    param: required_role: The role required to access the brain
+    Decorator to check if the user has the required role(s) for the brain
+    param: required_roles: The role(s) required to access the brain
     return: A wrapper function that checks the authorization
     """
 
     async def wrapper(brain_id: UUID, current_user: User = Depends(get_current_user)):
+        nonlocal required_roles
+        if isinstance(required_roles, str):
+            required_roles = [required_roles]  # Convert single role to a list
         validate_brain_authorization(
-            brain_id=brain_id, user_id=current_user.id, required_role=required_role
+            brain_id=brain_id, user_id=current_user.id, required_roles=required_roles
         )
 
     return wrapper
@@ -25,17 +37,17 @@ def has_brain_authorization(required_role: Optional[str] = "Owner"):
 def validate_brain_authorization(
     brain_id: UUID,
     user_id: UUID,
-    required_role: Optional[str] = "Owner",
+    required_roles: Optional[Union[RoleEnum, List[RoleEnum]]] = RoleEnum.Owner,
 ):
     """
-    Function to check if the user has the required role for the brain
+    Function to check if the user has the required role(s) for the brain
     param: brain_id: The id of the brain
     param: user_id: The id of the user
-    param: required_role: The role required to access the brain
+    param: required_roles: The role(s) required to access the brain
     return: None
     """
 
-    if required_role is None:
+    if required_roles is None:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Missing required role",
@@ -49,10 +61,13 @@ def validate_brain_authorization(
             detail="You don't have permission for this brain",
         )
 
-    # TODO: Update this logic when we have more roles
-    # Eg: Owner > Admin > User ... this should be taken into account
-    if user_brain.get("rights") != required_role:
+    # Convert single role to a list to handle both cases
+    if isinstance(required_roles, str):
+        required_roles = [required_roles]
+
+    # Check if the user has at least one of the required roles
+    if user_brain.get("rights") not in required_roles:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
-            detail="You don't have the required role for this brain",
+            detail="You don't have the required role(s) for this brain",
         )
