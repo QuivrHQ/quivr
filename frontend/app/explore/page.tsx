713006e039c8dfd811d506a1f99f1690531a6a7c
gozineb <zinebe@theodo.fr>
2023-07-19 13:32:39 +0200

    ✨ add brain name to explore and upload page

--- frontend/app/explore/page.tsx
+++ frontend/app/explore/page.tsx
@@ -1,21 +1,31 @@
-/* eslint-disable */
 "use client";
 import { AnimatePresence, motion } from "framer-motion";
 import Link from "next/link";
 
 import Button from "@/lib/components/ui/Button";
 import Spinner from "@/lib/components/ui/Spinner";
+import { useBrainContext } from "@/lib/context/BrainProvider/hooks/useBrainContext";
 import { useSupabase } from "@/lib/context/SupabaseProvider";
-
 import { redirectToLogin } from "@/lib/router/redirectToLogin";
+
 import DocumentItem from "./DocumentItem";
 import { useExplore } from "./hooks/useExplore";
 
 const ExplorePage = (): JSX.Element => {
   const { session } = useSupabase();
   const { documents, setDocuments, isPending } = useExplore();
+  const { currentBrain } = useBrainContext();
   if (session === null) {
     redirectToLogin();
+  }
+  if (currentBrain === undefined) {
+    return (
+      <div className="flex flex-col items-center justify-center mt-10 gap-1">
+        <p className="text-center">
+          {"You need to select a brain first. 🧠💡🥲"}
+        </p>
+      </div>
+    );
   }
 
   return (
@@ -23,7 +33,7 @@ const ExplorePage = (): JSX.Element => {
       <section className="w-full outline-none pt-10 flex flex-col gap-5 items-center justify-center p-6">
         <div className="flex flex-col items-center justify-center">
           <h1 className="text-3xl font-bold text-center">
-            Explore uploaded data
+            Explore uploaded data in {currentBrain.name}
           </h1>
           <h2 className="opacity-50">
             View or delete stored data used by your brain
