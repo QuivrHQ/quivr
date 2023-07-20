468e169aec3182ebe3d3a78c64a57613bd62dc04
MaHDiaLaGaB <elmahdi.alagab@gmail.com>
2023-07-20 21:50:03 +0200

    add new 16k model

--- backend/utils/constants.py
+++ backend/utils/constants.py
@@ -3,6 +3,6 @@ openai_function_compatible_models = [
     "gpt-4-0613",
 ]
 
-streaming_compatible_models = ["gpt-3.5-turbo, gpt4all-j-1.3"]
+streaming_compatible_models = ["gpt-3.5-turbo, gpt4all-j-1.3", "gpt-3.5-turbo-16k-0613"]
 
 private_models = ["gpt4all-j-1.3"]
