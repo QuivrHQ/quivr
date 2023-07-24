d1c8f24bdeb91a3e16fc6739e1b5730c5cadf43e
mamadoudicko <mamadoudicko100@gmail.com>
2023-07-24 10:19:00 +0200

    feat: add brain management button

---
+++ frontend/lib/components/NavBar/components/NavItems/components/BrainManagementButton.tsx
@@ -0,0 +1,11 @@
+import { FaBrain } from "react-icons/fa";
+import { MdSettings } from "react-icons/md";
+
+export const BrainManagementButton = (): JSX.Element => {
+  return (
+    <button type="button" className="flex items-center focus:outline-none">
+      <MdSettings className="w-6 h-6" color="gray" />
+      <FaBrain className="w-3 h-3" color="gray" />
+    </button>
+  );
+};
