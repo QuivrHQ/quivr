148f6e1e1632455c37539ff54bbfb535d5995af7
mamadoudicko <mamadoudicko100@gmail.com>
2023-07-24 10:40:28 +0200

    feat: add brains list

---
+++ frontend/app/brains-management/hooks/useBrainsList.ts
@@ -0,0 +1,21 @@
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+
+import { useDevice } from "@/lib/hooks/useDevice";
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const useBrainsList = () => {
+  const { isMobile } = useDevice();
+  const [open, setOpen] = useState(!isMobile);
+
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setOpen(!isMobile);
+  }, [isMobile, pathname]);
+
+  return {
+    open,
+    setOpen,
+  };
+};
