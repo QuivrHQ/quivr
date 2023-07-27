2036bdea6549452f67cffb8609e1a380edc70f1e
Corey Psoinos <coreypsoinos@gmail.com>
2023-07-26 20:06:44 -0400

    remove newlines from imports

--- frontend/app/explore/DocumentItem/__tests__/DocumentData.test.tsx
+++ frontend/app/explore/DocumentItem/__tests__/DocumentData.test.tsx
@@ -1,8 +1,6 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import { describe, expect, it, vi } from "vitest";
-
 import { BrainConfigProvider } from "@/lib/context/BrainConfigProvider";
-
 import DocumentData from "../DocumentData";
 
 const useSupabaseMock = vi.fn(() => ({
