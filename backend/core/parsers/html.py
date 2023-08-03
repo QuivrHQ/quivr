fdcdf581a8e7071848d4c50d927d2237e28dd172
ChloeMouret <63965373+ChloeMouret@users.noreply.github.com>
2023-08-03 16:20:36 +0200

    refactor: clean backend, add typing, delete dead code (#835)
    * type common dependencies
    
    * delete unused code

--- backend/core/parsers/html.py
+++ backend/core/parsers/html.py
@@ -9,8 +9,17 @@ from models.settings import CommonsDep
 from .common import process_file
 
 
-def process_html(commons: CommonsDep, file: File, enable_summarization, brain_id, user_openai_api_key):
-    return process_file(commons, file, UnstructuredHTMLLoader,  enable_summarization, brain_id, user_openai_api_key)
+def process_html(
+    commons: CommonsDep, file: File, enable_summarization, brain_id, user_openai_api_key
+):
+    return process_file(
+        commons,
+        file,
+        UnstructuredHTMLLoader,
+        enable_summarization,
+        brain_id,
+        user_openai_api_key,
+    )
 
 
 def get_html(url):
@@ -19,11 +28,3 @@ def get_html(url):
         return response.text
     else:
         return None
-
-
-def slugify(text):
-    text = unicodedata.normalize('NFKD', text).encode(
-        'ascii', 'ignore').decode('utf-8')
-    text = re.sub(r'[^\w\s-]', '', text).strip().lower()
-    text = re.sub(r'[-\s]+', '-', text)
-    return text
--- backend/core/utils/constants.py
+++
@@ -1,8 +0,0 @@
-openai_function_compatible_models = [
-    "gpt-3.5-turbo-0613",
-    "gpt-4-0613",
-]
-
-streaming_compatible_models = ["gpt-3.5-turbo, gpt4all-j-1.3"]
-
-private_models = ["gpt4all-j-1.3"]
