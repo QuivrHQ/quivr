f768bb2345c7c5df11cec6504953620ee0ca2c95
Gustavo Maciel <gustavo_m13@outlook.com>
2023-08-06 21:43:25 -0400

    add spanish translation on login page

--- frontend/app/(auth)/login/components/GoogleLogin/index.tsx
+++ frontend/app/(auth)/login/components/GoogleLogin/index.tsx
@@ -2,9 +2,11 @@
 import Button from "@/lib/components/ui/Button";
 
 import { useGoogleLogin } from "./hooks/useGoogleLogin";
+import { useTranslation } from "react-i18next";
 
 export const GoogleLoginButton = () => {
   const { isPending, signInWithGoogle } = useGoogleLogin();
+  const {t, i18n} = useTranslation(["login"]);
 
   return (
     <Button
@@ -14,7 +16,7 @@ export const GoogleLoginButton = () => {
       type="button"
       data-testid="google-login-button"
     >
-      Login with Google
+      {t("googleLogin",{ ns: 'login' })}
     </Button>
   );
 };
