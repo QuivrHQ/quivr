5915685fa2b376c622732b1285d812e4299706ae
B0rrA <gustavo_m13@outlook.com>
2023-08-06 21:48:59 -0400

    fix (i18n) init and types

---
+++ frontend/lib/config/LocaleConfig/i18n.ts
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable @typescript-eslint/no-floating-promises */
+// import the original type declarations
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import { resources } from "./resources";
+
+const defaultNS = "translation";
+
+i18n.use(initReactI18next).init({
+  lng: "en",
+  fallbackLng: "en",
+  defaultNS,
+  resources,
+  debug: process.env.NEXT_PUBLIC_ENV !== "prod",
+});
