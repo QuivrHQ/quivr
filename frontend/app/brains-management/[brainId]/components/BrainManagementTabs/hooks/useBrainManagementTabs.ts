a17bfae69688d81632419065cb0f64332fc9b9d3
Stepan Lebedev <129732311+StepanLebedevTheodo@users.noreply.github.com>
2023-08-08 18:18:05 +0200

    feat: deleting brains on brain manager page (#893)
    * fix: new libraries for translation
    
    * feat(deleting-brains-on-brain-management-page): adding a delete brain button and a confirmation modal on a management page
    
    * feat(deleting-brains-on-brain-management-page): adding tests for confirmation Modal
    
    * feat(deleting-brains-on-brain-management-page): Adding translation to the delete button and modal

--- frontend/app/brains-management/[brainId]/components/BrainManagementTabs/hooks/useBrainManagementTabs.ts
+++ frontend/app/brains-management/[brainId]/components/BrainManagementTabs/hooks/useBrainManagementTabs.ts
@@ -1,6 +1,8 @@
 import { UUID } from "crypto";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
+
+import { useBrainContext } from "@/lib/context/BrainProvider/hooks/useBrainContext";
 
 import { BrainManagementTab } from "../types";
 
@@ -8,14 +10,30 @@ import { BrainManagementTab } from "../types";
 export const useBrainManagementTabs = () => {
   const [selectedTab, setSelectedTab] =
     useState<BrainManagementTab>("settings");
+  const { deleteBrain, setCurrentBrainId } = useBrainContext();
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const router = useRouter();
 
   const params = useParams();
 
   const brainId = params?.brainId as UUID | undefined;
 
+  const handleDeleteBrain = () => {
+    if (brainId === undefined) {
+      return;
+    }
+    void deleteBrain(brainId);
+    setCurrentBrainId(null);
+    router.push("/brains-management");
+    setIsDeleteModalOpen(false);
+  };
+
   return {
     selectedTab,
     setSelectedTab,
     brainId,
+    handleDeleteBrain,
+    isDeleteModalOpen,
+    setIsDeleteModalOpen,
   };
 };
