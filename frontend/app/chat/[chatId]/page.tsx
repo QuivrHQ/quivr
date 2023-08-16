0324b52bee45b2450937c74a1f33e5875da64cc5
Aditya Mundhalia <aaditya.mundhalia@gmail.com>
2023-08-16 22:16:36 +1000

    correcting scroll & chat response view better code (#960)

--- frontend/app/chat/[chatId]/page.tsx
+++ frontend/app/chat/[chatId]/page.tsx
@@ -10,7 +10,7 @@ const SelectedChatPage = (): JSX.Element => {
       <section className="flex flex-col flex-1 items-center w-full h-full min-h-[70vh]">
         <ChatHeader />
         <div className="flex-1 flex flex-col mt-8 w-full shadow-md dark:shadow-primary/25 hover:shadow-xl transition-shadow rounded-xl overflow-hidden bg-white dark:bg-black border border-black/10 dark:border-white/25 p-12 pt-10 max-h-[80vh]">
-          <div className="flex flex-1 flex-col overflow-scroll">
+          <div className="flex flex-1 flex-col overflow-hidden">
             <ChatDialog />
           </div>
           <ActionsBar />
