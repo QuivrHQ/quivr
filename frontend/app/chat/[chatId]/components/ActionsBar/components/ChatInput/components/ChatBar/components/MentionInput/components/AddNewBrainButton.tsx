d4589e67b1159f7a14ca8b44376ef1674e4b6484
mamadoudicko <mamadoudicko100@gmail.com>
2023-08-21 08:10:32 +0200

    feat: update translations

--- frontend/app/chat/[chatId]/components/ActionsBar/components/ChatInput/components/ChatBar/components/MentionInput/components/AddNewBrainButton.tsx
+++ frontend/app/chat/[chatId]/components/ActionsBar/components/ChatInput/components/ChatBar/components/MentionInput/components/AddNewBrainButton.tsx
@@ -1,15 +1,20 @@
 import Link from "next/link";
 
 import Button from "@/lib/components/ui/Button";
+import { useTranslation } from "react-i18next";
 
-export const AddNewBrainButton = (): JSX.Element => (
-  <Link
-    href={"/brains-management"}
-    onClick={(event) => {
-      event.preventDefault();
-      event.stopPropagation();
-    }}
-  >
-    <Button variant={"tertiary"}>Create new brain</Button>
-  </Link>
-);
+export const AddNewBrainButton = (): JSX.Element => {
+  const { t } = useTranslation(["chat"]);
+
+  return (
+    <Link
+      href={"/brains-management"}
+      onClick={(event) => {
+        event.preventDefault();
+        event.stopPropagation();
+      }}
+    >
+      <Button variant={"tertiary"}>{t("new_brain")}</Button>
+    </Link>
+  );
+};
