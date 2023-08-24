1222e7df16ab68fb4efeeb98a8005043154d04c4
Carlos Alberto Castillo López <41802706+ccaztillol@users.noreply.github.com>
2023-08-24 12:57:53 -0600

    Can not find main.py

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,9 +1,10 @@
 import os
-
+import sys
 import pytest
 from fastapi.testclient import TestClient
-from ..main import app
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from main import app
 
 @pytest.fixture(scope="module")
 def client():
