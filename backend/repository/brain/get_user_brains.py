6a8265b74cb417832b50e28a85e5c8e9c70865c2
Carlos Alberto Castillo López <41802706+ccaztillol@users.noreply.github.com>
2023-08-24 15:30:17 -0600

    Import typing

--- backend/repository/brain/get_user_brains.py
+++ backend/repository/brain/get_user_brains.py
@@ -1,9 +1,10 @@
 from uuid import UUID
+from typing import Optional, List
 
 from models import BrainEntity, get_supabase_db
 
 
-def get_user_brains(user_id: UUID) -> list[BrainEntity]:
+def get_user_brains(user_id: UUID) -> List[BrainEntity]:
     supabase_db = get_supabase_db()
     results = supabase_db.get_user_brains(user_id)
 
