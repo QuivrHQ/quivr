28f748362ee2471c99a910867d73c813be074752
mamadoudicko <mamadoudicko100@gmail.com>
2023-08-30 12:58:03 +0200

    fix(shareBrain): prevent access duplication in db

--- backend/repository/brain/get_brain_for_user.py
+++ backend/repository/brain/get_brain_for_user.py
@@ -3,6 +3,6 @@ from uuid import UUID
 from models import MinimalBrainEntity, get_supabase_db
 
 
-def get_brain_for_user(user_id: UUID, brain_id: UUID) -> MinimalBrainEntity:
+def get_brain_for_user(user_id: UUID, brain_id: UUID) -> MinimalBrainEntity | None:
     supabase_db = get_supabase_db()
     return supabase_db.get_brain_for_user(user_id, brain_id)  # type: ignore
