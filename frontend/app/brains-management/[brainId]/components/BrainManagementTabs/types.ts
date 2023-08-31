42270cd72892d57edb0d09642bcf2fd4848e79ad
mamadoudicko <mamadoudicko100@gmail.com>
2023-08-31 15:52:01 +0200

    feat: change share brain button logic

--- frontend/app/brains-management/[brainId]/components/BrainManagementTabs/types.ts
+++ frontend/app/brains-management/[brainId]/components/BrainManagementTabs/types.ts
@@ -1,1 +1,3 @@
-export type BrainManagementTab = "settings" | "people" | "knowledge";
+export const brainManagementTabs = ["settings", "people", "knowledge"] as const;
+
+export type BrainManagementTab = (typeof brainManagementTabs)[number];
--- frontend/app/chat/[chatId]/components/ActionsBar/components/ChatInput/components/ChatBar/components/MentionInput/components/SuggestionRow.tsx
+++ frontend/app/chat/[chatId]/components/ActionsBar/components/ChatInput/components/ChatBar/components/MentionInput/components/SuggestionRow.tsx
@@ -1,8 +1,9 @@
 import { EntryComponentProps } from "@draft-js-plugins/mention/lib/MentionSuggestions/Entry/Entry";
-import { UUID } from "crypto";
+import { useRouter } from "next/navigation";
+import { MdShare } from "react-icons/md";
 
 import { MentionTriggerType } from "@/app/chat/[chatId]/components/ActionsBar/types";
-import { ShareBrain } from "@/lib/components/ShareBrain";
+import Button from "@/lib/components/ui/Button";
 
 import { BrainSuggestion } from "./BrainSuggestion";
 import { PromptSuggestion } from "./PromptSuggestion";
@@ -11,13 +12,23 @@ export const SuggestionRow = ({
   mention,
   ...otherProps
 }: EntryComponentProps): JSX.Element => {
+  const router = useRouter();
   if ((mention.trigger as MentionTriggerType) === "@") {
     return (
       <div {...otherProps}>
         <div className="relative flex group px-4">
           <BrainSuggestion content={mention.name} />
           <div className="absolute right-0 flex flex-row">
-            <ShareBrain brainId={mention.id as UUID} />
+            <Button
+              className="group-hover:visible invisible hover:text-red-500 transition-[colors,opacity] p-1"
+              onClick={() =>
+                router.push(`/brains-management/${mention.id as string}#people`)
+              }
+              variant={"tertiary"}
+              data-testId="share-brain-button"
+            >
+              <MdShare className="text-xl" />
+            </Button>
           </div>
         </div>
       </div>
