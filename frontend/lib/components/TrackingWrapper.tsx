bea275f2ff35c2ac0731b246aea676916a0d4da8
Mamadou DICKO <63923024+mamadoudicko@users.noreply.github.com>
2023-09-01 15:14:35 +0200

    feat: add tanstack query and optimistic fetch on brains settings page (#1087)
    * refactor: remove <TrackingWrapper />
    
    * feat: add tanstack query
    
    * feat: add optimistic fetch to brain settings page

--- frontend/lib/components/TrackingWrapper.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { usePageTracking } from "@/services/analytics/usePageTracking";
-
-export const TrackingWrapper = (): JSX.Element => {
-  usePageTracking();
-
-  return <></>;
-};
