bccbc320ddb74d39e90e7ca7a4222656ac396694
mamadoudicko <mamadoudicko100@gmail.com>
2023-09-01 12:36:22 +0200

    feat: add tanstack query

--- frontend/app/App.tsx
+++ frontend/app/App.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren, useEffect } from "react";
 
 import Footer from "@/lib/components/Footer";
@@ -9,6 +10,8 @@ import { useSupabase } from "@/lib/context/SupabaseProvider";
 import { UpdateMetadata } from "@/lib/helpers/updateMetadata";
 import { usePageTracking } from "@/services/analytics/usePageTracking";
 import "../lib/config/LocaleConfig/i18n";
+
+const queryClient = new QueryClient();
 
 // This wrapper is used to make effect calls at a high level in app rendering.
 export const App = ({ children }: PropsWithChildren): JSX.Element => {
@@ -25,11 +28,11 @@ export const App = ({ children }: PropsWithChildren): JSX.Element => {
   }, [session?.user]);
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <NavBar />
       <div className="flex-1">{children}</div>
       <Footer />
       <UpdateMetadata />
-    </>
+    </QueryClientProvider>
   );
 };
