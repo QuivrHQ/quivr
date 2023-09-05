fe8d3bc1e71867fa4952d5d2920a58c166471168
mamadoudicko <mamadoudicko100@gmail.com>
2023-09-05 14:29:31 +0200

    feat(Crawler): replace submit button with send icon

--- frontend/app/chat/[chatId]/components/ActionsBar/components/Feed/components/Crawler/index.tsx
+++ frontend/app/chat/[chatId]/components/ActionsBar/components/Feed/components/Crawler/index.tsx
@@ -1,16 +1,13 @@
 "use client";
 import { useTranslation } from "react-i18next";
+import { MdSend } from "react-icons/md";
 
-import Button from "@/lib/components/ui/Button";
-import Card from "@/lib/components/ui/Card";
 import Field from "@/lib/components/ui/Field";
-import { useBrainContext } from "@/lib/context/BrainProvider/hooks/useBrainContext";
 
 import { useCrawler } from "./hooks/useCrawler";
 
 export const Crawler = (): JSX.Element => {
-  const { urlInputRef, isCrawling, crawlWebsite } = useCrawler();
-  const { currentBrain } = useBrainContext();
+  const { urlInputRef } = useCrawler();
 
   const { t } = useTranslation(["translation", "upload"]);
 
@@ -19,7 +16,7 @@ export const Crawler = (): JSX.Element => {
       <div className="flex justify-center gap-5 px-6">
         <div className="max-w-xl w-full">
           <div className="flex-col justify-center gap-5">
-            <Card className="h-32 flex gap-5 justify-center items-center px-5">
+            <div className="h-32 flex gap-5 justify-center items-center px-5">
               <div className="text-center max-w-sm w-full flex flex-col gap-5 items-center">
                 <Field
                   name="crawlurl"
@@ -27,17 +24,10 @@ export const Crawler = (): JSX.Element => {
                   type="text"
                   placeholder={t("webSite", { ns: "upload" })}
                   className="w-full"
+                  icon={<MdSend />}
                 />
               </div>
-              <div className="flex flex-col items-center justify-center gap-5">
-                <Button
-                  isLoading={isCrawling}
-                  onClick={() => void crawlWebsite(currentBrain?.id)}
-                >
-                  {t("crawlButton")}
-                </Button>
-              </div>
-            </Card>
+            </div>
           </div>
         </div>
       </div>
