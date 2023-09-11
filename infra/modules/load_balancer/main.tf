92cc706c995abebb61cb9e5372e50c1c0a11bc9b
Stan Girard <girard.stanislas@gmail.com>
2023-09-11 13:50:57 +0200

    feat(terraform): added https certificate generation

--- infra/modules/load_balancer/main.tf
+++ infra/modules/load_balancer/main.tf
@@ -2,6 +2,32 @@ resource "aws_route53_zone" "this" {
   name = var.api_domain_name
 }
 
+resource "aws_acm_certificate" "this" {
+  domain_name       = var.api_domain_name
+  validation_method = "DNS"
+}
+
+resource "aws_route53_record" "validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  name    = each.value.name
+  records = [each.value.record]
+  ttl     = 300
+  type    = each.value.type
+  zone_id = aws_route53_zone.this.zone_id
+}
+
+resource "aws_acm_certificate_validation" "this" {
+  certificate_arn         = aws_acm_certificate.this.arn
+  validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
+}
+
 
 resource "aws_lb" "this" {
   name               = "quivr"
--- infra/moves.tf
+++ infra/moves.tf
@@ -26,3 +26,13 @@ import {
   to = module.api.aws_route53_record.this
   id = "Z075975614NR7YUNW2MOK_api.quivr.app_A"
 }
+
+import {
+  to = module.api.aws_acm_certificate.this
+  id = "arn:aws:acm:eu-west-3:253053805092:certificate/b553db94-a32a-43a3-ab1d-cb141ea79d87"
+}
+
+import {
+  to = module.api.aws_route53_record.validation["api.quivr.app"]
+  id = "Z075975614NR7YUNW2MOK__44e3506665204f319e6c4b8b63b840f0.api.quivr.app_CNAME"
+}
