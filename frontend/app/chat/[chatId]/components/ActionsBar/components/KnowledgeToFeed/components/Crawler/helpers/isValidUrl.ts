5ac3740078ab4983550c135ff7f5424c8f0615d0
mamadoudicko <mamadoudicko100@gmail.com>
2023-09-12 17:31:35 +0200

    feat: update isValidUrl function

--- frontend/app/chat/[chatId]/components/ActionsBar/components/KnowledgeToFeed/components/Crawler/helpers/isValidUrl.ts
+++ frontend/app/chat/[chatId]/components/ActionsBar/components/KnowledgeToFeed/components/Crawler/helpers/isValidUrl.ts
@@ -1,13 +1,9 @@
-export const isValidUrl = (urlString: string): boolean => {
-  const urlPattern = new RegExp(
-    "^(https?:\\/\\/)?" + // validate protocol
-      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // validate domain name
-      "((\\d{1,3}\\.){3}\\d{1,3}))" + // validate OR ip (v4) address
-      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // validate port and path
-      "(\\?[;&a-z\\d%_.~+=-]*)?" + // validate query string
-      "(\\#[-a-z\\d_]*)?$",
-    "i"
-  ); // validate fragment locator
+export const isValidUrl = (url: string): boolean => {
+  try {
+    new URL(url);
 
-  return !!urlPattern.test(urlString);
+    return true;
+  } catch (err) {
+    return false;
+  }
 };
