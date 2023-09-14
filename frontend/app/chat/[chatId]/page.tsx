223d3d91023a0401a4ed427959a32550d7175c40
Stan Girard <girard.stanislas@gmail.com>
2023-09-15 01:09:26 +0200

    feat(frontend): responsiveness (#1174)
    * feat: 🎸 responsiveness chat
    
    * feat: 🎸 responsive
    
    added responsive menu
    
    * feat: 🎸 responsive
    
    brain management
    
    * fix: 🐛 docker
    
    prod

--- frontend/app/chat/[chatId]/page.tsx
+++ frontend/app/chat/[chatId]/page.tsx
@@ -6,17 +6,17 @@ import { ChatHeader } from "./components/ChatHeader";
 
 const SelectedChatPage = (): JSX.Element => {
   return (
-    <main className="flex flex-col w-full pt-10" data-testid="chat-page">
-      <section className="flex flex-col flex-1 items-center w-full h-full min-h-[70vh]">
-        <ChatHeader />
-        <div className="flex-1 flex flex-col mt-8 w-full shadow-md dark:shadow-primary/25 hover:shadow-xl transition-shadow rounded-xl overflow-hidden bg-white dark:bg-black border border-black/10 dark:border-white/25 p-12 pt-10 max-h-[80vh]">
-          <div className="flex flex-1 flex-col overflow-hidden">
-            <ChatDialogueArea />
-          </div>
-          <ActionsBar />
+    <main className="flex flex-col w-full h-[calc(100vh-61px)] overflow-hidden" data-testid="chat-page">
+    <section className="flex flex-col flex-1 items-center w-full h-full overflow-y-auto">
+        <ChatHeader /> {/* Added margin-bottom */}
+        <div className="flex-1 flex flex-col mt-4 md:mt-8 w-full shadow-md dark:shadow-primary/25 hover:shadow-xl transition-shadow rounded-xl overflow-hidden bg-white dark:bg-black border border-black/10 dark:border-white/25 p-2 md:p-12 pt-4 md:pt-10">
+            <div className="flex flex-1 flex-col overflow-y-auto">
+                <ChatDialogueArea />
+            </div>
+            <ActionsBar/> {/* Added margin-top */}
         </div>
-      </section>
-    </main>
+    </section>
+  </main>
   );
 };
 
