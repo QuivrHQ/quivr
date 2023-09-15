ac9333682ba999544cacd9d710060a8c11887049
gozineb <zinebe@theodo.fr>
2023-09-15 10:49:35 +0200

    🌱 list files in storage & generate signed URL

---
+++ backend/repository/files/list_files.py
@@ -0,0 +1,17 @@
+from multiprocessing import get_logger
+
+from models import get_supabase_client
+from supabase.client import Client
+
+logger = get_logger()
+
+
+def list_files_from_storage(path):
+    supabase_client: Client = get_supabase_client()
+
+    try:
+        response = supabase_client.storage.from_("quivr").list(path)
+        logger.info("RESPONSE", response)
+        return response
+    except Exception as e:
+        logger.error(e)
