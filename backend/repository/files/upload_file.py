5f7232ce9f51cf61c5224a8e971ddfc44e5a0dbb
Stan Girard <girard.stanislas@gmail.com>
2023-09-15 15:51:06 +0200

    feat(upload): changed to task

--- backend/repository/files/upload_file.py
+++ backend/repository/files/upload_file.py
@@ -1,6 +1,9 @@
+import json
 from multiprocessing import get_logger
 
 from httpx import Response
+from langchain.pydantic_v1 import Field
+from langchain.schema import Document
 from models import get_supabase_client
 from supabase.client import Client
 
@@ -19,3 +22,35 @@ def upload_file_storage(file, file_identifier: str) -> Response:
         logger.error(e)
         print(e)
         return response
+
+
+class DocumentSerializable(Document):
+    """Class for storing a piece of text and associated metadata."""
+
+    page_content: str
+    metadata: dict = Field(default_factory=dict)
+
+    @property
+    def lc_serializable(self) -> bool:
+        return True
+
+    def __repr__(self):
+        return f"Document(page_content='{self.page_content[:50]}...', metadata={self.metadata})"
+
+    def __str__(self):
+        return self.__repr__()
+
+    def to_json(self) -> str:
+        """Convert the Document object to a JSON string."""
+        return json.dumps(
+            {
+                "page_content": self.page_content,
+                "metadata": self.metadata,
+            }
+        )
+
+    @classmethod
+    def from_json(cls, json_str: str):
+        """Create a Document object from a JSON string."""
+        data = json.loads(json_str)
+        return cls(page_content=data["page_content"], metadata=data["metadata"])
