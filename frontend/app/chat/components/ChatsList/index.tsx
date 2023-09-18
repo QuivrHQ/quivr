8914c7c3573a2f2047df80718fca1e6e9721fefa
Stan Girard <girard.stanislas@gmail.com>
2023-09-18 18:25:28 +0200

    fix(chats): now in order and with a little bonus ;) (#1200)
    * fix(chats): now in order and with a little bonus ;)
    
    * style(eslint): fixed

--- frontend/app/chat/components/ChatsList/index.tsx
+++ frontend/app/chat/components/ChatsList/index.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable complexity */
 "use client";
 import { motion, MotionConfig } from "framer-motion";
 import { MdChevronRight } from "react-icons/md";
 
 import { useChatsContext } from "@/lib/context/ChatsProvider/hooks/useChatsContext";
-import { cn } from "@/lib/utils";
+import { cn, isToday, isWithinLast30Days, isWithinLast7Days, isYesterday } from "@/lib/utils";
 
 import { ChatsListItem } from "./components/ChatsListItem";
 import { MiniFooter } from "./components/ChatsListItem/components/MiniFooter";
@@ -15,6 +16,13 @@ export const ChatsList = (): JSX.Element => {
   const { allChats } = useChatsContext();
   const { open, setOpen } = useChatsList();
   useSelectedChatPage();
+
+ 
+  // Filtering chats into different groups
+  const todayChats = allChats.filter(chat => isToday(new Date(chat.creation_time)));
+  const yesterdayChats = allChats.filter(chat => isYesterday(new Date(chat.creation_time)));
+  const last7DaysChats = allChats.filter(chat => isWithinLast7Days(new Date(chat.creation_time)));
+  const last30DaysChats = allChats.filter(chat => isWithinLast30Days(new Date(chat.creation_time)));
 
   return (
     <MotionConfig transition={{ mass: 1, damping: 10 }}>
@@ -48,9 +56,26 @@ export const ChatsList = (): JSX.Element => {
               data-testid="chats-list-items"
               className="flex-1 overflow-auto scrollbar h-full"
             >
-              {allChats.map((chat) => (
+              {todayChats.length > 0 && <div className="bg-gray-100 text-black rounded-md px-3 py-1 mt-2">Today</div>}
+              {todayChats.map((chat) => (
                 <ChatsListItem key={chat.chat_id} chat={chat} />
               ))}
+
+              {yesterdayChats.length > 0 && <div className="bg-gray-100 text-black rounded-md px-3 py-1 mt-2">Yesterday</div>}
+              {yesterdayChats.map((chat) => (
+                <ChatsListItem key={chat.chat_id} chat={chat} />
+              ))}
+
+              {last7DaysChats.length > 0 && <div className="bg-gray-100 text-black rounded-md px-3 py-1 mt-2">Previous 7 Days</div>}
+              {last7DaysChats.map((chat) => (
+                <ChatsListItem key={chat.chat_id} chat={chat} />
+              ))}
+
+              {last30DaysChats.length > 0 && <div className="bg-gray-100 text-black rounded-md px-3 py-1 mt-2">Previous 30 Days</div>}
+              {last30DaysChats.map((chat) => (
+                <ChatsListItem key={chat.chat_id} chat={chat} />
+              ))}
+
             </div>
             <MiniFooter />
           </div>
