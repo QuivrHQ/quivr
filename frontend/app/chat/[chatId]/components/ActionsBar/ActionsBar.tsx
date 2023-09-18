c0ce14a5fbf77b96ed9322b9f1e730bf7e47d4d5
mamadoudicko <mamadoudicko100@gmail.com>
2023-09-18 17:30:37 +0200

    feat: remove unrelevant toast

--- frontend/app/chat/[chatId]/components/ActionsBar/ActionsBar.tsx
+++ frontend/app/chat/[chatId]/components/ActionsBar/ActionsBar.tsx
@@ -12,11 +12,12 @@ export const ActionsBar = (): JSX.Element => {
     hasPendingRequests,
     setHasPendingRequests,
   } = useActionBar();
-  
-  const { addContent, contents, feedBrain, removeContent } = useKnowledgeUploader({
-    setHasPendingRequests,
-    setShouldDisplayUploadCard,
-  });
+
+  const { addContent, contents, feedBrain, removeContent } =
+    useKnowledgeUploader({
+      setHasPendingRequests,
+      setShouldDisplayUploadCard,
+    });
 
   const { t } = useTranslation(["chat"]);
 
@@ -25,13 +26,17 @@ export const ActionsBar = (): JSX.Element => {
       {hasPendingRequests && (
         <div className="flex mt-1 flex-col md:flex-row w-full shadow-md dark:shadow-primary/25 hover:shadow-xl transition-shadow rounded-xl bg-white dark:bg-black border border-black/10 dark:border-white/25 p-2 md:p-6 pl-6">
           <div className="flex flex-1 items-center mb-2 md:mb-0">
-            <span className="text-sm md:text-1xl">{t("filesUploading")}</span>
+            <span className="text-sm md:text-1xl">{t("feedingBrain")}</span>
           </div>
           <AiOutlineLoading3Quarters className="animate-spin text-2xl md:text-3xl self-center" />
         </div>
       )}
 
-      <div className={shouldDisplayUploadCard ? "h-full flex flex-col flex-auto" : ""}>
+      <div
+        className={
+          shouldDisplayUploadCard ? "h-full flex flex-col flex-auto" : ""
+        }
+      >
         {shouldDisplayUploadCard && (
           <div className="flex flex-1 overflow-y-scroll shadow-md dark:shadow-primary/25 hover:shadow-xl transition-shadow rounded-xl bg-white dark:bg-black border border-black/10 dark:border-white/25 p-4 md:p-6">
             <KnowledgeToFeed
