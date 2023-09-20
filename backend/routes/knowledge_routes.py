ca16f0ef8bfba52dd59d14a3f265ba56c4566171
gozineb <zinebe@theodo.fr>
2023-09-20 11:56:33 +0200

    ✨ add remove knowledge and update router

--- backend/routes/knowledge_routes.py
+++ backend/routes/knowledge_routes.py
@@ -4,8 +4,11 @@ from auth import AuthBearer, get_current_user
 from fastapi import APIRouter, Depends, Query
 from logger import get_logger
 from models import Brain, UserIdentity, get_supabase_db
+from repository.files.delete_file import delete_file_from_storage
 from repository.files.generate_file_signed_url import generate_file_signed_url
-from repository.files.list_files import list_files_from_storage
+from repository.knowledge.get_all_knowledge import get_all_knowledge
+from repository.knowledge.get_knowledge import get_knowledge
+from repository.knowledge.remove_knowledge import remove_knowledge
 from routes.authorizations.brain_authorization import (
     RoleEnum,
     has_brain_authorization,
@@ -31,20 +34,22 @@ async def list_knowledge_in_brain_endpoint(
 
     brain = Brain(id=brain_id)
 
-    files = list_files_from_storage(str(brain_id))
-    logger.info("List of files from storage", files)
+    # files = list_files_from_storage(str(brain_id))
+    # logger.info("List of files from storage", files)
 
+    knowledges = get_all_knowledge(brain_id)
+    logger.info("List of knowledge from knowledge table", knowledges)
     # TO DO: Retrieve from Knowledge table instead of storage or vectors
     unique_data = brain.get_unique_brain_files()
 
     print("UNIQUE DATA", unique_data)
     unique_data.sort(key=lambda x: int(x["size"]), reverse=True)
 
-    return {"documents": unique_data}
+    return {"knowledges": knowledges}
 
 
 @knowledge_router.delete(
-    "/knowledge/{file_name}/",
+    "/knowledge/{knowledge_id}/",
     dependencies=[
         Depends(AuthBearer()),
         Depends(has_brain_authorization(RoleEnum.Owner)),
@@ -52,18 +57,28 @@ async def list_knowledge_in_brain_endpoint(
     tags=["Knowledge"],
 )
 async def delete_endpoint(
-    file_name: str,
+    knowledge_id: UUID,
     current_user: UserIdentity = Depends(get_current_user),
     brain_id: UUID = Query(..., description="The ID of the brain"),
 ):
     """
-    Delete a specific user file by file name.
+    Delete a specific knowledge from a brain.
     """
 
     validate_brain_authorization(brain_id=brain_id, user_id=current_user.id)
 
     brain = Brain(id=brain_id)
-    brain.delete_file_from_brain(file_name)
+
+    knowledge = get_knowledge(knowledge_id)
+    file_name = knowledge.file_name if knowledge.file_name else knowledge.url
+    message = remove_knowledge(knowledge_id)
+
+    print("MESSAGE to remove", message)
+    if knowledge.file_name:
+        delete_file_from_storage(f"{brain_id}/{knowledge.file_name}")
+        brain.delete_file_from_brain(knowledge.file_name)
+    elif knowledge.url:
+        brain.delete_file_from_brain(knowledge.url)
 
     return {
         "message": f"{file_name} of brain {brain_id} has been deleted by user {current_user.email}."
