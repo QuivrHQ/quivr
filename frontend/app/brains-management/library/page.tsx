5fde3b7ab89e097d8ee2a57b11e9a750f6cb67d8
mamadoudicko <mamadoudicko100@gmail.com>
2023-09-20 12:00:16 +0200

    feat: add brain library button

---
+++ frontend/app/brains-management/library/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+const BrainsLibrary = (): JSX.Element => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <h1>Brains Library</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BrainsLibrary;
