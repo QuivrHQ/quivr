30465c4fd6101d1e39df7eda1c56f1a9f9c64843
mamadoudicko <mamadoudicko100@gmail.com>
2023-09-25 16:52:31 +0200

    feat: update useSettingsTab add usebrainFormState and useSettings tab

---
+++ frontend/app/brains-management/[brainId]/components/BrainManagementTabs/components/SettingsTab/hooks/useBrainFormState.ts
@@ -0,0 +1,55 @@
+import { UUID } from "crypto";
+import { useForm } from "react-hook-form";
+
+import { defaultBrainConfig } from "@/lib/config/defaultBrainConfig";
+import { useBrainContext } from "@/lib/context/BrainProvider/hooks/useBrainContext";
+import { BrainConfig } from "@/lib/types/brainConfig";
+
+import { useBrainFetcher } from "../../../hooks/useBrainFetcher";
+
+type UseBrainFormStateProps = {
+  brainId: UUID;
+};
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const useBrainFormState = ({ brainId }: UseBrainFormStateProps) => {
+  const { defaultBrainId } = useBrainContext();
+
+  const {
+    register,
+    getValues,
+    watch,
+    setValue,
+    reset,
+    formState: { dirtyFields },
+  } = useForm<BrainConfig>({
+    defaultValues: { ...defaultBrainConfig, status: undefined },
+  });
+  const { brain } = useBrainFetcher({
+    brainId,
+  });
+
+  const isDefaultBrain = defaultBrainId === brainId;
+  const promptId = watch("prompt_id");
+  const openAiKey = watch("openAiKey");
+  const model = watch("model");
+  const temperature = watch("temperature");
+  const maxTokens = watch("maxTokens");
+  const status = watch("status");
+
+  return {
+    brain,
+    model,
+    temperature,
+    maxTokens,
+    isDefaultBrain,
+    promptId,
+    openAiKey,
+    dirtyFields,
+    status,
+    register,
+    getValues,
+    setValue,
+    reset,
+  };
+};
