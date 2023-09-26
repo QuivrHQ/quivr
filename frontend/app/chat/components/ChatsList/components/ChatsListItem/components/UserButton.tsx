88fa2c39e7d9a1f9b57d3ddf9162a1dc87f789d1
Matthieu Jacq <67386567+matthieujacq@users.noreply.github.com>
2023-09-26 15:37:48 +0200

    Display email in the user button

--- frontend/app/chat/components/ChatsList/components/ChatsListItem/components/UserButton.tsx
+++ frontend/app/chat/components/ChatsList/components/ChatsListItem/components/UserButton.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link";
 import { MdPerson } from "react-icons/md";
 
+import { useUserData } from "@/lib/hooks/useUserData";
+
 import { sidebarLinkStyle } from "../styles/SidebarLinkStyle";
 
 export const UserButton = (): JSX.Element => {
+  const { userData } = useUserData();
+
   return (
     <Link aria-label="account" className={sidebarLinkStyle} href={"/user"}>
       <MdPerson className="text-4xl" />
-      <span>Account</span>
+      <span>{userData?.email}</span>
     </Link>
   );
 };
