5dbb9badf95cce7c32393453d4bf6122f8a4a41b
Matthieu Jacq <67386567+matthieujacq@users.noreply.github.com>
2023-09-27 10:40:49 +0200

    ✨ Implement gravatar

--- frontend/app/chat/components/ChatsList/components/ChatsListItem/components/UserButton.tsx
+++ frontend/app/chat/components/ChatsList/components/ChatsListItem/components/UserButton.tsx
@@ -1,16 +1,40 @@
+import { createHash } from "crypto";
+import Image from "next/image";
 import Link from "next/link";
-import { MdPerson } from "react-icons/md";
 
 import { useSupabase } from "@/lib/context/SupabaseProvider";
 
 import { sidebarLinkStyle } from "../styles/SidebarLinkStyle";
+
+// Gravatar images may be requested just like a normal image, using an IMG tag. To get an image specific to a user, you must first calculate their email hash.
+// The most basic image request URL looks like this:
+// https://www.gravatar.com/avatar/HASH
+
+const computeHash = (email = "") => {
+  return createHash("md5")
+    .update(typeof email === "string" ? email.trim().toLowerCase() : "")
+    .digest("hex");
+};
+
+const getGravatarUrl = (email?: string) => {
+  const hash = computeHash(email);
+
+  return `https://www.gravatar.com/avatar/${hash}?d=mp`;
+};
 
 export const UserButton = (): JSX.Element => {
   const { session } = useSupabase();
 
   return (
     <Link aria-label="account" className={sidebarLinkStyle} href={"/user"}>
-      <MdPerson className="text-4xl" />
+      <div className="relative w-8 h-8">
+        <Image
+          alt="gravatar"
+          layout="fill"
+          src={getGravatarUrl(session?.user.email)}
+          className="rounded-xl"
+        />
+      </div>
       <span className="text-ellipsis overflow-hidden">
         {session?.user.email ?? ""}
       </span>
