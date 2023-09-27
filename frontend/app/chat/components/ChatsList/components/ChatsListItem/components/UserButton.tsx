443c13b2bc5690bcb5a6de55496fcab247d93bc9
Matthieu Jacq <67386567+matthieujacq@users.noreply.github.com>
2023-09-27 11:09:27 +0200

    ♻️  refact gravatar url generation with a custom hook

--- frontend/app/chat/components/ChatsList/components/ChatsListItem/components/UserButton.tsx
+++ frontend/app/chat/components/ChatsList/components/ChatsListItem/components/UserButton.tsx
@@ -1,29 +1,14 @@
-import { createHash } from "crypto";
 import Image from "next/image";
 import Link from "next/link";
 
 import { useSupabase } from "@/lib/context/SupabaseProvider";
 
+import { useGravatar } from "../hooks/useGravatar";
 import { sidebarLinkStyle } from "../styles/SidebarLinkStyle";
-
-// Gravatar images may be requested just like a normal image, using an IMG tag. To get an image specific to a user, you must first calculate their email hash.
-// The most basic image request URL looks like this:
-// https://www.gravatar.com/avatar/HASH
-
-const computeHash = (email = "") => {
-  return createHash("md5")
-    .update(typeof email === "string" ? email.trim().toLowerCase() : "")
-    .digest("hex");
-};
-
-const getGravatarUrl = (email?: string) => {
-  const hash = computeHash(email);
-
-  return `https://www.gravatar.com/avatar/${hash}?d=mp`;
-};
 
 export const UserButton = (): JSX.Element => {
   const { session } = useSupabase();
+  const { gravatarUrl } = useGravatar();
 
   return (
     <Link aria-label="account" className={sidebarLinkStyle} href={"/user"}>
@@ -31,7 +16,7 @@ export const UserButton = (): JSX.Element => {
         <Image
           alt="gravatar"
           layout="fill"
-          src={getGravatarUrl(session?.user.email)}
+          src={gravatarUrl}
           className="rounded-xl"
         />
       </div>
