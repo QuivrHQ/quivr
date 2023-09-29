625a9db17b4a9be4badeb858bec14bbe54846494
mamadoudicko <mamadoudicko100@gmail.com>
2023-09-29 11:12:15 +0200

    fix: update createBrain tests

--- frontend/app/chat/components/ChatsList/components/ChatsListItem/components/BrainManagementButton.tsx
+++ frontend/app/chat/components/ChatsList/components/ChatsListItem/components/BrainManagementButton.tsx
@@ -13,6 +13,7 @@ export const BrainManagementButton = (): JSX.Element => {
     <Link
       href={`/brains-management/${currentBrainId ?? ""}`}
       className={sidebarLinkStyle}
+      data-testid="brain-management-button"
     >
       <FaBrain className="w-8 h-8" />
       <span>{t("myBrains")}</span>
