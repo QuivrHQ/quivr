92494aa6c6d1ce08373b8f10a9fa07ecd354fb91
Nick Guernsey <64866427+nguernse@users.noreply.github.com>
2023-10-02 02:33:18 -0500

    fix(Select): fix JSX typo (#1292)
    * fix: fixed clipRule typo
    
    * fix: fill-rule typo

--- frontend/lib/components/ui/Select.tsx
+++ frontend/lib/components/ui/Select.tsx
@@ -94,9 +94,9 @@ export const Select = <T extends string | number>({
                   aria-hidden="true"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   />
                 </svg>
               </span>
