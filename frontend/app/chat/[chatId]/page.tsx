5b2e396a308c07533467be8b3f57574d960d4a10
Matthieu Jacq <67386567+matthieujacq@users.noreply.github.com>
2023-10-02 17:23:42 +0200

    🔥 Remove Chat page intro (component and translations)

--- frontend/app/chat/[chatId]/page.tsx
+++ frontend/app/chat/[chatId]/page.tsx
@@ -5,7 +5,6 @@ import { useCustomDropzone } from "@/lib/hooks/useDropzone";
 
 import { ActionsBar } from "./components/ActionsBar";
 import { ChatDialogueArea } from "./components/ChatDialogueArea/ChatDialogue";
-import { ChatHeader } from "./components/ChatHeader";
 
 const SelectedChatPage = (): JSX.Element => {
   const { getRootProps } = useCustomDropzone();
@@ -18,7 +17,6 @@ const SelectedChatPage = (): JSX.Element => {
       {...getRootProps()}
     >
       <section className="flex flex-col flex-1 items-center w-full h-full overflow-y-auto">
-        <ChatHeader />
         <div
           className={`flex-1 flex flex-col mt-4 md:mt-8 w-full shadow-md dark:shadow-primary/25 hover:shadow-xl transition-shadow rounded-xl overflow-hidden dark:bg-black border border-black/10 dark:border-white/25 p-2 md:p-12 pt-4 md:pt-10   ${
             shouldDisplayFeedCard ? "bg-chat-bg-gray" : "bg-white"
