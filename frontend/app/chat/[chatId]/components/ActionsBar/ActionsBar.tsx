eee155358c67a141e8a28586f385e1ada6010fd2
mamadoudicko <mamadoudicko100@gmail.com>
2023-10-03 17:42:13 +0200

    refactor: update ChatInput

--- frontend/app/chat/[chatId]/components/ActionsBar/ActionsBar.tsx
+++ frontend/app/chat/[chatId]/components/ActionsBar/ActionsBar.tsx
@@ -42,9 +42,7 @@ export const ActionsBar = (): JSX.Element => {
           </AnimatePresence>
         )}
         {!shouldDisplayFeedCard && (
-          <div className="flex mt-1 flex-col w-full shadow-md dark:shadow-primary/25 hover:shadow-xl transition-shadow rounded-xl bg-white dark:bg-black border border-black/10 dark:border-white/25 p-2">
-            <ChatInput shouldDisplayFeedCard={shouldDisplayFeedCard} />
-          </div>
+          <ChatInput shouldDisplayFeedCard={shouldDisplayFeedCard} />
         )}
       </div>
     </>
