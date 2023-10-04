b5a3554f31ca48d07c5c2be039e429ad357874f4
mamadoudicko <mamadoudicko100@gmail.com>
2023-10-04 12:16:19 +0200

    feat: add <Step3/> content

--- frontend/app/chat/[chatId]/components/ChatDialogueArea/components/ChatDialogue/components/Onboarding/components/Step3.tsx
+++ frontend/app/chat/[chatId]/components/ChatDialogueArea/components/ChatDialogue/components/Onboarding/components/Step3.tsx
@@ -1,8 +1,12 @@
 import { Fragment } from "react";
+import { useTranslation } from "react-i18next";
 
 import { useOnboardingContext } from "@/lib/hooks/useOnboardingContext";
 
+import { MessageRow } from "../../QADisplay";
 import { checkIfShouldDisplayStep } from "../helpers/checkIfShouldDisplayStep";
+import { useStreamText } from "../hooks/useStreamText";
+import { stepsContainerStyle } from "../styles";
 
 export const Step3 = (): JSX.Element => {
   const { currentStep } = useOnboardingContext();
@@ -11,9 +15,31 @@ export const Step3 = (): JSX.Element => {
     step: "UPLOADED",
   });
 
+  const { t } = useTranslation(["chat"]);
+  const firstMessage = t("onboarding.last_step");
+  const secondMessageStream = t("onboarding.ask_question_to_file");
+
+  const { streamingText: streamingAssistantMessage, isDone: isAssistantDone } =
+    useStreamText({
+      text: firstMessage,
+      enabled: shouldStepBeDisplayed,
+    });
+
+  const { streamingText: firstMessageStream } = useStreamText({
+    text: secondMessageStream,
+    enabled: isAssistantDone && shouldStepBeDisplayed,
+  });
+
   if (!shouldStepBeDisplayed) {
     return <Fragment />;
   }
 
-  return <p>hello</p>;
+  return (
+    <MessageRow speaker={"assistant"} brainName={"Quivr"}>
+      <div className={stepsContainerStyle}>
+        <p>{streamingAssistantMessage}</p>
+        <p>{firstMessageStream}</p>
+      </div>
+    </MessageRow>
+  );
 };
