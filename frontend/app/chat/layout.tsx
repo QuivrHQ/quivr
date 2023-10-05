9834250cd43f3bf4d3c52c45e83963ce91ea56a0
mamadoudicko <mamadoudicko100@gmail.com>
2023-10-05 17:20:20 +0200

    feat: remove onboarding context

--- frontend/app/chat/layout.tsx
+++ frontend/app/chat/layout.tsx
@@ -3,7 +3,6 @@ import { ReactNode } from "react";
 
 import { ChatProvider, KnowledgeToFeedProvider } from "@/lib/context";
 import { ChatsProvider } from "@/lib/context/ChatsProvider/chats-provider";
-import { OnboardingContextProvider } from "@/lib/context/OnboardingContext/knowledgeToFeed-provider";
 import { useSupabase } from "@/lib/context/SupabaseProvider";
 import { redirectToLogin } from "@/lib/router/redirectToLogin";
 
@@ -24,12 +23,10 @@ const Layout = ({ children }: LayoutProps): JSX.Element => {
     <KnowledgeToFeedProvider>
       <ChatsProvider>
         <ChatProvider>
-          <OnboardingContextProvider>
-            <div className="relative h-full w-full flex justify-stretch items-stretch">
-              <ChatsList />
-              {children}
-            </div>
-          </OnboardingContextProvider>
+          <div className="relative h-full w-full flex justify-stretch items-stretch">
+            <ChatsList />
+            {children}
+          </div>
         </ChatProvider>
       </ChatsProvider>
     </KnowledgeToFeedProvider>
