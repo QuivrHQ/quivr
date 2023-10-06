961d689e1bfa35877bb43541e398c48f2904b197
mamadoudicko <mamadoudicko100@gmail.com>
2023-10-06 12:34:18 +0200

    test: test unplug chat

---
+++ frontend/e2e/tests/chat/utils/testUnplugChat.ts
@@ -0,0 +1,20 @@
+import { Page } from "@playwright/test";
+import { getEditor } from "./getEditor";
+
+export const testUnplugChat = async (page: Page): Promise<void> => {
+  await page.getByTestId("remove-mention").click();
+  await page.getByTestId("mention-input").isHidden();
+
+  const randomMessage = Math.random().toString(36).substring(7);
+
+  const editor = getEditor(page);
+
+  await editor.fill(randomMessage);
+
+  await page.getByTestId("submit-button").click();
+
+  await page
+    .getByTestId("chat-message-text")
+    .getByText(`${randomMessage}`)
+    .isVisible();
+};
