c4ae896de8f26530703a9bc12bdd06b415f9a889
mamadoudicko <mamadoudicko100@gmail.com>
2023-10-09 14:54:03 +0200

    chore: rename upload-button to feed-button

--- frontend/e2e/tests/crawl.ts
+++ frontend/e2e/tests/crawl.ts
@@ -5,7 +5,7 @@ import { login } from "../utils/login";
 export const crawlTests = (): void => {
   test("it should be able to add url to crawl", async ({ page }) => {
     await login(page);
-    await page.getByTestId("upload-button").click();
+    await page.getByTestId("feed-button").click();
     await page.getByTestId("feed-card").isVisible();
     await page.getByTestId("urlToCrawlInput").click();
     await page.getByTestId("urlToCrawlInput").fill("https://quivr.app");
