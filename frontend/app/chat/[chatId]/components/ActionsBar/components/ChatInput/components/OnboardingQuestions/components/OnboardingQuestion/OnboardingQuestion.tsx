38a8ab47ab4732887fb00fac1618c739d9af513f
mamadoudicko <mamadoudicko100@gmail.com>
2023-10-09 18:18:54 +0200

    feat(suggestions): submit question and update onboarding status on click

--- frontend/app/chat/[chatId]/components/ActionsBar/components/ChatInput/components/OnboardingQuestions/components/OnboardingQuestion/OnboardingQuestion.tsx
+++ frontend/app/chat/[chatId]/components/ActionsBar/components/ChatInput/components/OnboardingQuestions/components/OnboardingQuestion/OnboardingQuestion.tsx
@@ -1,10 +1,8 @@
 import { Fragment } from "react";
-import { useTranslation } from "react-i18next";
 
 import { useOnboarding } from "@/lib/hooks/useOnboarding";
 
 import { useOnboardingQuestion } from "./hooks/useOnboardingQuestion";
-import { questionIdToTradPath } from "./utils";
 import { QuestionId } from "../../types";
 
 type OnboardingQuestionsProps = {
@@ -15,21 +13,18 @@ export const OnboardingQuestion = ({
   questionId,
 }: OnboardingQuestionsProps): JSX.Element => {
   const { onboarding } = useOnboarding();
-  const { t } = useTranslation("chat");
-  const { handleSuggestionClick } = useOnboardingQuestion(questionId);
+  const { handleSuggestionClick, question } = useOnboardingQuestion(questionId);
 
   if (!onboarding[questionId]) {
     return <Fragment />;
   }
-
-  const onboardingStep = questionIdToTradPath[questionId];
 
   return (
     <div
       onClick={() => void handleSuggestionClick()}
       className="cursor-pointer shadow-md dark:shadow-primary/25 hover:shadow-xl transition-shadow bg-yellow-100 px-3 py-1 rounded-xl border-black/10 dark:border-white/25"
     >
-      {t(`onboarding.${onboardingStep}`)}
+      {question}
     </div>
   );
 };
