f8effa3896fb85506a89b35b894cd5bd5bc6bf30
Matthieu Jacq <67386567+matthieujacq@users.noreply.github.com>
2023-10-11 15:07:43 +0200

    feat: switch between desktop and mobile header

--- frontend/app/(home)/components/HomeHeader.tsx
+++ frontend/app/(home)/components/HomeHeader.tsx
@@ -12,11 +12,14 @@ export const HomeHeader = (): JSX.Element => {
   ];
 
   const navLinks = navItems.map(({ href, label }) => (
-    <li key={label} className="flex justify-between hover:text-primary p-2">
-      <Link href={href} className="">
+    <li key={label}>
+      <Link
+        href={href}
+        className="flex justify-between items-center hover:text-primary p-2 md:text-white md:hover:text-gray-300"
+      >
         {label}
+        <LuChevronRight size={16} />
       </Link>
-      <LuChevronRight size={16} />
     </li>
   ));
 
@@ -27,59 +30,64 @@ export const HomeHeader = (): JSX.Element => {
           <QuivrLogo size={64} />
           <div className="cursor-default">Quivr</div>
         </div>
-        <Popover.Root>
-          <div>
-            <Popover.Anchor />
-            <Popover.Trigger>
-              <button
-                title="menu"
-                type="button"
-                className="text-white bg-[#D9D9D9] bg-opacity-30 rounded-full px-4 py-1"
-              >
-                <LuMenu size={32} />
-              </button>
-            </Popover.Trigger>
-          </div>
-          <Popover.Content
-            style={{
-              minWidth: "max-content",
-              width: "250px",
-              backgroundColor: "white",
-              borderRadius: "0.75rem",
-              paddingTop: "0.5rem",
-              paddingInline: "1rem",
-              paddingBottom: "1.5rem",
-              display: "flex",
-              flexDirection: "column",
-              gap: "1rem",
-              marginRight: "1rem",
-              marginTop: "-1rem",
-            }}
-          >
-            <div className="flex justify-between items-center">
-              <div className="flex gap-2 items-center">
-                <QuivrLogo size={64} color="primary" />
-                <div className="text-lg font-medium text-primary cursor-default ">
-                  Quivr
-                </div>
-              </div>
-              <Popover.Close>
+        <div className="hidden md:flex">
+          <ul className="flex gap-2 items-center">{navLinks}</ul>
+        </div>
+        <div className="md:hidden">
+          <Popover.Root>
+            <div>
+              <Popover.Anchor />
+              <Popover.Trigger>
                 <button
-                  title="close"
+                  title="menu"
                   type="button"
-                  className="hover:text-primary p-2"
+                  className="text-white bg-[#D9D9D9] bg-opacity-30 rounded-full px-4 py-1"
                 >
-                  <LuX size={24} />
+                  <LuMenu size={32} />
                 </button>
-              </Popover.Close>
+              </Popover.Trigger>
             </div>
-            <nav>
-              <ul className="flex flex-col bg-[#F5F8FF] rounded-xl p-2">
-                {navLinks}
-              </ul>
-            </nav>
-          </Popover.Content>
-        </Popover.Root>
+            <Popover.Content
+              style={{
+                minWidth: "max-content",
+                width: "calc(100vw - 2rem)",
+                backgroundColor: "white",
+                borderRadius: "0.75rem",
+                paddingTop: "0.5rem",
+                paddingInline: "1rem",
+                paddingBottom: "1.5rem",
+                display: "flex",
+                flexDirection: "column",
+                gap: "1rem",
+                marginRight: "1rem",
+                marginTop: "-1rem",
+              }}
+            >
+              <div className="flex justify-between items-center">
+                <div className="flex gap-2 items-center">
+                  <QuivrLogo size={64} color="primary" />
+                  <div className="text-lg font-medium text-primary cursor-default ">
+                    Quivr
+                  </div>
+                </div>
+                <Popover.Close>
+                  <button
+                    title="close"
+                    type="button"
+                    className="hover:text-primary p-2"
+                  >
+                    <LuX size={24} />
+                  </button>
+                </Popover.Close>
+              </div>
+              <nav>
+                <ul className="flex flex-col bg-[#F5F8FF] rounded-xl p-2">
+                  {navLinks}
+                </ul>
+              </nav>
+            </Popover.Content>
+          </Popover.Root>
+        </div>
       </header>
     </>
   );
