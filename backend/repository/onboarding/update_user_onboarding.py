db35e0339e457b3d6f3f5897497b431770f40d90
mamadoudicko <mamadoudicko100@gmail.com>
2023-10-11 14:11:09 +0200

    fix: update update_user_onboarding function

--- backend/repository/onboarding/update_user_onboarding.py
+++ backend/repository/onboarding/update_user_onboarding.py
@@ -15,7 +15,7 @@ def update_user_onboarding(
     supabase_db = get_supabase_db()
     updated_onboarding = supabase_db.update_user_onboarding(user_id, onboarding)
 
-    if all(not value for value in onboarding.dict().values()):
+    if all(not value for value in updated_onboarding.dict().values()):
         supabase_db.remove_user_onboarding(user_id)
 
     return updated_onboarding
