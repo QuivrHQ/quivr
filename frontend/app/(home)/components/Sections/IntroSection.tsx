8471e95a0f977b3bc84f8b4ad7ffb947a2baf010
Matthieu Jacq <67386567+matthieujacq@users.noreply.github.com>
2023-10-17 13:38:41 +0200

    First screen content layout

--- frontend/app/(home)/components/Sections/IntroSection.tsx
+++ frontend/app/(home)/components/Sections/IntroSection.tsx
@@ -1,18 +1,28 @@
 export const IntroSection = (): JSX.Element => {
   return (
     <>
-      <div className="flex flex-col md:flex-row items-center justify-center gap-10 h-[calc(100vh-300px)] mb-[calc(50vw*tan(6deg))] md:mb-0">
-        <div>
-          <h1 className="text-5xl font-bold">Get a second brain with Quivr</h1>
-          <p>Upload all your files and start talking with them.</p>
-          <button className="text-white bg-black rounded-full px-4 py-2 mx-2">
-            Try free demo
-          </button>
-          <button className="font-semibold px-4 py-2 mx-2">
-            Contact sales team
-          </button>
+      <div className="flex flex-col lg:flex-row items-center justify-center md:justify-start gap-10 h-[calc(100vh-300px)] mb-[calc(50vw*tan(6deg))] md:mb-0">
+        <div className="w-[80vw] lg:w-[60%] lg:shrink-0 flex flex-col justify-center gap-20">
+          <div>
+            <h1 className="text-6xl leading-[5rem] sm:text-7xl sm:leading-[6rem] font-bold text-black block max-w-2xl">
+              Get a second Brain with{" "}
+              <span className="text-primary">Quivr</span>
+            </h1>
+            <br />
+            <p className="text-xl">
+              Upload all your files and start talking with them.
+            </p>
+          </div>
+          <div>
+            <button className="text-white bg-black rounded-full px-4 py-2 mx-2">
+              Try free demo
+            </button>
+            <button className="font-semibold px-4 py-2 mx-2">
+              Contact sales team
+            </button>
+          </div>
         </div>
-        <div className="w-[80vw] md:w-[400px] h-[80vw] md:h-[400px] bg-slate-200 rounded flex flex-col items-center justify-center">
+        <div className="w-[80vw] lg:w-[calc(50vw-10%-4rem)] lg:shrink-0 h-[80vw] lg:h-[400px] bg-slate-200 rounded flex flex-col items-center justify-center">
           <p>💻 📱 Laptop / mobile image</p>
           <div className="mx-auto my-5 p-5 w-min-content bg-yellow-100 rounded-lg">
             🚧 New homepage in progress 🚧
