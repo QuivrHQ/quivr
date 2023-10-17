4412fefaa11d1c6672bb1a47885e409b8c2967ac
Matthieu Jacq <67386567+matthieujacq@users.noreply.github.com>
2023-10-17 16:25:37 +0200

    add translation for the intro

--- frontend/app/(home)/components/Sections/IntroSection.tsx
+++ frontend/app/(home)/components/Sections/IntroSection.tsx
@@ -1,31 +1,31 @@
 import Link from "next/link";
+import { useTranslation } from "react-i18next";
 import { LuChevronRight } from "react-icons/lu";
 
 import Button from "@/lib/components/ui/Button";
 
 export const IntroSection = (): JSX.Element => {
+  const { t } = useTranslation("home", { keyPrefix: "intro" });
+
   return (
     <>
       <div className="flex flex-col lg:flex-row items-center justify-center md:justify-start gap-10 lg:h-[calc(100vh-300px)] mb-[calc(50vw*tan(6deg))] md:mb-0">
         <div className="w-[80vw] lg:w-[60%] lg:shrink-0 flex flex-col justify-center gap-20">
           <div>
             <h1 className="text-5xl leading-[4rem] sm:text-7xl sm:leading-[6rem] font-bold text-black block max-w-2xl">
-              Get a second Brain with{" "}
-              <span className="text-primary">Quivr</span>
+              {t("title")} <span className="text-primary">Quivr</span>
             </h1>
             <br />
-            <p className="text-xl">
-              Upload all your files and start talking with them.
-            </p>
+            <p className="text-xl">{t("subtitle")}</p>
           </div>
           <div className="flex flex-col items-start sm:flex-row sm:items-center gap-5">
             <Link href="/signup">
               <Button className="text-white bg-black rounded-full">
-                Try free demo <LuChevronRight size={24} />
+                {t("try_demo")} <LuChevronRight size={24} />
               </Button>
             </Link>
             <Button variant="tertiary" className="font-semibold">
-              Contact sales team <LuChevronRight size={24} />
+              {t("contact_sales")} <LuChevronRight size={24} />
             </Button>
           </div>
         </div>
