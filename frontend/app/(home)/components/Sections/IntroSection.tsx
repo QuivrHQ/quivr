850ef8cedd734b8c0e4d45dc7c5aa5cbd45e1475
Matthieu Jacq <67386567+matthieujacq@users.noreply.github.com>
2023-10-17 14:52:43 +0200

    style buttons

--- frontend/app/(home)/components/Sections/IntroSection.tsx
+++ frontend/app/(home)/components/Sections/IntroSection.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
+import { LuChevronRight } from "react-icons/lu";
+
+import Button from "@/lib/components/ui/Button";
+
 export const IntroSection = (): JSX.Element => {
   return (
     <>
-      <div className="flex flex-col lg:flex-row items-center justify-center md:justify-start gap-10 h-[calc(100vh-300px)] mb-[calc(50vw*tan(6deg))] md:mb-0">
+      <div className="flex flex-col lg:flex-row items-center justify-center md:justify-start gap-10 lg:h-[calc(100vh-300px)] mb-[calc(50vw*tan(6deg))] md:mb-0">
         <div className="w-[80vw] lg:w-[60%] lg:shrink-0 flex flex-col justify-center gap-20">
           <div>
-            <h1 className="text-6xl leading-[5rem] sm:text-7xl sm:leading-[6rem] font-bold text-black block max-w-2xl">
+            <h1 className="text-5xl leading-[4rem] sm:text-7xl sm:leading-[6rem] font-bold text-black block max-w-2xl">
               Get a second Brain with{" "}
               <span className="text-primary">Quivr</span>
             </h1>
@@ -13,13 +18,15 @@ export const IntroSection = (): JSX.Element => {
               Upload all your files and start talking with them.
             </p>
           </div>
-          <div>
-            <button className="text-white bg-black rounded-full px-4 py-2 mx-2">
-              Try free demo
-            </button>
-            <button className="font-semibold px-4 py-2 mx-2">
-              Contact sales team
-            </button>
+          <div className="flex">
+            <Link href="/signup">
+              <Button className="text-white bg-black rounded-full">
+                Try free demo <LuChevronRight size={24} />
+              </Button>
+            </Link>
+            <Button variant="tertiary" className="font-semibold">
+              Contact sales team <LuChevronRight size={24} />
+            </Button>
           </div>
         </div>
         <div className="w-[80vw] lg:w-[calc(50vw-10%-4rem)] lg:shrink-0 h-[80vw] lg:h-[400px] bg-slate-200 rounded flex flex-col items-center justify-center">
