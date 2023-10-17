afde3737af50ef80d0b68ea7d518632da2b3f30d
Matthieu Jacq <67386567+matthieujacq@users.noreply.github.com>
2023-10-17 17:29:43 +0200

    Demo: add signup button

--- frontend/app/(home)/components/Sections/DemoSection/DemoSection.tsx
+++ frontend/app/(home)/components/Sections/DemoSection/DemoSection.tsx
@@ -1,12 +1,19 @@
+import Link from "next/link";
+
+import Button from "@/lib/components/ui/Button";
+
 import { VideoPlayer } from "./components/VideoPlayer";
 
 export const DemoSection = (): JSX.Element => {
   return (
     <>
-      <h2 className="text-center text-3xl font-semibold mb-2">Demo video</h2>
+      <h2 className="text-center text-3xl font-semibold mb-5">Demo video</h2>
       <div className="max-w-3xl">
         <VideoPlayer videoSrc="https://user-images.githubusercontent.com/19614572/239713902-a6463b73-76c7-4bc0-978d-70562dca71f5.mp4" />
       </div>
+      <Link href="/signup">
+        <Button className="mt-10 rounded-full">Start now</Button>
+      </Link>
     </>
   );
 };
