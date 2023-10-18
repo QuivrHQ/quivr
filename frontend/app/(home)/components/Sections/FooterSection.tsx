220a0623406a94f7f5f939b5e291246d38de29b2
Matthieu Jacq <67386567+matthieujacq@users.noreply.github.com>
2023-10-18 16:25:48 +0200

    feat: HomePage new footer (#1425)
    # Description
    
    Footer section
    
    Epic: #1232 
    US: #1422
    
    ## Screenshots (if appropriate):
    
    <img width="1127" alt="image"
    src="https://github.com/StanGirard/quivr/assets/67386567/b8d5410b-7f1d-49fe-b3e6-87dec5e18529">

--- frontend/app/(home)/components/Sections/FooterSection.tsx
+++ frontend/app/(home)/components/Sections/FooterSection.tsx
@@ -1,10 +1,67 @@
+import Link from "next/link";
+import { useTranslation } from "react-i18next";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { LuChevronRight } from "react-icons/lu";
+import { RiTwitterXLine } from "react-icons/ri";
+
+import Button from "@/lib/components/ui/Button";
+import { GITHUB_URL, LINKEDIN_URL, TWITTER_URL } from "@/lib/config/CONSTANTS";
+
 export const FooterSection = (): JSX.Element => {
+  const { t } = useTranslation("home", { keyPrefix: "footer" });
+
   return (
-    <>
-      <h2 className="text-2xl text-center text-white">Get started today</h2>
-      <div className="w-[80vw] md:w-[400px] h-[80vw] md:h-[400px] bg-slate-200 rounded flex items-center justify-center">
-        ⬇️ Footer
+    <div className="flex flex-col items-center gap-10 text-white text-center text-lg">
+      <h2 className="text-3xl">{t("title")}</h2>
+      <p>
+        {t("description_1")} <br /> {t("description_2")}{" "}
+      </p>
+      <div className="flex items-center">
+        <Link href="/signup">
+          <Button className=" rounded-full">
+            {t("start_using")}
+            <LuChevronRight size={24} />
+          </Button>
+        </Link>
+        <Button variant="tertiary">
+          {t("contact_sales")} <LuChevronRight size={24} />
+        </Button>
       </div>
-    </>
+      <ul className="flex gap-10 mt-5 mb-10 text-black">
+        <li>
+          <a
+            href={LINKEDIN_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Quivr LinkedIn"
+            className="hover:text-black"
+          >
+            <FaLinkedin size={52} />
+          </a>
+        </li>
+        <li>
+          <a
+            href={TWITTER_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Quivr Twitter"
+            className="hover:text-black"
+          >
+            <RiTwitterXLine size={52} />
+          </a>
+        </li>
+        <li>
+          <a
+            href={GITHUB_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Quivr GitHub"
+            className="hover:text-black"
+          >
+            <FaGithub size={52} />
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 };
