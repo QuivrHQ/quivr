2743e0a9df53b6f4621aeb3a5fe890e204e79040
Matthieu Jacq <67386567+matthieujacq@users.noreply.github.com>
2023-10-18 15:33:27 +0200

    Footer: Add social icons
    Use react-icons for social icons

--- frontend/app/(home)/components/Sections/FooterSection.tsx
+++ frontend/app/(home)/components/Sections/FooterSection.tsx
@@ -1,8 +1,11 @@
 import Link from "next/link";
 import { useTranslation } from "react-i18next";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { LuChevronRight } from "react-icons/lu";
+import { RiTwitterXLine } from "react-icons/ri";
 
 import Button from "@/lib/components/ui/Button";
+import { GITHUB_URL, LINKEDIN_URL, TWITTER_URL } from "@/lib/config/CONSTANTS";
 
 export const FooterSection = (): JSX.Element => {
   const { t } = useTranslation("home", { keyPrefix: "footer" });
@@ -24,6 +27,41 @@ export const FooterSection = (): JSX.Element => {
           {t("contact_sales")} <LuChevronRight size={24} />
         </Button>
       </div>
+      <ul className="flex gap-10 mt-5 mb-10 text-black">
+        <li>
+          <a
+            href={LINKEDIN_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Quivr LinkedIn"
+            className="hover:text-black"
+          >
+            <FaLinkedin size={52} />
+          </a>
+        </li>
+        <li>
+          <a
+            href={TWITTER_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Quivr Twitter"
+            className="hover:text-black"
+          >
+            <RiTwitterXLine size={52} />
+          </a>
+        </li>
+        <li>
+          <a
+            href={GITHUB_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Quivr GitHub"
+            className="hover:text-black"
+          >
+            <FaGithub size={52} />
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
