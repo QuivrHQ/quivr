ef5eef20482e3b19df49cc4bea9bfbc99d88e2e6
Matthieu Jacq <67386567+matthieujacq@users.noreply.github.com>
2023-10-18 10:47:42 +0200

    Larger demo video

--- frontend/app/(home)/components/Sections/DemoSection/DemoSection.tsx
+++ frontend/app/(home)/components/Sections/DemoSection/DemoSection.tsx
@@ -12,11 +12,11 @@ export const DemoSection = (): JSX.Element => {
   return (
     <div className="sm:min-h-[calc(100vh-250px)] flex flex-col items-center justify-center gap-10">
       <h2 className="text-center text-3xl font-semibold mb-5">{t("title")}</h2>
-      <div className="max-w-3xl">
+      <div className="max-w-4xl">
         <VideoPlayer videoSrc="https://user-images.githubusercontent.com/19614572/239713902-a6463b73-76c7-4bc0-978d-70562dca71f5.mp4" />
       </div>
       <Link href="/signup">
-        <Button className="mt-10 rounded-full">
+        <Button className="mt-2 rounded-full">
           {t("start_now")} <LuChevronRight size={24} />
         </Button>
       </Link>
