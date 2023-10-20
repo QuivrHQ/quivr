77348dc0c6c9de17f3324f3717f236b9af9b86cb
Matthieu Jacq <67386567+matthieujacq@users.noreply.github.com>
2023-10-20 15:18:12 +0200

    Use Feature flag on contact sales page

--- frontend/app/contact/page.tsx
+++ frontend/app/contact/page.tsx
@@ -1,6 +1,13 @@
 "use client";
+import { useFeatureIsOn } from "@growthbook/growthbook-react";
+import { redirect } from "next/navigation";
 
 const ContactSalesPage = (): JSX.Element => {
+  const isNewHomePage = useFeatureIsOn("new-homepage-activated");
+  if (!isNewHomePage) {
+    redirect("/");
+  }
+
   return <div>Not implemented</div>;
 };
 
