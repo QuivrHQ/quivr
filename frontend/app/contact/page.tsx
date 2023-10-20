5878efb36a7638285e15034edebb113865b23e30
Matthieu Jacq <67386567+matthieujacq@users.noreply.github.com>
2023-10-20 15:19:09 +0200

    Home header can be black or white

--- frontend/app/contact/page.tsx
+++ frontend/app/contact/page.tsx
@@ -2,13 +2,19 @@
 import { useFeatureIsOn } from "@growthbook/growthbook-react";
 import { redirect } from "next/navigation";
 
+import { HomeHeader } from "../(home)/components";
+
 const ContactSalesPage = (): JSX.Element => {
   const isNewHomePage = useFeatureIsOn("new-homepage-activated");
   if (!isNewHomePage) {
     redirect("/");
   }
 
-  return <div>Not implemented</div>;
+  return (
+    <div className="bg-[#FCFAF6]">
+      <HomeHeader color="black" />
+    </div>
+  );
 };
 
 export default ContactSalesPage;
