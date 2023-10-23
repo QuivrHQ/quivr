a0e4862927d04ff49f8c8dbed7f723d2df77bd4d
Stan Girard <girard.stanislas@gmail.com>
2023-10-23 10:44:50 +0200

    feat(cms): added single video demo (#1459)
    # Description
    
    Please include a summary of the changes and the related issue. Please
    also include relevant motivation and context.
    
    ## Checklist before requesting a review
    
    Please delete options that are not relevant.
    
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my code
    - [ ] I have commented hard-to-understand areas
    - [ ] I have ideally added tests that prove my fix is effective or that
    my feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] Any dependent changes have been merged
    
    ## Screenshots (if appropriate):

---
+++ cms/quivr/src/api/demo-video/routes/demo-video.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * demo-video router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::demo-video.demo-video');
