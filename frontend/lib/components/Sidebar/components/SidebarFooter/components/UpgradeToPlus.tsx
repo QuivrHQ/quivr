3cd3e220c9fd8ec3798840a60bccd9eaf5cc4dcb
Matthieu Jacq <67386567+matthieujacq@users.noreply.github.com>
2023-10-24 16:09:25 +0200

    Add translations

--- frontend/lib/components/Sidebar/components/SidebarFooter/components/UpgradeToPlus.tsx
+++ frontend/lib/components/Sidebar/components/SidebarFooter/components/UpgradeToPlus.tsx
@@ -1,4 +1,5 @@
 import { useFeatureIsOn } from "@growthbook/growthbook-react";
+import { useTranslation } from "react-i18next";
 import { FiUser } from "react-icons/fi";
 
 import { Modal } from "@/lib/components/ui/Modal";
@@ -13,6 +14,7 @@ export const UpgradeToPlus = (): JSX.Element => {
   const { userData } = useUserData();
   const is_premium = userData?.is_premium;
   const featureIsOn = useFeatureIsOn("monetization");
+  const { t } = useTranslation("monetization");
 
   if (!featureIsOn || is_premium === true) {
     return <></>;
@@ -24,8 +26,10 @@ export const UpgradeToPlus = (): JSX.Element => {
         <button type="button" className={sidebarLinkStyle}>
           <FiUser className="w-8 h-8" />
           <span>
-            Upgrade to plus{" "}
-            <span className="rounded bg-primary/50 py-1 px-3 text-xs">New</span>
+            {t("upgrade")}{" "}
+            <span className="rounded bg-primary/50 py-1 px-3 text-xs">
+              {t("new")}
+            </span>
           </span>
         </button>
       }
