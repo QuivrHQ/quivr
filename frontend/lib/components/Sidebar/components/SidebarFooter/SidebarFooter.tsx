fb082dcf510c0606608573b6fb71da391cc89221
Matthieu Jacq <67386567+matthieujacq@users.noreply.github.com>
2023-10-25 10:26:37 +0200

    Clean code: use a map to show buttons sidebar footer

--- frontend/lib/components/Sidebar/components/SidebarFooter/SidebarFooter.tsx
+++ frontend/lib/components/Sidebar/components/SidebarFooter/SidebarFooter.tsx
@@ -12,12 +12,16 @@ type SidebarFooterProps = {
 export const SidebarFooter = ({
   showButtons,
 }: SidebarFooterProps): JSX.Element => {
+  const buttons = {
+    myBrains: <BrainManagementButton />,
+    upgradeToPlus: <UpgradeToPlus />,
+    user: <UserButton />,
+  };
+
   return (
     <div className="bg-gray-50 dark:bg-gray-900 border-t dark:border-white/10 mt-auto p-2">
       <div className="max-w-screen-xl flex justify-center items-center flex-col">
-        {showButtons.includes("myBrains") && <BrainManagementButton />}
-        {showButtons.includes("upgradeToPlus") && <UpgradeToPlus />}
-        {showButtons.includes("user") && <UserButton />}
+        {showButtons.map((button) => buttons[button])}
       </div>
     </div>
   );
