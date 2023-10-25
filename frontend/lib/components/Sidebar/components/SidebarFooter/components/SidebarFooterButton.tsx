3e5828f4bb5f582695cc4d5f99c9d805a673869d
Matthieu Jacq <67386567+matthieujacq@users.noreply.github.com>
2023-10-25 12:04:12 +0200

    SidebarFooter button: prefetch

--- frontend/lib/components/Sidebar/components/SidebarFooter/components/SidebarFooterButton.tsx
+++ frontend/lib/components/Sidebar/components/SidebarFooter/components/SidebarFooterButton.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 type SidebarFooterButtonProps = {
   icon: JSX.Element;
@@ -20,6 +21,12 @@ export const SidebarFooterButton = ({
       void router.push(href);
     };
   }
+
+  useEffect(() => {
+    if (href !== undefined) {
+      void router.prefetch(href);
+    }
+  }, [href, router]);
 
   return (
     <button
