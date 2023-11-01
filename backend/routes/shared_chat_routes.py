f8c0342cfe7b6dd9d688318e32a3ad15d264e500
Luke Zeng <haohao@stayreal.studio>
2023-11-01 09:29:49 -0700

    return chat history ok

--- backend/routes/shared_chat_routes.py
+++ backend/routes/shared_chat_routes.py
@@ -1,6 +1,13 @@
 from uuid import UUID
+from typing import List
+
+# from repository.chat.get_chat_history_with_notifications import ChatItem
 from repository.shared_chat import (
     get_shared_chat_by_shared_chat_id,
+)
+from repository.chat.get_chat_history_with_notifications import (
+    ChatItem,
+    get_chat_history_with_notifications,
 )
 from fastapi import APIRouter
 
@@ -9,8 +16,12 @@ shared_chat_router = APIRouter()
 
 
 @shared_chat_router.get("/shared/chat/{shared_chat_id}")
-def get_shared_chat_identity_route(shared_chat_id: UUID) -> str:
+def get_shared_chat_identity_route(shared_chat_id: UUID) -> List[ChatItem]:
     """
     Get user identity.
     """
-    return get_shared_chat_by_shared_chat_id(shared_chat_id)
+    chat_id = get_shared_chat_by_shared_chat_id(shared_chat_id)
+    # chat = get_chat_by_id(chat_id)
+    # chat_history = get_chat_history(chat_id)
+
+    return get_chat_history_with_notifications(chat_id)
