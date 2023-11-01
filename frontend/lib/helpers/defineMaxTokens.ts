c42e4a4eb32105dd76e49ddf7b3a9c17a3d58fda
Stan Girard <girard.stanislas@gmail.com>
2023-11-01 08:52:31 +0100

    feat: 🎸 max-token
    added limit to 4k for gpt4

--- frontend/lib/helpers/defineMaxTokens.ts
+++ frontend/lib/helpers/defineMaxTokens.ts
@@ -4,11 +4,11 @@ export const defineMaxTokens = (model: Model | PaidModels): number => {
   //At the moment is evaluating only models from OpenAI
   switch (model) {
     case "gpt-3.5-turbo":
-      return 750;
-    case "gpt-3.5-turbo-16k":
-      return 2000;
-    case "gpt-4":
       return 1000;
+    case "gpt-3.5-turbo-16k":
+      return 4000;
+    case "gpt-4":
+      return 4000;
     default:
       return 500;
   }
