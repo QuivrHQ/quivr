ec82fa5c147ed40256e0c5783c8ce97a9e39a180
Stef Bloom <stef@modtechgroup.com>
2023-11-02 06:54:10 -0700

    fixes custom prompt dropdown text color for dark mode with text-gray-700

--- frontend/app/brains-management/[brainId]/components/BrainManagementTabs/components/SettingsTab/components/PublicPrompts/components/PublicPromptsList/PublicPromptsList.tsx
+++ frontend/app/brains-management/[brainId]/components/BrainManagementTabs/components/SettingsTab/components/PublicPrompts/components/PublicPromptsList/PublicPromptsList.tsx
@@ -42,7 +42,7 @@ export const PublicPromptsList = ({
           {options.map((option) => (
             <div
               key={option.id}
-              className="px-4 py-2 cursor-pointer hover:bg-gray-100"
+              className="px-4 py-2 cursor-pointer hover:bg-gray-100 text-gray-700"
               onClick={() => handleOptionClick(option)}
             >
               {option.title}
