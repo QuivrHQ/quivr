038dcfc56e86e7742d6c417946524b9a2d078b8a
mamadoudicko <mamadoudicko100@gmail.com>
2023-11-09 12:42:08 +0100

    feat: update APIBrainQA utils

--- backend/llm/utils/call_brain_api.py
+++ backend/llm/utils/call_brain_api.py
@@ -1,28 +1,30 @@
 from uuid import UUID
 
-from llm.utils.extract_brain_api_params_values_from_llm_output import (
-    extract_brain_api_params_values_from_llm_output,
+from fastapi import HTTPException
+from llm.utils.extract_api_brain_definition_values_from_llm_output import (
+    extract_api_brain_definition_values_from_llm_output,
 )
-from llm.utils.make_api_request import make_api_request
+from llm.utils.make_api_request import get_api_call_response_as_text
 from repository.api_brain_definition.get_api_brain_definition import (
     get_api_brain_definition,
 )
 from repository.external_api_secret.read_secret import read_secret
 
 
-def call_brain_api(brain_id: UUID, user_id: UUID, arguments: dict):
+def call_brain_api(brain_id: UUID, user_id: UUID, arguments: dict) -> str:
     brain_definition = get_api_brain_definition(brain_id)
-    if brain_definition is None:
-        raise Exception("Brain definition not found")
 
-    brain_params = brain_definition.params.properties
-    brain_params_values = extract_brain_api_params_values_from_llm_output(
-        brain_params, arguments
+    if brain_definition is None:
+        raise HTTPException(
+            status_code=404, detail=f"Brain definition {brain_id} not found"
+        )
+
+    brain_params_values = extract_api_brain_definition_values_from_llm_output(
+        brain_definition.params, arguments
     )
 
-    brain_search_params = brain_definition.search_params.properties
-    brain_search_params_values = extract_brain_api_params_values_from_llm_output(
-        brain_search_params, arguments
+    brain_search_params_values = extract_api_brain_definition_values_from_llm_output(
+        brain_definition.search_params, arguments
     )
 
     secrets = brain_definition.secrets
@@ -34,9 +36,10 @@ def call_brain_api(brain_id: UUID, user_id: UUID, arguments: dict):
         )
         secrets_values[secret.name] = secret_value
 
-    return make_api_request(
+    return get_api_call_response_as_text(
         api_url=brain_definition.url,
         params=brain_params_values,
         search_params=brain_search_params_values,
         secrets=secrets_values,
+        method=brain_definition.method,
     )
