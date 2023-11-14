5a3f284785bc0cfdbfe0fd3ab32adb90c82641ff
Stan Girard <girard.stanislas@gmail.com>
2023-11-14 08:36:44 +0100

    test(all): added (#1624)
    # Description
    
    Please include a summary of the changes and the related issue. Please
    also include relevant motivation and context.
    
    ## Checklist before requesting a review
    
    Please delete options that are not relevant.
    
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my code
    - [ ] I have commented hard-to-understand areas
    - [ ] I have ideally added tests that prove my fix is effective or that
    my feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] Any dependent changes have been merged
    
    ## Screenshots (if appropriate):

--- backend/tests/test_user.py
+++ backend/tests/test_user.py
@@ -16,3 +16,20 @@ def test_get_user_info(client, api_key):
     assert "models" in user_info
     assert "requests_stats" in user_info
     assert "id" in user_info
+
+
+def test_get_user_identity(client, api_key):
+    # Send a request to get user identity
+    response = client.get(
+        "/user/identity", headers={"Authorization": "Bearer " + api_key}
+    )
+
+    # Assert that the response status code is 200 (HTTP OK)
+    assert response.status_code == 200
+
+    # Assert that the response contains the expected fields
+    user_identity = response.json()
+    print(user_identity)
+    assert "id" in user_identity
+    assert "email" in user_identity
+    assert "openai_api_key" in user_identity
