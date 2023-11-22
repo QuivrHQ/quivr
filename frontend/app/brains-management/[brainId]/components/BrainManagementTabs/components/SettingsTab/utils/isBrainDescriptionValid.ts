bd5f495ab42fc096fab90de64b6e908341f1ff0d
mamadoudicko <mamadoudicko100@gmail.com>
2023-11-22 10:54:16 +0100

    feat(updateChat): make description required

--- frontend/app/brains-management/[brainId]/components/BrainManagementTabs/components/SettingsTab/utils/isBrainDescriptionValid.ts
+++ frontend/app/brains-management/[brainId]/components/BrainManagementTabs/components/SettingsTab/utils/isBrainDescriptionValid.ts
@@ -2,17 +2,19 @@ import { TFunction } from "i18next";
 
 import { ToastData } from "@/lib/components/ui/Toast/domain/types";
 
-export const checkBrainName = (
-  name: string,
+export const isBrainDescriptionValid = (
+  description: string,
   publish: (toast: ToastData) => void,
   t: TFunction<["translation", "brain", "config"]>
-): void => {
-  if (name.trim() === "") {
+): boolean => {
+  if (description.trim() === "") {
     publish({
       variant: "danger",
-      text: t("nameRequired", { ns: "config" }),
+      text: t("descriptionRequired", { ns: "config" }),
     });
 
-    return;
+    return false;
   }
+
+  return true;
 };
