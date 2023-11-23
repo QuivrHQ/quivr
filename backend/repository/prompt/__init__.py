98c5658c641fe7f789b209c1b93dccee76c94ff7
gozineb <zinebe@theodo.fr>
2023-11-23 08:34:39 +0100

    ♻️ new prompt Service

--- backend/repository/prompt/__init__.py
+++
@@ -1,5 +0,0 @@
-from .create_prompt import create_prompt
-from .get_prompt_by_id import get_prompt_by_id
-from .get_public_prompts import get_public_prompts
-from .update_prompt_by_id import update_prompt_by_id
-from .delete_prompt_py_id import delete_prompt_by_id
