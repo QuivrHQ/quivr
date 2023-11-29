89ae98d36b03c78305dbe564fa637587e026f0d6
tianxiaxi <warren@stayreal.studio>
2023-11-29 11:05:02 +0800

    feat: update gettr link

--- frontend/lib/config/CONSTANTS.ts
+++ frontend/lib/config/CONSTANTS.ts
@@ -4,4 +4,4 @@ export const DISCORD_URL = "https://discord.gg/HUpRgp2HG8";
 export const LINKEDIN_URL = "https://www.linkedin.com/company/getquivr";
 export const DEFAULT_BACKEND_URL = "http://localhost:5050";
 export const DEFAULT_CMS_URL = "http://localhost:1337";
-export const GETTR_URL = "https://gettr.com/user/mrhaohao";
+export const GETTR_URL = "https://gettr.com/user/nfscluke";
