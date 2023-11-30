f00b48be0f0fd151eeaf4d3dbd13d1c9d1d17d9e
tianxiaxi <warren@stayreal.studio>
2023-11-30 22:13:15 +0800

    feat: i18n default language

--- frontend/lib/config/LocaleConfig/i18n.ts
+++ frontend/lib/config/LocaleConfig/i18n.ts
@@ -5,13 +5,15 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
+import { getBrowserLang } from "@/lib/api/chat/utils";
+
 import { resources } from "./resources";
 
 const defaultNS = "translation";
 
 i18n.use(initReactI18next).init({
-  lng: "en",
-  fallbackLng: "en",
+  lng: getBrowserLang(),
+  fallbackLng: getBrowserLang(),
   defaultNS,
   resources,
   debug: process.env.NEXT_PUBLIC_ENV !== "prod",
