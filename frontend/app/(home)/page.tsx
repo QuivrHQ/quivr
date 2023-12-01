956667e385d74317b8a1b43424fcd1800141bd8e
tianxiaxi <warren@stayreal.studio>
2023-12-01 23:46:22 +0800

    feat: home page ui op

--- frontend/app/(home)/page.tsx
+++ frontend/app/(home)/page.tsx
@@ -26,10 +26,10 @@ const HomePage = (): JSX.Element => {
       <HomeHeader />
 
       <main
-        className="relative flex flex-col items-center h-full"
+        className=" flex flex-col items-center h-full"
         data-testid="home-page"
       >
-        <HomeSection bg="transparent">
+        <HomeSection bg="transparent" className="flex-1">
           <IntroSection />
         </HomeSection>
 
@@ -57,7 +57,6 @@ const HomePage = (): JSX.Element => {
         <HomeSection
           bg="bg-gradient-to-b from-sky-700 to-sky-200"
           slantBefore="up"
-          className="absolute bottom-0"
         >
           <FooterSection />
         </HomeSection>
