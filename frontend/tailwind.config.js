b09d93e547210c6b361423bd7e917f69c5c657ff
Stan Girard <girard.stanislas@gmail.com>
2023-12-12 14:29:58 +0100

    feat: Add @tailwindcss/forms plugin and update (#1869)
    Init for platform

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -26,5 +26,7 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require('@tailwindcss/forms')],
 };
