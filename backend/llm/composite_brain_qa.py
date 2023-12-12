71221dadd4f4484da5db606add8216c152360e55
gozineb <zinebe@theodo.fr>
2023-12-12 09:00:02 +0100

    ✨ add history & clean function

--- backend/llm/composite_brain_qa.py
+++ backend/llm/composite_brain_qa.py
@@ -11,6 +11,7 @@ from logger import get_logger
 from modules.brain.entity.brain_entity import BrainEntity, BrainType
 from modules.brain.service.brain_service import BrainService
 from modules.chat.dto.chats import ChatQuestion
+from modules.chat.dto.inputs import CreateChatHistory
 from modules.chat.dto.outputs import GetChatHistoryOutput
 from modules.chat.service.chat_service import ChatService
 
@@ -39,7 +40,6 @@ def format_brain_to_tool(brain):
                     },
                 },
                 "required": ["brain_id", "question"],
-                # "exclude": ["brain_id" dans visited_brains + question like questions dans asked_questions[brain_id]]
             },
         },
     }
@@ -74,7 +74,6 @@ class CompositeBrainQA(
         self.user_id = user_id
 
     def get_answer_generator_from_brain_type(self, brain: BrainEntity):
-        print("BRAIN", brain)
         if brain.brain_type == BrainType.COMPOSITE:
             return self.generate_answer
         elif brain.brain_type == BrainType.API:
@@ -97,34 +96,12 @@ class CompositeBrainQA(
                 streaming=self.streaming,
                 prompt_id=self.prompt_id,
             ).generate_answer
-        # brain_type_to_answer_generator = {
-        #     "composite_brain": self.generate_answer,
-        #     "api_brain": APIBrainQA(
-        #         brain_id=brain.id,
-        #         chat_id=self.chat_id,
-        #         model=self.model,
-        #         max_tokens=self.max_tokens,
-        #         temperature=self.temperature,
-        #         streaming=self.streaming,
-        #         prompt_id=self.prompt_id,
-        #     ).generate_answer,
-        #     "knowledge_brain": KnowledgeBrainQA(
-        #         brain_id=brain.id,
-        #         chat_id=self.chat_id,
-        #         model=self.model,
-        #         max_tokens=self.max_tokens,
-        #         temperature=self.temperature,
-        #         streaming=self.streaming,
-        #         prompt_id=self.prompt_id,
-        #     ).generate_answer,
-        # }
-        # return brain_type_to_answer_generator.get(brain.brain_type)
 
-    def generate_answer(self, chat_id: UUID, question: ChatQuestion) -> str:
-        # Retrieve the connected brains for the current composite brain
+    def generate_answer(
+        self, chat_id: UUID, question: ChatQuestion, save_answer: bool
+    ) -> str:
         connected_brains = brain_service.get_connected_brains(self.brain_id)
         if not connected_brains:
-            # If no connected brains, default to regular HeadlessQA
             response = HeadlessQA(
                 chat_id=chat_id,
                 model=self.model,
@@ -140,7 +117,7 @@ class CompositeBrainQA(
         tools = []
         available_functions = {}
 
-        # Create tools from the connected brains similarly to this:
+        connected_brains_details = {}
         for brain_id in connected_brains:
             brain = brain_service.get_brain_by_id(brain_id)
             if brain == None:
@@ -148,66 +125,98 @@ class CompositeBrainQA(
 
             tools.append(format_brain_to_tool(brain))
 
-            print("tools", tools)
-
             available_functions[brain_id] = self.get_answer_generator_from_brain_type(
                 brain
             )
-            print("available_functions", available_functions)
+
+            connected_brains_details[str(brain.id)] = brain
 
         print("tools", tools)
         print("available_functions", available_functions)
-
+        print("connected_brains_details", connected_brains_details)
         CHOOSE_BRAIN_FROM_TOOLS_PROMPT = (
             "Based on the provided user content, find the most appropriate tools to answer"
-            + "If you can't find any tool to answer and only then, and if you can answer without using any tool, please do so with a 3 year old assistant personality. In that case, let the user know that you are not using any particular brain (i.e tool) "
+            + "If you can't find any tool to answer and only then, and if you can answer without using any tool. In that case, let the user know that you are not using any particular brain (i.e tool) "
             # Pour diminuer le risque d'avoir des boucles
             + "what has been called before: nom de la fonction + argument, dont call unless really needed"
         )
 
-        # Have all the steps and answers logged in a document that will be passed to emettre l'intention et creer les tool_calls
-        # Call completion with this tools
-        # Assuming question.question is a string
-        messages = [
-            {"role": "user", "content": question.question},
-            # add quivr or brain prompt: Helpful assistant bla bla bla
-            {"role": "system", "content": CHOOSE_BRAIN_FROM_TOOLS_PROMPT},
-        ]
-        # Add any additional messages to this list as needed
+        messages = [{"role": "system", "content": CHOOSE_BRAIN_FROM_TOOLS_PROMPT}]
+
+        history = chat_service.get_chat_history(self.chat_id)
+
+        for message in history:
+            formatted_message = [
+                {"role": "user", "content": message.user_message},
+                {"role": "assistant", "content": message.assistant},
+            ]
+            messages.extend(formatted_message)
+
+        messages.append({"role": "user", "content": question.question})
 
         response = completion(
             model="gpt-3.5-turbo",
-            messages=messages,  # Pass the correctly formatted messages
+            messages=messages,  # is History included ?
             tools=tools,
-            tool_choice="auto",  # auto is default, but we'll be explicit
+            tool_choice="auto",
         )
 
         print("\nFirst LLM Response:\n", response)
 
-        # backend-core  | First LLM Response:
-        # backend-core  |  ModelResponse(id='chatcmpl-8TTmScnbGzs5gX0SePNxzbg0y8K0E', choices=[Choices(finish_reason='tool_calls', index=0, message=Message(content=None, role='assistant', tool_calls=[ChatCompletionMessageToolCall(id='call_E6SXHf50pqbeg9cdE3WW1cte', function=Function(arguments='{"question":"What do you know about coffee?"}', name='3d71786c-f176-42b1-b7f5-9f40daaaf922'), type='function')]))], created=1702036680, model='gpt-3.5-turbo-1106', object='chat.completion', system_fingerprint='fp_eeff13170a', usage=Usage(completion_tokens=42, prompt_tokens=262, total_tokens=304), _response_ms=1505.449)
-
-        # check if the finish_reason is stop
         if response.choices[0].finish_reason == "stop":
-            return response.choices[0].message
+            answer = response.choices[0].message
+            print("ANSWER 1", answer)
+            if save_answer:
+                new_chat = chat_service.update_chat_history(
+                    CreateChatHistory(
+                        **{
+                            "chat_id": chat_id,
+                            "user_message": question.question,
+                            "assistant": answer,
+                            "brain_id": question.brain_id,
+                            "prompt_id": self.prompt_to_use_id,
+                        }
+                    )
+                )
+
+                return GetChatHistoryOutput(
+                    **{
+                        "chat_id": chat_id,
+                        "user_message": question.question,
+                        "assistant": answer,
+                        "message_time": new_chat.message_time,
+                        "prompt_title": self.prompt_to_use.title
+                        if self.prompt_to_use
+                        else None,
+                        "brain_name": brain.name if brain else None,
+                        "message_id": new_chat.message_id,
+                    }
+                )
+
+            return GetChatHistoryOutput(
+                **{
+                    "chat_id": chat_id,
+                    "user_message": question.question,
+                    "assistant": answer,
+                    "message_time": None,
+                    "prompt_title": self.prompt_to_use.title
+                    if self.prompt_to_use
+                    else None,
+                    "brain_name": None,
+                    "message_id": None,
+                }
+            )
 
         response_message = response.choices[0].message
         tool_calls = response_message.tool_calls
 
-        print("\nLength of tool calls", len(tool_calls))
-
         called_tools_log = []
-        # messages = [question.question]
-        # Step 2: check if the model wanted to call a function
-        if tool_calls:
-            # Step 3: call the function
-            # Note: the JSON response may not always be valid; be sure to handle errors
-            messages.append(response_message)
-            # Step 4: send the info for each function call and function response to the model
-            for tool_call in tool_calls:
-                print("tool to call", tool_call)
 
+        if tool_calls:
+            messages.append(response_message)
+            for tool_call in tool_calls:
                 function_name = tool_call.function.name
+                queried_brain = connected_brains_details[function_name]
                 function_to_call = available_functions[function_name]
                 function_args = json.loads(tool_call.function.arguments)
                 print("function_args", function_args["question"])
@@ -215,13 +224,12 @@ class CompositeBrainQA(
                 function_response = function_to_call(
                     chat_id=chat_id,
                     question=question,
+                    save_answer=False,
                 )
 
-                # store tool logs in a list as an object
                 called_tools_log.append(
                     {
-                        # get the actual brain name
-                        "log_message": "🧠< Querying the brain {tool_call.function.name} with the following arguments: {tool_call.function.arguments} >🧠",
+                        "log_message": f"🧠< Querying the brain {queried_brain.name} with the following arguments: {function_args} >🧠",
                         "role": "tool",
                         "name": function_name,
                         "content": function_response,
@@ -235,7 +243,7 @@ class CompositeBrainQA(
                         "name": function_name,
                         "content": function_response.assistant,
                     }
-                )  # extend conversation with function response
+                )
 
             PROMPT_2 = "If initial question can be answered by our converaation messsages, then give an answer and end the conversation. \
                         Otherwise, ask a new question to the assistant and choose brains you would like to ask questions."
@@ -247,47 +255,66 @@ class CompositeBrainQA(
                     f"Message {idx}: Role - {msg['role']}, Content - {msg['content']}"
                 )
 
-            second_response = completion(
+            response_after_tools_answers = completion(
                 model="gpt-3.5-turbo",
                 messages=messages,
                 tools=tools,
-                tool_choice="auto",  # auto is default, but we'll be explicit
-            )  # get a new response from the model where it can see the function response
+                tool_choice="auto",
+            )
 
-            print("\nSecond LLM response:\n", second_response)
+            print("\nSecond LLM response:\n", response_after_tools_answers)
 
-            # update the chat history with the new response
+            if response_after_tools_answers.choices[0].finish_reason == "stop":
+                answer = response_after_tools_answers.choices[0].message.content
+                print("ANSWER 2", answer)
+                if save_answer:
+                    new_chat = chat_service.update_chat_history(
+                        CreateChatHistory(
+                            **{
+                                "chat_id": chat_id,
+                                "user_message": question.question,
+                                "assistant": answer,
+                                "brain_id": question.brain_id,
+                                "prompt_id": self.prompt_to_use_id,
+                            }
+                        )
+                    )
+
+                    return GetChatHistoryOutput(
+                        **{
+                            "chat_id": chat_id,
+                            "user_message": question.question,
+                            "assistant": answer,
+                            "message_time": new_chat.message_time,
+                            "prompt_title": self.prompt_to_use.title
+                            if self.prompt_to_use
+                            else None,
+                            "brain_name": brain.name if brain else None,
+                            "message_id": new_chat.message_id,
+                        }
+                    )
+                return GetChatHistoryOutput(
+                    **{
+                        "chat_id": chat_id,
+                        "user_message": question.question,
+                        "assistant": answer,
+                        "message_time": "123",
+                        "prompt_title": None,
+                        "brain_name": brain.name,
+                        "message_id": None,
+                    }
+                )
+
+            print("response_after_tools_answers", response_after_tools_answers)
+
             return GetChatHistoryOutput(
                 **{
                     "chat_id": chat_id,
                     "user_message": question.question,
-                    "assistant": second_response.choices[0].message.content,
+                    "assistant": response_after_tools_answers.choices[0].message,
                     "message_time": "123",
                     "prompt_title": None,
                     "brain_name": brain.name,
-                    "message_id": "e3894199-6fa3-43e6-9fb4-7e3864f517eb",
+                    "message_id": None,
                 }
             )
-
-        # add an intermediary function to handle recursive call:
-        # condition d'arret: plus aucun tool calls
-        # si pas de tool calls: return response
-        # sinon: call intermediary_function_call avec les tool calls et le prompt updaté (PROMPT_2) + les derniers messages
-        # check if any tools are to be called again
-        # do the thing
-        # else: answer
-
-        # print("\nSecond LLM response:\n", second_response)
-        # check second_response for function calls and repeat steps 2-4 if necessary
-        # return response.choices[0].message
-        # return GetChatHistoryOutput(
-        #     **{
-        #         "chat_id": chat_id,
-        #         "user_message": question.question,
-        #         "assistant": response.choices[0].message,
-        #         "message_time": "123",
-        #         "prompt_title": None,
-        #         "brain_name": None,
-        #         "message_id": 1234,
-        #     }
-        # )
