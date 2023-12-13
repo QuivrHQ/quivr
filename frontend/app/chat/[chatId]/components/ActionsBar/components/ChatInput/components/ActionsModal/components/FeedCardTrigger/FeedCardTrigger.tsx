6b1f3fa64312b89bf39e87121e38a5bcd88f315b
mamadoudicko <mamadoudicko100@gmail.com>
2023-12-13 15:59:07 +0100

    feat: customise feed manage according to brain type

--- frontend/app/chat/[chatId]/components/ActionsBar/components/ChatInput/components/ActionsModal/components/FeedCardTrigger/FeedCardTrigger.tsx
+++ frontend/app/chat/[chatId]/components/ActionsBar/components/ChatInput/components/ActionsModal/components/FeedCardTrigger/FeedCardTrigger.tsx
@@ -1,18 +1,18 @@
-import { useTranslation } from "react-i18next";
-import { LuChevronRight, LuFilePlus } from "react-icons/lu";
+import { LuChevronRight } from "react-icons/lu";
 
 import { useKnowledgeToFeedContext } from "@/lib/context/KnowledgeToFeedProvider/hooks/useKnowledgeToFeedContext";
 
-import { Button } from "./Button";
+import { useFeedCardTrigger } from "./hooks/useFeedCardTrigger";
+import { Button } from "../Button";
 
 export const FeedCardTrigger = (): JSX.Element => {
-  const { t } = useTranslation("chat");
   const { setShouldDisplayFeedCard } = useKnowledgeToFeedContext();
+  const { label, Icon } = useFeedCardTrigger();
 
   return (
     <Button
-      label={t("add_document")}
-      startIcon={<LuFilePlus size={18} />}
+      label={label}
+      startIcon={<Icon size={18} />}
       endIcon={<LuChevronRight size={18} />}
       className="w-full"
       onClick={() => setShouldDisplayFeedCard(true)}
