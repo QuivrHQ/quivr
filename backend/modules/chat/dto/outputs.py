67b884798779df7ccf7d6e01cc42076f465fa7b1
gozineb <zinebe@theodo.fr>
2023-12-14 21:29:23 +0100

    🏷️ add chat dto fpr model completion

--- backend/modules/chat/dto/outputs.py
+++ backend/modules/chat/dto/outputs.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import List, Optional
 from uuid import UUID
 
 from pydantic import BaseModel
@@ -19,3 +19,32 @@ class GetChatHistoryOutput(BaseModel):
         chat_history["message_id"] = str(chat_history.get("message_id"))
 
         return chat_history
+
+
+class FunctionCall(BaseModel):
+    arguments: str
+    name: str
+
+
+class ChatCompletionMessageToolCall(BaseModel):
+    id: str
+    function: FunctionCall
+    type: str = "function"
+
+
+class CompletionMessage(BaseModel):
+    # = "assistant" | "user" | "system" | "tool"
+    role: str
+    content: str | None
+    tool_calls: Optional[List[ChatCompletionMessageToolCall]]
+
+
+class CompletionResponse(BaseModel):
+    finish_reason: str
+    message: CompletionMessage
+
+
+class BrainCompletionOutput(BaseModel):
+    messages: List[CompletionMessage]
+    question: str
+    response: CompletionResponse
