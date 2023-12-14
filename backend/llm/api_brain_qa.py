22a1cbff427b6c32e6e4c07fc8410559de3411ec
gozineb <zinebe@theodo.fr>
2023-12-14 21:25:36 +0100

    🎨 make_completion_without_streaming

--- backend/llm/api_brain_qa.py
+++ backend/llm/api_brain_qa.py
@@ -1,9 +1,7 @@
-import asyncio
 import json
 from typing import Optional
 from uuid import UUID
 
-import nest_asyncio
 from fastapi import HTTPException
 from litellm import completion
 from llm.knowledge_brain_qa import KnowledgeBrainQA
@@ -242,31 +240,158 @@ class APIBrainQA(KnowledgeBrainQA, QAInterface):
                 assistant="".join(response_tokens),
             )
 
+    def make_completion_without_streaming(
+        self,
+        messages,
+        functions,
+        brain_id: UUID,
+        recursive_count=0,
+        should_log_steps=False,
+    ):
+        if recursive_count > 5:
+            print(
+                "The assistant is having issues and took more than 5 calls to the API. Please try again later or an other instruction."
+            )
+            return
+
+        if should_log_steps:
+            print("🧠<Deciding what to do>🧠")
+
+        response = completion(
+            model=self.model,
+            temperature=self.temperature,
+            max_tokens=self.max_tokens,
+            messages=messages,
+            functions=functions,
+            stream=False,
+            function_call="auto",
+        )
+
+        response_message = response.choices[0].message
+        finish_reason = response.choices[0].finish_reason
+
+        if finish_reason == "function_call":
+            function_call = response_message.function_call
+            try:
+                arguments = json.loads(function_call.arguments)
+
+            except Exception:
+                arguments = {}
+
+            if should_log_steps:
+                print(f"🧠<Calling {brain_id} with arguments {arguments}>🧠")
+
+            try:
+                api_call_response = call_brain_api(
+                    brain_id=brain_id,
+                    user_id=self.user_id,
+                    arguments=arguments,
+                )
+            except Exception as e:
+                raise HTTPException(
+                    status_code=400,
+                    detail=f"Error while calling API: {e}",
+                )
+
+            function_name = function_call.name
+            messages.append(
+                {
+                    "role": "function",
+                    "name": function_call.name,
+                    "content": f"The function {function_name} was called and gave The following answer:(data from function) {api_call_response} (end of data from function). Don't call this function again unless there was an error or extremely necessary and asked specifically by the user.",
+                }
+            )
+
+            return self.make_completion_without_streaming(
+                messages=messages,
+                functions=functions,
+                brain_id=brain_id,
+                recursive_count=recursive_count + 1,
+                should_log_steps=should_log_steps,
+            )
+
+        if finish_reason == "stop":
+            return response_message
+
+        else:
+            print("Never ending completion")
+
     def generate_answer(
         self,
         chat_id: UUID,
         question: ChatQuestion,
         save_answer: bool = True,
     ):
-        async def a_generate_answer():
-            api_brain_question_answer: GetChatHistoryOutput = None
+        if not question.brain_id:
+            raise HTTPException(
+                status_code=400, detail="No brain id provided in the question"
+            )
 
-            async for answer in self.generate_stream(
-                chat_id, question, should_log_steps=False, save_answer=save_answer
-            ):
-                answer = answer.split("data: ")[1]
-                answer_parsed: GetChatHistoryOutput = GetChatHistoryOutput(
-                    **json.loads(answer)
+        brain = brain_service.get_brain_by_id(question.brain_id)
+
+        if not brain:
+            raise HTTPException(status_code=404, detail="Brain not found")
+
+        prompt_content = "You are a helpful assistant that can access functions to help answer questions. If there are information missing in the question, you can ask follow up questions to get more information to the user. Once all the information is available, you can call the function to get the answer."
+
+        if self.prompt_to_use:
+            prompt_content += self.prompt_to_use.content
+
+        messages = [{"role": "system", "content": prompt_content}]
+
+        history = chat_service.get_chat_history(self.chat_id)
+
+        for message in history:
+            formatted_message = [
+                {"role": "user", "content": message.user_message},
+                {"role": "assistant", "content": message.assistant},
+            ]
+            messages.extend(formatted_message)
+
+        messages.append({"role": "user", "content": question.question})
+
+        response = self.make_completion_without_streaming(
+            messages=messages,
+            functions=[get_api_brain_definition_as_json_schema(brain)],
+            brain_id=question.brain_id,
+            should_log_steps=False,
+        )
+
+        answer = response.content
+        if save_answer:
+            new_chat = chat_service.update_chat_history(
+                CreateChatHistory(
+                    **{
+                        "chat_id": chat_id,
+                        "user_message": question.question,
+                        "assistant": answer,
+                        "brain_id": question.brain_id,
+                        "prompt_id": self.prompt_to_use_id,
+                    }
                 )
-                if api_brain_question_answer is None:
-                    api_brain_question_answer = answer_parsed
-                else:
-                    api_brain_question_answer.assistant += answer_parsed.assistant
+            )
 
-            return api_brain_question_answer
-
-        nest_asyncio.apply()
-        loop = asyncio.get_event_loop()
-        result = loop.run_until_complete(a_generate_answer())
-
-        return result
+            return GetChatHistoryOutput(
+                **{
+                    "chat_id": chat_id,
+                    "user_message": question.question,
+                    "assistant": answer,
+                    "message_time": new_chat.message_time,
+                    "prompt_title": self.prompt_to_use.title
+                    if self.prompt_to_use
+                    else None,
+                    "brain_name": brain.name if brain else None,
+                    "message_id": new_chat.message_id,
+                }
+            )
+        return GetChatHistoryOutput(
+            **{
+                "chat_id": chat_id,
+                "user_message": question.question,
+                "assistant": answer,
+                "message_time": "123",
+                "prompt_title": None,
+                "brain_name": brain.name,
+                "message_id": None,
+            }
+        )
