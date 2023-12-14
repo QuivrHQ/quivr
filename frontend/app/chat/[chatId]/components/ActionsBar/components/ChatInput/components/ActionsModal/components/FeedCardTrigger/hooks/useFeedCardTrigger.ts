01c4bd258d06da57b63953d36ca5ed3135145ead
mamadoudicko <mamadoudicko100@gmail.com>
2023-12-14 11:29:16 +0100

    feat: add default feed button label

--- frontend/app/chat/[chatId]/components/ActionsBar/components/ChatInput/components/ActionsModal/components/FeedCardTrigger/hooks/useFeedCardTrigger.ts
+++ frontend/app/chat/[chatId]/components/ActionsBar/components/ChatInput/components/ActionsModal/components/FeedCardTrigger/hooks/useFeedCardTrigger.ts
@@ -1,4 +1,5 @@
-import { Fragment } from "react";
+import { useTranslation } from "react-i18next";
+import { LuFilePlus } from "react-icons/lu";
 
 import { useBrainContext } from "@/lib/context/BrainProvider/hooks/useBrainContext";
 
@@ -6,15 +7,21 @@ import { useFeedCardTriggerUtils } from "./useFeedCardTriggerUtils";
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const useFeedCardTrigger = () => {
+  const { t } = useTranslation(["chat", "brain"]);
+
+  const feedCardButtonDefaultLabel = t("chat:add_document");
+  const feedCardButtonDefaultIcon = LuFilePlus;
   const { brainTypeToIcon, brainTypeToLabel } = useFeedCardTriggerUtils();
   const { currentBrain } = useBrainContext();
 
   const isBrainTypeDefined = currentBrain?.brain_type !== undefined;
 
   return {
-    label: isBrainTypeDefined ? brainTypeToLabel[currentBrain.brain_type] : "",
+    label: isBrainTypeDefined
+      ? brainTypeToLabel[currentBrain.brain_type]
+      : feedCardButtonDefaultLabel,
     Icon: isBrainTypeDefined
       ? brainTypeToIcon[currentBrain.brain_type]
-      : Fragment,
+      : feedCardButtonDefaultIcon,
   };
 };
