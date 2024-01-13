36a3eafa183ec3593ab1e855f6b0b082940b5926
Antoine Dewez <antoinedewez@MacBook-Pro-de-Antoine.local>
2024-01-13 14:29:27 -0800

    remove right panel and reduce chat width size

--- frontend/app/chat/[chatId]/page.tsx
+++ frontend/app/chat/[chatId]/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useMenuWidth } from "@/lib/components/Menu/hooks/useMenuWidth";
 import { useKnowledgeToFeedContext } from "@/lib/context/KnowledgeToFeedProvider/hooks/useKnowledgeToFeedContext";
 import { useCustomDropzone } from "@/lib/hooks/useDropzone";
 import { cn } from "@/lib/utils";
@@ -13,38 +12,31 @@ import { useChatsList } from "./hooks/useChatsList";
 const SelectedChatPage = (): JSX.Element => {
   const { getRootProps } = useCustomDropzone();
   const { shouldDisplayFeedCard } = useKnowledgeToFeedContext();
-  const { shouldDisplayRightSideBar, OPENED_MENU_WIDTH } = useMenuWidth();
 
   useChatsList();
   useChatNotificationsSync();
 
   return (
-    <div className="flex flex-1">
-      <div
-        className={cn(
-          "flex flex-col flex-1 items-center justify-stretch w-full h-full overflow-hidden",
-          shouldDisplayFeedCard ? "bg-chat-bg-gray" : "bg-tertiary",
-          "dark:bg-black transition-colors ease-out duration-500"
-        )}
-        data-testid="chat-page"
-        {...getRootProps()}
-      >
-        <div
-          className={`flex flex-col flex-1 w-full max-w-5xl h-full dark:shadow-primary/25 overflow-hidden p-2 sm:p-4 md:p-6 lg:p-8`}
-        >
-          <div className="flex flex-1 flex-col overflow-y-auto">
-            <ChatDialogueArea />
-          </div>
-          <ActionsBar />
-        </div>
-      </div>
-      {shouldDisplayRightSideBar && (
-        <div
-          className="h-full bg-highlight"
-          style={{ width: OPENED_MENU_WIDTH }}
-        />
-      )}
-    </div>
+	<div className="flex flex-1">
+	  <div
+		className={cn(
+		  "flex flex-col flex-1 items-center justify-stretch w-full h-full overflow-hidden",
+		  shouldDisplayFeedCard ? "bg-chat-bg-gray" : "bg-tertiary",
+		  "dark:bg-black transition-colors ease-out duration-500"
+		)}
+		data-testid="chat-page"
+		{...getRootProps()}
+	  >
+		<div
+		  className={`flex flex-col flex-1 w-full max-w-4xl h-full dark:shadow-primary/25 overflow-hidden p-2 sm:p-4 md:p-6 lg:p-8`}
+		>
+		  <div className="flex flex-1 flex-col overflow-y-auto">
+			<ChatDialogueArea />
+		  </div>
+		  <ActionsBar />
+		</div>
+	  </div>
+	</div>
   );
 };
 
