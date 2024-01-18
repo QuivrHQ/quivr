5169b0d642d2f5e03d42cd3a10bad7734f50e130
Zewed <dewez.antoine2@gmail.com>
2024-01-18 13:07:04 -0800

    wip search page

--- frontend/app/search/page.tsx
+++ frontend/app/search/page.tsx
@@ -1,9 +1,16 @@
 "use client";
 
+import { SearchBar } from "@/lib/components/ui/SearchBar";
+import "./page.scss"
+
 const Search = (): JSX.Element => {
-  return (
-   <div>Search</div>
-  );
+    return (
+        <div className="search_page_container">
+            <div className="search_bar_wrapper">
+                <SearchBar />
+            </div>
+        </div>
+    );
 };
 
 export default Search;
---
+++ frontend/lib/components/ui/SearchBar.tsx
@@ -0,0 +1,5 @@
+export const SearchBar = (): JSX.Element => {
+    return (
+        <div>Search</div>
+    )
+}
