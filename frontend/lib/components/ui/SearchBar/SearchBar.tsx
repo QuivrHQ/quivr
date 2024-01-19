0670ce73fd7c7408e77383a098501f9a35426601
Zewed <dewez.antoine2@gmail.com>
2024-01-18 16:52:59 -0800

    add icon

--- frontend/lib/components/ui/SearchBar/SearchBar.tsx
+++ frontend/lib/components/ui/SearchBar/SearchBar.tsx
@@ -1,13 +1,36 @@
+import { ChangeEvent, Dispatch, SetStateAction, useState } from 'react';
+import { LuSearch } from "react-icons/lu";
+
 import styles from './SearchBar.module.scss'
 
 export const SearchBar = (): JSX.Element => {
+    const [inputValue, setInputValue]: [string, Dispatch<SetStateAction<string>>] = useState('');
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            submit()
+        }
+    };
+
+    const submit = () => {
+        console.info('submit')
+    }
+    
     return (
         <div className={styles.search_bar_wrapper}>
             <input
                 className={styles.search_input}
                 type="text"
                 placeholder="Search"
+                value={inputValue}
+                onChange={handleChange}
+                onKeyDown={handleEnter}
             />
+            <LuSearch className={`${styles.search_icon} ${!inputValue && styles.disabled}`} onClick={submit}/>
         </div>
     )
 }
