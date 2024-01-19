7e64ecd4cec8e5c4f6b0b3fb43bbed6fd6828c82
Zewed <dewez.antoine2@gmail.com>
2024-01-18 18:18:56 -0800

    hamburger

--- frontend/app/search/page.tsx
+++ frontend/app/search/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { GiHamburgerMenu } from "react-icons/gi";
+
 import { SearchBar } from "@/lib/components/ui/SearchBar/SearchBar";
 
 import styles from "./page.module.scss"
@@ -8,6 +10,7 @@ import { QuivrLogo } from "@/lib/assets/QuivrLogo";
 const Search = (): JSX.Element => {
     return (
         <div className={styles.search_page_container}>
+            <GiHamburgerMenu className={styles.hamburger} />
             <div className={styles.main_container}>
                 <div className={styles.quivr_logo_wrapper}>
                     <QuivrLogo size={80} color="black" />
@@ -15,10 +18,6 @@ const Search = (): JSX.Element => {
                         <span>Talk to </span>
                         <span className={styles.quivr_text_primary}>Quivr</span>
                     </div>
-                    {/* <p className="text-center text-4xl font-medium">
-                        {t("talk_to", { ns: "login" })}{" "}
-                        <span className="text-primary">Quivr</span>
-                    </p> */}
                 </div>
                 <div className={styles.search_bar_container}>
                     <SearchBar />
