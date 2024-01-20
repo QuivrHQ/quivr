577fbb40536a5f3862abe354676ea41145733a4d
Stan Girard <girard.stanislas@gmail.com>
2024-01-19 22:56:24 -0800

    feat: adding search (#2031)
    # Description
    
    Please include a summary of the changes and the related issue. Please
    also include relevant motivation and context.
    
    ## Checklist before requesting a review
    
    Please delete options that are not relevant.
    
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my code
    - [ ] I have commented hard-to-understand areas
    - [ ] I have ideally added tests that prove my fix is effective or that
    my feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] Any dependent changes have been merged
    
    ## Screenshots (if appropriate):
    
    ---------
    
    Co-authored-by: Zewed <dewez.antoine2@gmail.com>
    Co-authored-by: Antoine Dewez <44063631+Zewed@users.noreply.github.com>

--- frontend/app/chat/[chatId]/components/ActionsBar/components/ChatInput/components/MenuControlButton/MenuControlButton.tsx
+++ frontend/app/chat/[chatId]/components/ActionsBar/components/ChatInput/components/MenuControlButton/MenuControlButton.tsx
@@ -2,12 +2,18 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import { LuArrowLeftFromLine } from "react-icons/lu";
 
 import { useMenuContext } from "@/lib/context/MenuProvider/hooks/useMenuContext";
+import { useDevice } from "@/lib/hooks/useDevice";
 
-import styles from './MenuControlButton.module.scss'
+import styles from './MenuControlButton.module.scss';
 
 export const MenuControlButton = (): JSX.Element => {
     const { isOpened, setIsOpened } = useMenuContext();
+    const { isMobile } = useDevice();
     const Icon = isOpened ? LuArrowLeftFromLine : GiHamburgerMenu;
+
+    if (isOpened && isMobile) {
+        return <></>;
+    }
 
     return (
         <Icon
