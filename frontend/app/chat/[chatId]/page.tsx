d2bfcf5f4e51d64c22033459a86e56fa177fe0e5
Zewed <dewez.antoine2@gmail.com>
2024-01-22 20:06:18 -0800

    initial

--- frontend/app/chat/[chatId]/page.tsx
+++ frontend/app/chat/[chatId]/page.tsx
@@ -9,33 +9,34 @@ import { ChatDialogueArea } from "./components/ChatDialogueArea/ChatDialogue";
 import { useChatNotificationsSync } from "./hooks/useChatNotificationsSync";
 
 const SelectedChatPage = (): JSX.Element => {
-    const { getRootProps } = useCustomDropzone();
-    const { shouldDisplayFeedCard } = useKnowledgeToFeedContext();
+  const { getRootProps } = useCustomDropzone();
+  const { shouldDisplayFeedCard } = useKnowledgeToFeedContext();
 
-    useChatNotificationsSync();
+  useChatNotificationsSync();
 
-    return (
-        <div className="flex flex-1">
-            <div
-                className={cn(
-                    "flex flex-col flex-1 items-center justify-stretch w-full h-full overflow-hidden",
-                    shouldDisplayFeedCard ? "bg-chat-bg-gray" : "bg-ivory",
-                    "dark:bg-black transition-colors ease-out duration-500"
-                )}
-                data-testid="chat-page"
-                {...getRootProps()}
-            >
-                <div
-                    className={`flex flex-col flex-1 w-full max-w-4xl h-full dark:shadow-primary/25 overflow-hidden p-2 sm:p-4 md:p-6 lg:p-8`}
-                >
-                    <div className="flex flex-1 flex-col overflow-y-auto">
-                        <ChatDialogueArea />
-                    </div>
-                    <ActionsBar />
-                </div>
-            </div>
+  return (
+    <div className="flex flex-1">
+      <div
+        className={cn(
+          "flex flex-col flex-1 items-center justify-stretch w-full h-full overflow-hidden",
+          shouldDisplayFeedCard ? "bg-chat-bg-gray" : "bg-ivory",
+          "dark:bg-black transition-colors ease-out duration-500"
+        )}
+        data-testid="chat-page"
+        {...getRootProps()}
+      >
+        <div
+          className={`flex flex-col flex-1 w-full max-w-4xl h-full dark:shadow-primary/25 overflow-hidden p-2 sm:p-4 md:p-6 lg:p-8`}
+        >
+          <div className="flex flex-1 flex-col overflow-y-auto">
+            <ChatDialogueArea />
+          </div>
+          <ActionsBar />
         </div>
-    );
+      </div>
+      <div className="bg-red-500 h-full w-64"></div>
+    </div>
+  );
 };
 
 export default SelectedChatPage;
