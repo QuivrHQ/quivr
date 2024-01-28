7617be99d2769c8a3e9fa71aef8607b8a676131d
Zewed <dewez.antoine2@gmail.com>
2024-01-27 16:37:32 -0800

    wip

--- frontend/app/chat/[chatId]/components/ChatDialogueArea/components/ChatDialogue/components/QADisplay/components/MessageRow/components/QuestionPrompt/QuestionPrompt.tsx
+++ frontend/app/chat/[chatId]/components/ChatDialogueArea/components/ChatDialogue/components/QADisplay/components/MessageRow/components/QuestionPrompt/QuestionPrompt.tsx
@@ -16,7 +16,7 @@ export const QuestionPrompt = ({
 
   return (
     <div data-testid="prompt-tags" className={styles.prompt_name_wrapper}>
-      <Icon name="hastag" color="primary" size="normal" />
+      <Icon name="hastag" color="black" size="normal" />
       <span>{promptName}</span>
     </div>
   );
