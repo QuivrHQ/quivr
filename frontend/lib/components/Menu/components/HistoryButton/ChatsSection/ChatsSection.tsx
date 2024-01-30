de794de59dd87aa1dbb0a83a277a05df8d17a144
Zewed <dewez.antoine2@gmail.com>
2024-01-29 16:40:56 -0800

    refacto

---
+++ frontend/lib/components/Menu/components/HistoryButton/ChatsSection/ChatsSection.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+import { ChatEntity } from "@/app/chat/[chatId]/types";
+
+import styles from "./ChatsSection.module.scss";
+
+type ChatSectionProps = {
+  chats: ChatEntity[]; // replace ChatType with the actual type of your chats
+  title: string;
+};
+
+export const ChatsSection = (props: ChatSectionProps): JSX.Element => {
+  if (props.chats.length === 0) {
+    return <></>;
+  }
+
+  return (
+    <div>
+      <div>{props.title}</div>
+      <div className={styles.chats_wrapper}>
+        {props.chats.map((chat) => (
+          <Link href={`/chat/${chat.chat_id}`} key={chat.chat_id}>
+            <div>{chat.chat_name.trim()}</div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
--- frontend/lib/components/Menu/components/HistoryButton/HistoryButton.tsx
+++ frontend/lib/components/Menu/components/HistoryButton/HistoryButton.tsx
@@ -1,11 +1,12 @@
 import { isToday } from "date-fns";
-import Link from "next/link";
 import { useTranslation } from "react-i18next";
 
 import { FoldableSection } from "@/lib/components/ui/FoldableSection/FoldableSection";
 import { useChatsContext } from "@/lib/context/ChatsProvider/hooks/useChatsContext";
 
+import { ChatsSection } from "./ChatsSection/ChatsSection";
 import styles from "./HistoryButton.module.scss";
+import { isWithinLast30Days, isWithinLast7Days, isYesterday } from "./utils";
 
 export const HistoryButton = (): JSX.Element => {
   const { allChats } = useChatsContext();
@@ -13,29 +14,23 @@ export const HistoryButton = (): JSX.Element => {
   const todayChats = allChats.filter((chat) =>
     isToday(new Date(chat.creation_time))
   );
-  //   const yesterdayChats = allChats.filter((chat) =>
-  //     isYesterday(new Date(chat.creation_time))
-  //   );
-  //   const last7DaysChats = allChats.filter((chat) =>
-  //     isWithinLast7Days(new Date(chat.creation_time))
-  //   );
-  //   const last30DaysChats = allChats.filter((chat) =>
-  //     isWithinLast30Days(new Date(chat.creation_time))
-  //   );
+  const yesterdayChats = allChats.filter((chat) =>
+    isYesterday(new Date(chat.creation_time))
+  );
+  const last7DaysChats = allChats.filter((chat) =>
+    isWithinLast7Days(new Date(chat.creation_time))
+  );
+  const last30DaysChats = allChats.filter((chat) =>
+    isWithinLast30Days(new Date(chat.creation_time))
+  );
 
   return (
     <FoldableSection label={t("history")} icon="history" darkMode={true}>
       <div className={styles.history_content_wrapper}>
-        {todayChats.length > 0 && (
-          <div className={styles.section_title}>{t("today")}</div>
-        )}
-        <div className={styles.chats_wrapper}>
-          {todayChats.map((chat) => (
-            <Link href={`/chat/${chat.chat_id}`} key={chat.chat_id}>
-              <div>{chat.chat_name.trim()}</div>
-            </Link>
-          ))}
-        </div>
+        <ChatsSection chats={todayChats} title={t("today")} />
+        <ChatsSection chats={yesterdayChats} title={t("yesterday")} />
+        <ChatsSection chats={last7DaysChats} title={t("last7Days")} />
+        <ChatsSection chats={last30DaysChats} title={t("last30Days")} />
       </div>
     </FoldableSection>
   );
