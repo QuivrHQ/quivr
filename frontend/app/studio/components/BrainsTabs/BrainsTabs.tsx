6e6d522e692ac2f49c750146d1e0efbf4588000d
Zewed <dewez.antoine2@gmail.com>
2024-02-04 14:10:22 -0800

    add

--- frontend/app/studio/components/BrainsTabs/BrainsTabs.tsx
+++ frontend/app/studio/components/BrainsTabs/BrainsTabs.tsx
@@ -1,24 +1,12 @@
-import { useTranslation } from "react-i18next";
-
 import Spinner from "@/lib/components/ui/Spinner";
-import { Tabs, TabsContent, TabsList } from "@/lib/components/ui/Tabs";
 
 import { BrainSearchBar } from "./components/BrainSearchBar";
 import { BrainsList } from "./components/BrainsList";
 import { useBrainsTabs } from "./hooks/useBrainsTabs";
 
-import { StyledTabsTrigger } from "../StyledTabsTrigger";
-
 export const BrainsTabs = (): JSX.Element => {
-  const { t } = useTranslation(["brain", "translation"]);
-  const {
-    searchQuery,
-    isFetchingBrains,
-    setSearchQuery,
-    brains,
-    privateBrains,
-    publicBrains,
-  } = useBrainsTabs();
+  const { searchQuery, isFetchingBrains, setSearchQuery, brains } =
+    useBrainsTabs();
 
   if (isFetchingBrains && brains.length === 0) {
     return (
@@ -29,33 +17,13 @@ export const BrainsTabs = (): JSX.Element => {
   }
 
   return (
-    <Tabs defaultValue="all" className="flex flex-col">
-      <TabsList className="flex flex-row justify-start gap-2 border-b-2 rounded-none pb-3">
-        <StyledTabsTrigger value="all">
-          {t("translation:all")}
-        </StyledTabsTrigger>
-        <StyledTabsTrigger value="private" className="capitalize">
-          {t("private_brain_label")}
-        </StyledTabsTrigger>
-        <StyledTabsTrigger value="public" className="capitalize">
-          {t("public_brain_label")}
-        </StyledTabsTrigger>
-        <div className="w-full flex justify-end">
-          <BrainSearchBar
-            searchQuery={searchQuery}
-            setSearchQuery={setSearchQuery}
-          />
-        </div>
-      </TabsList>
-      <TabsContent value="all">
-        <BrainsList brains={brains} />
-      </TabsContent>
-      <TabsContent value="private">
-        <BrainsList brains={privateBrains} />
-      </TabsContent>
-      <TabsContent value="public">
-        <BrainsList brains={publicBrains} />
-      </TabsContent>
-    </Tabs>
+    <div>
+      <BrainSearchBar
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+      />
+
+      <BrainsList brains={brains} />
+    </div>
   );
 };
--- frontend/app/studio/page.tsx
+++ frontend/app/studio/page.tsx
@@ -6,6 +6,7 @@ import { Icon } from "@/lib/components/ui/Icon/Icon";
 import { Tabs } from "@/lib/components/ui/Tabs/Tabs";
 import { Tab } from "@/lib/types/Tab";
 
+import { BrainsTabs } from "./components/BrainsTabs/BrainsTabs";
 import styles from "./page.module.scss";
 
 const Studio = (): JSX.Element => {
@@ -37,6 +38,7 @@ const Studio = (): JSX.Element => {
         <h1 className={styles.title}>Studio</h1>
       </div>
       <Tabs tabList={studioTabs} />
+      {selectedTab === "Manage my brains" && <BrainsTabs />}
     </div>
   );
 };
