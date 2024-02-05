f88de7fff688f281bb6894c4f8966fcecb3754b6
Zewed <dewez.antoine2@gmail.com>
2024-02-04 20:13:44 -0800

    wip

--- frontend/app/studio/components/BrainsTabs/components/BrainItem/BrainItem.tsx
+++ frontend/app/studio/components/BrainsTabs/components/BrainItem/BrainItem.tsx
@@ -1,6 +1,10 @@
 import Link from "next/link";
 
+import { DeleteOrUnsubscribeConfirmationModal } from "@/app/studio/[brainId]/components/BrainManagementTabs/components/Modals/DeleteOrUnsubscribeConfirmationModal";
+import { useBrainManagementTabs } from "@/app/studio/[brainId]/components/BrainManagementTabs/hooks/useBrainManagementTabs";
+import { getBrainPermissions } from "@/app/studio/[brainId]/components/BrainManagementTabs/utils/getBrainPermissions";
 import Icon from "@/lib/components/ui/Icon/Icon";
+import { useBrainContext } from "@/lib/context/BrainProvider/hooks/useBrainContext";
 import { MinimalBrainForUser } from "@/lib/context/BrainProvider/types";
 
 import styles from "./BrainItem.module.scss";
@@ -11,6 +15,20 @@ type BrainItemProps = {
 };
 
 export const BrainItem = ({ brain, even }: BrainItemProps): JSX.Element => {
+  const {
+    handleUnsubscribeOrDeleteBrain,
+    isDeleteOrUnsubscribeModalOpened,
+    setIsDeleteOrUnsubscribeModalOpened,
+    isDeleteOrUnsubscribeRequestPending,
+  } = useBrainManagementTabs();
+
+  const { allBrains } = useBrainContext();
+
+  const { isOwnedByCurrentUser } = getBrainPermissions({
+    brainId: brain.id,
+    userAccessibleBrains: allBrains,
+  });
+
   return (
     <div
       className={`
@@ -22,7 +40,22 @@ export const BrainItem = ({ brain, even }: BrainItemProps): JSX.Element => {
         <span className={styles.name}>{brain.name}</span>
         <span className={styles.description}>{brain.description}</span>
       </Link>
-      <Icon name="delete" size="normal" color="dangerous" handleHover={true} />
+      <Icon
+        name="delete"
+        size="normal"
+        color="dangerous"
+        handleHover={true}
+        onClick={() => setIsDeleteOrUnsubscribeModalOpened(true)}
+      />
+      <DeleteOrUnsubscribeConfirmationModal
+        isOpen={isDeleteOrUnsubscribeModalOpened}
+        setOpen={setIsDeleteOrUnsubscribeModalOpened}
+        onConfirm={() => void handleUnsubscribeOrDeleteBrain()}
+        isOwnedByCurrentUser={isOwnedByCurrentUser}
+        isDeleteOrUnsubscribeRequestPending={
+          isDeleteOrUnsubscribeRequestPending
+        }
+      />
     </div>
   );
 };
