81df99861f267ac82ec2310112994afb476dfb45
Zewed <dewez.antoine2@gmail.com>
2024-02-08 23:34:38 -0800

    add from website url

--- frontend/app/chat/[chatId]/components/ActionsBar/components/KnowledgeToFeed/components/FromWebsites/FromWebsites.tsx
+++ frontend/app/chat/[chatId]/components/ActionsBar/components/KnowledgeToFeed/components/FromWebsites/FromWebsites.tsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+
+import { TextInput } from "@/lib/components/ui/TextInput/TextInput";
+
 import styles from "./FromWebsites.module.scss";
 
 export const FromWebsites = (): JSX.Element => {
-  return <div className={styles.from_document_wrapper}>W</div>;
+  const [websiteValue, setWebsiteValue] = useState<string>("");
+
+  return (
+    <div className={styles.from_document_wrapper}>
+      <TextInput
+        label="Enter a website URL"
+        setInputValue={setWebsiteValue}
+        inputValue={websiteValue}
+      />
+    </div>
+  );
 };
