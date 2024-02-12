a3a0a58a6b70dca2678ecae31bb5d63a6ddc13c9
Zewed <dewez.antoine2@gmail.com>
2024-02-12 11:00:21 -0800

    initial commit

--- frontend/lib/components/AddBrainModal/components/AddBrainSteps/AddBrainSteps.tsx
+++ frontend/lib/components/AddBrainModal/components/AddBrainSteps/AddBrainSteps.tsx
@@ -3,10 +3,6 @@ import { useTranslation } from "react-i18next";
 import { Modal } from "@/lib/components/ui/Modal/Modal";
 
 import { useBrainCreationContext } from "./brainCreation-provider";
-import { BrainKnowledgeStep } from "./components/BrainKnowledgeStep/BrainKnowledgeStep";
-import { BrainParamsStep } from "./components/BrainParamsStep/BrainParamsStep";
-import { BrainTypeSelectionStep } from "./components/BrainTypeSelectionStep/BrainTypeSelectionStep";
-import { Stepper } from "./components/Stepper/Stepper";
 
 export const AddBrainSteps = (): JSX.Element => {
   const { t } = useTranslation(["translation", "brain", "config"]);
@@ -20,25 +16,8 @@ export const AddBrainSteps = (): JSX.Element => {
       desc={t("newBrainSubtitle", { ns: "brain" })}
       isOpen={isBrainCreationModalOpened}
       setOpen={setIsBrainCreationModalOpened}
+      bigModal={true}
       CloseTrigger={<div />}
-    >
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-        }}
-        className="my-10 flex flex-col items-center gap-2 justify-center"
-      >
-        <Stepper />
-        <BrainTypeSelectionStep
-          onCancelBrainCreation={() => setIsBrainCreationModalOpened(false)}
-        />
-        <BrainParamsStep
-          onCancelBrainCreation={() => setIsBrainCreationModalOpened(false)}
-        />
-        <BrainKnowledgeStep
-          onCancelBrainCreation={() => setIsBrainCreationModalOpened(false)}
-        />
-      </form>
-    </Modal>
+    ></Modal>
   );
 };
