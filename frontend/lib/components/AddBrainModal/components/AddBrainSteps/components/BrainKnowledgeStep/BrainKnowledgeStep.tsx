5bce0071e360d402da28d9059cad918c455e1f4b
Zewed <dewez.antoine2@gmail.com>
2024-02-14 09:54:34 -0800

    wip

--- frontend/lib/components/AddBrainModal/components/AddBrainSteps/components/BrainKnowledgeStep/BrainKnowledgeStep.tsx
+++ frontend/lib/components/AddBrainModal/components/AddBrainSteps/components/BrainKnowledgeStep/BrainKnowledgeStep.tsx
@@ -1,25 +1,53 @@
+import { KnowledgeToFeed } from "@/app/chat/[chatId]/components/ActionsBar/components";
 import { ApiRequestDefinition } from "@/lib/components/ApiRequestDefinition";
+import QuivrButton from "@/lib/components/ui/QuivrButton/QuivrButton";
 import { BrainType } from "@/lib/types/BrainConfig";
 
+import styles from "./BrainKnowledgeStep.module.scss";
 import { CompositeBrainConnections } from "./components/CompositeBrainConnections/CompositeBrainConnections";
-import { KnowledgeToFeedInput } from "./components/KnowledgeToFeedInput";
 import { useBrainKnowledgeStep } from "./hooks/useBrainKnowledgeStep";
 
 import { useBrainCreationSteps } from "../../hooks/useBrainCreationSteps";
 
 export const BrainKnowledgeStep = (): JSX.Element => {
   const { brainType } = useBrainKnowledgeStep();
-  const { currentStepIndex } = useBrainCreationSteps();
+  const { currentStepIndex, goToPreviousStep } = useBrainCreationSteps();
 
   const brainTypeToKnowledgeComponent: Record<BrainType, JSX.Element> = {
-    doc: <KnowledgeToFeedInput />,
+    doc: <KnowledgeToFeed hideBrainSelector={true} />,
     api: <ApiRequestDefinition />,
     composite: <CompositeBrainConnections />,
+  };
+
+  const previous = (): void => {
+    goToPreviousStep();
+  };
+
+  const feed = (): void => {
+    console.info("hey");
   };
 
   if (currentStepIndex !== 2 || !brainType) {
     return <></>;
   }
 
-  return <>{brainTypeToKnowledgeComponent[brainType]}</>;
+  return (
+    <div className={styles.brain_knowledge_wrapper}>
+      {brainTypeToKnowledgeComponent[brainType]}
+      <div className={styles.buttons_wrapper}>
+        <QuivrButton
+          label="Previous step"
+          color="primary"
+          iconName="chevronLeft"
+          onClick={previous}
+        />
+        <QuivrButton
+          label="Create brain"
+          color="primary"
+          iconName="add"
+          onClick={feed}
+        />
+      </div>
+    </div>
+  );
 };
