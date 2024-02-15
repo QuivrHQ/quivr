5f22b34576b07843ec0f2e380c8ebf95160d2ccd
Zewed <dewez.antoine2@gmail.com>
2024-02-14 19:43:26 -0800

    done

--- frontend/app/studio/components/BrainsTabs/components/BrainItem/BrainItem.tsx
+++ frontend/app/studio/components/BrainsTabs/components/BrainItem/BrainItem.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { DeleteOrUnsubscribeConfirmationModal } from "@/app/studio/[brainId]/components/BrainManagementTabs/components/Modals/DeleteOrUnsubscribeConfirmationModal";
 import { useBrainManagementTabs } from "@/app/studio/[brainId]/components/BrainManagementTabs/hooks/useBrainManagementTabs";
@@ -16,13 +16,15 @@ type BrainItemProps = {
 };
 
 export const BrainItem = ({ brain, even }: BrainItemProps): JSX.Element => {
+  const [optionsOpened, setOptionsOpened] = useState<boolean>(false);
+  const [deleteHovered, setDeleteHovered] = useState<boolean>(false);
+  const [editHovered, setEditHovered] = useState<boolean>(false);
   const {
     handleUnsubscribeOrDeleteBrain,
     isDeleteOrUnsubscribeModalOpened,
     setIsDeleteOrUnsubscribeModalOpened,
     isDeleteOrUnsubscribeRequestPending,
   } = useBrainManagementTabs(brain.id);
-  const [isHovered, setIsHovered] = useState<boolean>(false);
 
   const { allBrains } = useBrainContext();
 
@@ -31,33 +33,79 @@ export const BrainItem = ({ brain, even }: BrainItemProps): JSX.Element => {
     userAccessibleBrains: allBrains,
   });
 
+  const optionsRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        optionsRef.current &&
+        !optionsRef.current.contains(event.target as Node)
+      ) {
+        setOptionsOpened(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div
       className={`
       ${even ? styles.even : styles.odd}
       ${styles.brain_item_wrapper}
       `}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
     >
       <Link className={styles.brain_info_wrapper} href={`/studio/${brain.id}`}>
         <span className={styles.name}>{brain.name}</span>
         <span className={styles.description}>{brain.description}</span>
       </Link>
-      <Icon
-        name="edit"
-        size="normal"
-        color="black"
-        hovered={isHovered}
-        onClick={() => (window.location.href = `/studio/${brain.id}`)}
-      />
-      <Icon
-        name="delete"
-        size="normal"
-        color="dangerous"
-        handleHover={true}
-        onClick={() => setIsDeleteOrUnsubscribeModalOpened(true)}
-      />
+
+      <div>
+        <div
+          onClick={(event: React.MouseEvent<HTMLElement>) => {
+            event.stopPropagation();
+            setOptionsOpened(!optionsOpened);
+          }}
+        >
+          <Icon name="options" size="normal" color="black" handleHover={true} />
+        </div>
+        {optionsOpened && (
+          <div className={styles.options_menu} ref={optionsRef}>
+            <div
+              className={styles.option}
+              onClick={() => setIsDeleteOrUnsubscribeModalOpened(true)}
+              onMouseEnter={() => setDeleteHovered(true)}
+              onMouseLeave={() => setDeleteHovered(false)}
+            >
+              <span>Delete</span>
+              <Icon
+                name="delete"
+                size="normal"
+                color="dangerous"
+                hovered={deleteHovered}
+              />
+            </div>
+            <div
+              className={styles.option}
+              onClick={() => (window.location.href = `/studio/${brain.id}`)}
+              onMouseEnter={() => setEditHovered(true)}
+              onMouseLeave={() => setEditHovered(false)}
+            >
+              <span>Edit</span>
+              <Icon
+                name="edit"
+                size="normal"
+                color="black"
+                hovered={editHovered}
+              />
+            </div>
+          </div>
+        )}
+      </div>
       <DeleteOrUnsubscribeConfirmationModal
         isOpen={isDeleteOrUnsubscribeModalOpened}
         setOpen={setIsDeleteOrUnsubscribeModalOpened}
