2c71e0edc76369710cf79cbffd297b2b7ff3b8bd
Stan Girard <girard.stanislas@gmail.com>
2024-02-18 23:05:13 -0800

    feat: Add pricing calculation method to GPT4Brain class and update user usage in chat controller (#2210)
    This pull request adds a new method called `calculate_pricing` to the
    `GPT4Brain` class in the codebase. This method calculates the pricing
    for the GPT4Brain model. Additionally, the user usage in the chat
    controller has been updated to include the new pricing calculation
    method.

--- backend/modules/chat/controller/chat/utils.py
+++ backend/modules/chat/controller/chat/utils.py
@@ -31,44 +31,39 @@ class NullableUUID(UUID):
 
 def find_model_and_generate_metadata(
     chat_id: UUID,
-    brain,
+    brain_model: str,
     user_settings,
     models_settings,
-    metadata_brain,
 ):
-    # Add metadata_brain to metadata
-    metadata = {}
-    metadata = {**metadata, **metadata_brain}
-    follow_up_questions = chat_service.get_follow_up_question(chat_id)
-    metadata["follow_up_questions"] = follow_up_questions
+
     # Default model is gpt-3.5-turbo-0125
     default_model = "gpt-3.5-turbo-0125"
     model_to_use = LLMModels(  # TODO Implement default models in database
         name=default_model, price=1, max_input=4000, max_output=1000
     )
 
-    logger.info("Brain model: %s", brain.model)
+    logger.debug("Brain model: %s", brain_model)
 
     # If brain.model is None, set it to the default_model
-    if brain.model is None:
-        brain.model = default_model
+    if brain_model is None:
+        brain_model = default_model
 
     is_brain_model_available = any(
-        brain.model == model_dict.get("name") for model_dict in models_settings
+        brain_model == model_dict.get("name") for model_dict in models_settings
     )
 
-    is_user_allowed_model = brain.model in user_settings.get(
+    is_user_allowed_model = brain_model in user_settings.get(
         "models", [default_model]
     )  # Checks if the model is available in the list of models
 
-    logger.info(f"Brain model: {brain.model}")
-    logger.info(f"User models: {user_settings.get('models', [])}")
-    logger.info(f"Model available: {is_brain_model_available}")
-    logger.info(f"User allowed model: {is_user_allowed_model}")
+    logger.debug(f"Brain model: {brain_model}")
+    logger.debug(f"User models: {user_settings.get('models', [])}")
+    logger.debug(f"Model available: {is_brain_model_available}")
+    logger.debug(f"User allowed model: {is_user_allowed_model}")
 
     if is_brain_model_available and is_user_allowed_model:
         # Use the model from the brain
-        model_to_use.name = brain.model
+        model_to_use.name = brain_model
         for model_dict in models_settings:
             if model_dict.get("name") == model_to_use.name:
                 model_to_use.price = model_dict.get("price")
@@ -76,19 +71,12 @@ def find_model_and_generate_metadata(
                 model_to_use.max_output = model_dict.get("max_output")
                 break
 
-    metadata["model"] = model_to_use.name
-    metadata["max_tokens"] = model_to_use.max_output
-    metadata["max_input"] = model_to_use.max_input
-
     logger.info(f"Model to use: {model_to_use}")
-    logger.info(f"Metadata: {metadata}")
 
-    return model_to_use, metadata
+    return model_to_use
 
 
-def check_user_requests_limit(
-    usage: UserUsage, user_settings, models_settings, model_name: str
-):
+def update_user_usage(usage: UserUsage, user_settings, cost: int = 100):
     """Checks the user requests limit.
     It checks the user requests limit and raises an exception if the user has reached the limit.
     By default, the user has a limit of 100 requests per month. The limit can be increased by upgrading the plan.
@@ -105,18 +93,13 @@ def check_user_requests_limit(
     date = time.strftime("%Y%m%d")
 
     monthly_chat_credit = user_settings.get("monthly_chat_credit", 100)
-    daily_user_count = usage.get_user_monthly_usage(date)
-    user_choosen_model_price = 1000
+    montly_usage = usage.get_user_monthly_usage(date)
 
-    for model_setting in models_settings:
-        if model_setting["name"] == model_name:
-            user_choosen_model_price = model_setting["price"]
-
-    if int(daily_user_count + user_choosen_model_price) > int(monthly_chat_credit):
+    if int(montly_usage + cost) > int(monthly_chat_credit):
         raise HTTPException(
             status_code=429,  # pyright: ignore reportPrivateUsage=none
             detail=f"You have reached your monthly chat limit of {monthly_chat_credit} requests per months. Please upgrade your plan to increase your daily chat limit.",
         )
     else:
-        usage.handle_increment_user_request_count(date, user_choosen_model_price)
+        usage.handle_increment_user_request_count(date, cost)
         pass
