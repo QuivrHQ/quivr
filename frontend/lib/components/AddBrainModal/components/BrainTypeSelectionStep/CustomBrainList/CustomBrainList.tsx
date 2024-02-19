f78ea61fa9a5513c93d588580bce976fa6d098b1
Zewed <dewez.antoine2@gmail.com>
2024-02-18 23:36:28 -0800

    wip

--- frontend/lib/components/AddBrainModal/components/BrainTypeSelectionStep/CustomBrainList/CustomBrainList.tsx
+++ frontend/lib/components/AddBrainModal/components/BrainTypeSelectionStep/CustomBrainList/CustomBrainList.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 import { IntegrationBrains } from "@/lib/api/brain/types";
+import { Icon } from "@/lib/components/ui/Icon/Icon";
 
 import styles from "./CustomBrainList.module.scss";
 
@@ -13,14 +14,26 @@ export const CustomBrainList = ({
     <div>
       {customBrainList.map((brain) => {
         return (
-          <div className={styles.brain_card_wrapper} key={brain.id}>
-            <span className={styles.brain_title}>{brain.integration_name}</span>
-            <Image
-              src={brain.integration_logo_url}
-              alt={brain.integration_name}
-              width={70}
-              height={70}
-            />
+          <div className={styles.card_container} key={brain.id}>
+            <div className={styles.icon_help}>
+              <Icon
+                color="black"
+                handleHover={true}
+                size="normal"
+                name="help"
+              />
+            </div>
+            <div className={styles.brain_card_wrapper}>
+              <span className={styles.brain_title}>
+                {brain.integration_name}
+              </span>
+              <Image
+                src={brain.integration_logo_url}
+                alt={brain.integration_name}
+                width={70}
+                height={70}
+              />
+            </div>
           </div>
         );
       })}
