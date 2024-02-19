bace002c644c0de3d5d27050f5a0248038d3e826
Zewed <dewez.antoine2@gmail.com>
2024-02-19 08:55:35 -0800

    wip

--- frontend/lib/components/AddBrainModal/components/BrainTypeSelectionStep/CustomBrainList/CustomBrainList.tsx
+++ frontend/lib/components/AddBrainModal/components/BrainTypeSelectionStep/CustomBrainList/CustomBrainList.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 import { IntegrationBrains } from "@/lib/api/brain/types";
 import { Icon } from "@/lib/components/ui/Icon/Icon";
+import Tooltip from "@/lib/components/ui/Tooltip/Tooltip";
 
 import styles from "./CustomBrainList.module.scss";
 
@@ -16,12 +17,16 @@ export const CustomBrainList = ({
         return (
           <div className={styles.card_container} key={brain.id}>
             <div className={styles.icon_help}>
-              <Icon
-                color="black"
-                handleHover={true}
-                size="normal"
-                name="help"
-              />
+              <Tooltip tooltip={brain.description}>
+                <div>
+                  <Icon
+                    color="black"
+                    handleHover={true}
+                    size="normal"
+                    name="help"
+                  />
+                </div>
+              </Tooltip>
             </div>
             <div className={styles.brain_card_wrapper}>
               <span className={styles.brain_title}>
---
+++ frontend/lib/components/ui/Tooltip/Tooltip.tsx
@@ -0,0 +1,49 @@
+"use client";
+import * as TooltipPrimitive from "@radix-ui/react-tooltip";
+import { AnimatePresence, motion } from "framer-motion";
+import { ReactNode, useState } from "react";
+
+import styles from "./Tooltip.module.scss";
+
+interface TooltipProps {
+  children?: ReactNode;
+  tooltip?: ReactNode;
+}
+
+const Tooltip = ({ children, tooltip }: TooltipProps): JSX.Element => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <TooltipPrimitive.Provider>
+      <TooltipPrimitive.Root onOpenChange={setOpen} open={open}>
+        <TooltipPrimitive.Trigger asChild>{children}</TooltipPrimitive.Trigger>
+        <AnimatePresence>
+          {open && (
+            <TooltipPrimitive.Portal forceMount>
+              <TooltipPrimitive.Content sideOffset={5} asChild>
+                <motion.div
+                  initial={{ y: 10, opacity: 0 }}
+                  animate={{
+                    y: 0,
+                    opacity: 1,
+                    transition: { ease: "easeOut", duration: 0.1 },
+                  }}
+                  exit={{
+                    y: 10,
+                    opacity: 0,
+                    transition: { ease: "easeIn", duration: 0.1 },
+                  }}
+                  className={styles.tooltip_content_wrapper}
+                >
+                  {tooltip}
+                </motion.div>
+              </TooltipPrimitive.Content>
+            </TooltipPrimitive.Portal>
+          )}
+        </AnimatePresence>
+      </TooltipPrimitive.Root>
+    </TooltipPrimitive.Provider>
+  );
+};
+
+export default Tooltip;
