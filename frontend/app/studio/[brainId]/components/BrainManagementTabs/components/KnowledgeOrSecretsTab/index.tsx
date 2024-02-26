52c4f9b5a88342d17baaf3267a52f7949a896b63
Zewed <dewez.antoine2@gmail.com>
2024-02-25 18:20:46 -0800

    wip

--- frontend/app/studio/[brainId]/components/BrainManagementTabs/components/KnowledgeOrSecretsTab/index.tsx
+++ frontend/app/studio/[brainId]/components/BrainManagementTabs/components/KnowledgeOrSecretsTab/index.tsx
@@ -1,11 +1,9 @@
 "use client";
 import { UUID } from "crypto";
-import { useTranslation } from "react-i18next";
 
+import { KnowledgeToFeed } from "@/app/chat/[chatId]/components/ActionsBar/components";
 import { ApiBrainSecretsInputs } from "@/lib/components/ApiBrainSecretsInputs/ApiBrainSecretsInputs";
-import { Divider } from "@/lib/components/ui/Divider";
 
-import { AddKnowledge } from "./components/AddKnowledge/AddKnowledge";
 import { AddedKnowledge } from "./components/AddedKnowledge/AddedKnowledge";
 
 import { useBrainFetcher } from "../../hooks/useBrainFetcher";
@@ -19,10 +17,10 @@ export const KnowledgeOrSecretsTab = ({
   brainId,
   hasEditRights,
 }: KnowledgeOrSecretsTabProps): JSX.Element => {
-  const { t } = useTranslation(["translation", "explore", "config"]);
   const { brain } = useBrainFetcher({
     brainId,
   });
+
   if (!hasEditRights) {
     return <NoAccess />;
   }
@@ -32,23 +30,9 @@ export const KnowledgeOrSecretsTab = ({
   }
 
   return (
-    <main>
-      <section className="w-full outline-none pt-10 flex flex-col gap-5 items-center justify-center p-6">
-        <div className="flex flex-col items-center justify-center">
-          <h1 className="text-3xl font-bold text-center">
-            {t("title", { ns: "explore" })}
-          </h1>
-          <h2 className="opacity-50">{t("subtitle", { ns: "explore" })}</h2>
-        </div>
-        <Divider text={t("Upload")} />
-        <AddKnowledge />
-        <Divider
-          text={t("knowledge", {
-            ns: "config",
-          })}
-        />
-        <AddedKnowledge brainId={brainId} />
-      </section>
-    </main>
+    <>
+      <KnowledgeToFeed hideBrainSelector={true} />
+      <AddedKnowledge brainId={brainId} />
+    </>
   );
 };
--- frontend/app/studio/[brainId]/page.tsx
+++ frontend/app/studio/[brainId]/page.tsx
@@ -1,33 +1,24 @@
 "use client";
 
-import Link from "next/link";
-import { useTranslation } from "react-i18next";
-import { LuBrain, LuChevronLeftCircle } from "react-icons/lu";
-
-import Button from "@/lib/components/ui/Button";
+import PageHeader from "@/lib/components/PageHeader/PageHeader";
 
 import { BrainManagementTabs } from "./components";
 import { useBrainManagement } from "./hooks/useBrainManagement";
+import styles from "./page.module.scss";
 
 const BrainsManagement = (): JSX.Element => {
-  const { t } = useTranslation(["translation"]);
   const { brain } = useBrainManagement();
 
+  if (!brain) {
+    return <></>;
+  }
+
   return (
-    <div className="flex flex-col w-full p-5 lg:p-20 bg-highlight">
-      <div>
-        <Link href="/studio">
-          <Button variant="tertiary" className="p-0">
-            <LuChevronLeftCircle className="text-primary" />
-            {t("previous")}
-          </Button>
-        </Link>
+    <div className={styles.brain_management_wrapper}>
+      <PageHeader iconName="brain" label={brain.name} buttons={[]} />
+      <div className={styles.content_wrapper}>
+        <BrainManagementTabs />
       </div>
-      <div className="w-full justify-center flex items-center gap-2">
-        <LuBrain size={25} className="text-primary" />
-        <span className="text-3xl font-semibold">{brain?.name}</span>
-      </div>
-      <BrainManagementTabs />
     </div>
   );
 };
