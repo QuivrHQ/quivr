892bf7f55645fa3fcb50142457e76d0967291f76
Zewed <dewez.antoine2@gmail.com>
2024-02-27 18:34:06 -0800

    fix

---
+++ frontend/lib/components/UploadDocumentModal/hooks/useFeedBrain.ts
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+
+import { useChatApi } from "@/lib/api/chat/useChatApi";
+import { useBrainContext } from "@/lib/context/BrainProvider/hooks/useBrainContext";
+import { useKnowledgeToFeedContext } from "@/lib/context/KnowledgeToFeedProvider/hooks/useKnowledgeToFeedContext";
+import { useToast } from "@/lib/hooks";
+import { useUrlBrain } from "@/lib/hooks/useBrainIdFromUrl";
+
+import { useFeedBrainHandler } from "./useFeedBrainHandler";
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const useFeedBrain = ({
+  dispatchHasPendingRequests,
+  closeFeedInput,
+}: {
+  dispatchHasPendingRequests?: () => void;
+  closeFeedInput?: () => void;
+}) => {
+  const { publish } = useToast();
+  const { t } = useTranslation(["upload"]);
+  let { brainId } = useUrlBrain();
+  const { currentBrainId } = useBrainContext();
+  const { setKnowledgeToFeed, knowledgeToFeed, setShouldDisplayFeedCard } =
+    useKnowledgeToFeedContext();
+  const [hasPendingRequests, setHasPendingRequests] = useState(false);
+  const { handleFeedBrain } = useFeedBrainHandler();
+
+  const { createChat, deleteChat } = useChatApi();
+
+  const feedBrain = async (): Promise<void> => {
+    brainId ??= currentBrainId ?? undefined;
+    if (brainId === undefined) {
+      publish({
+        variant: "danger",
+        text: t("selectBrainFirst"),
+      });
+
+      return;
+    }
+
+    if (knowledgeToFeed.length === 0) {
+      publish({
+        variant: "danger",
+        text: t("addFiles"),
+      });
+
+      return;
+    }
+
+    //TODO: Modify backend archi to avoid creating a chat for each feed action
+    const currentChatId = (await createChat("New Chat")).chat_id;
+
+    try {
+      dispatchHasPendingRequests?.();
+      closeFeedInput?.();
+      setHasPendingRequests(true);
+      setShouldDisplayFeedCard(false);
+      await handleFeedBrain({
+        brainId,
+        chatId: currentChatId,
+      });
+
+      setKnowledgeToFeed([]);
+    } catch (e) {
+      publish({
+        variant: "danger",
+        text: JSON.stringify(e),
+      });
+    } finally {
+      setHasPendingRequests(false);
+      await deleteChat(currentChatId);
+    }
+  };
+
+  return {
+    feedBrain,
+    hasPendingRequests,
+    setHasPendingRequests,
+  };
+};
