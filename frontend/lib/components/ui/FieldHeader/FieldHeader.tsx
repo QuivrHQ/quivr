e96e4f70db6e589f0f64498a9854b868bf1ec528
Antoine Dewez <44063631+Zewed@users.noreply.github.com>
2024-03-07 09:12:24 -0800

    feat(frontend): add help tooltip for model selection (#2318)
    # Description
    
    Please include a summary of the changes and the related issue. Please
    also include relevant motivation and context.
    
    ## Checklist before requesting a review
    
    Please delete options that are not relevant.
    
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my code
    - [ ] I have commented hard-to-understand areas
    - [ ] I have ideally added tests that prove my fix is effective or that
    my feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] Any dependent changes have been merged
    
    ## Screenshots (if appropriate):

--- frontend/lib/components/ui/FieldHeader/FieldHeader.tsx
+++ frontend/lib/components/ui/FieldHeader/FieldHeader.tsx
@@ -1,3 +1,5 @@
+import Tooltip from "@/lib/components/ui/Tooltip/Tooltip";
+
 import styles from "./FieldHeader.module.scss";
 
 import { Icon } from "../Icon/Icon";
@@ -5,16 +7,25 @@ import { Icon } from "../Icon/Icon";
 type FieldHeaderProps = {
   iconName: string;
   label: string;
+  help?: string;
 };
 
 export const FieldHeader = ({
   iconName,
   label,
+  help,
 }: FieldHeaderProps): JSX.Element => {
   return (
     <div className={styles.field_header_wrapper}>
       <Icon name={iconName} color="black" size="small" />
       <label>{label}</label>
+      {help && (
+        <Tooltip tooltip={help}>
+          <div>
+            <Icon name="help" size="normal" color="black" handleHover={true} />
+          </div>
+        </Tooltip>
+      )}
     </div>
   );
 };
