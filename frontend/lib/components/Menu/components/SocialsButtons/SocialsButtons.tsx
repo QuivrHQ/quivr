a90a2dbaba94fc1733e4c8e8044f958d05109fbc
Zewed <dewez.antoine2@gmail.com>
2024-03-08 16:16:12 -0800

    finished

--- frontend/lib/components/Menu/components/SocialsButtons/SocialsButtons.tsx
+++ frontend/lib/components/Menu/components/SocialsButtons/SocialsButtons.tsx
@@ -3,16 +3,33 @@ import { Icon } from "@/lib/components/ui/Icon/Icon";
 import styles from "./SocialsButtons.module.scss";
 
 export const SocialsButtons = (): JSX.Element => {
+  const handleClick = (url: string) => {
+    window.open(url, "_blank");
+  };
+
   return (
     <div className={styles.socials_buttons_wrapper}>
-      <Icon name="github" color="black" size="small" handleHover={true}></Icon>
+      <Icon
+        name="github"
+        color="black"
+        size="small"
+        handleHover={true}
+        onClick={() => handleClick("https://github.com/QuivrHQ/quivr")}
+      />
       <Icon
         name="linkedin"
         color="black"
         size="small"
         handleHover={true}
-      ></Icon>
-      <Icon name="twitter" color="black" size="small" handleHover={true}></Icon>
+        onClick={() => handleClick("https://www.linkedin.com/company/getquivr")}
+      />
+      <Icon
+        name="twitter"
+        color="black"
+        size="small"
+        handleHover={true}
+        onClick={() => handleClick("https://twitter.com/quivr_brain")}
+      />
     </div>
   );
 };
