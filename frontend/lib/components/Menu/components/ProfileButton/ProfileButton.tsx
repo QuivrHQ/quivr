0d5f07e1cb546c10a030ce868f245d89e0977ac0
Zewed <dewez.antoine2@gmail.com>
2024-03-13 15:21:18 -0700

    update username

--- frontend/lib/components/Menu/components/ProfileButton/ProfileButton.tsx
+++ frontend/lib/components/Menu/components/ProfileButton/ProfileButton.tsx
@@ -2,15 +2,17 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 import { MenuButton } from "@/lib/components/Menu/components/MenuButton/MenuButton";
+import { useUserData } from "@/lib/hooks/useUserData";
 
 export const ProfileButton = (): JSX.Element => {
   const pathname = usePathname() ?? "";
   const isSelected = pathname.includes("/user");
+  const { userIdentityData } = useUserData();
 
   return (
     <Link href="/user">
       <MenuButton
-        label="Profile"
+        label={userIdentityData?.username ?? "Profile"}
         iconName="user"
         type="open"
         isSelected={isSelected}
