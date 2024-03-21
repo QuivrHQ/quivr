4a341ba59d90c3248c1da5b0c0c77a9c3ce3f7d5
Antoine Dewez <44063631+Zewed@users.noreply.github.com>
2024-03-21 00:17:19 -0700

    fix(backend): migration legacy (#2370)
    # Description
    
    Please include a summary of the changes and the related issue. Please
    also include relevant motivation and context.
    
    ## Checklist before requesting a review
    
    Please delete options that are not relevant.
    
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my code
    - [ ] I have commented hard-to-understand areas
    - [ ] I have ideally added tests that prove my fix is effective or that
    my feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] Any dependent changes have been merged
    
    ## Screenshots (if appropriate):

--- supabase/migrations/20240318024425_rename_feedback_to_thumbs.sql
+++ supabase/migrations/20240318024425_rename_feedback_to_thumbs.sql
@@ -1,11 +1,3 @@
-create type "public"."user_identity_company_size" as enum ('1-10', '10-25', '25-50', '50-100', '100-250', '250-500', '500-1000', '1000-5000', '+5000');
-
 alter table "public"."chat_history" drop column "user_feedback";
 
 alter table "public"."chat_history" add column "thumbs" boolean;
-
-alter table "public"."user_identity" add column "company_size" user_identity_company_size;
-
-alter table "public"."user_identity" add column "usage_purpose" text;
-
-
