4148001f729dd8024a42e30e60c4856f10194a6f
Zewed <dewez.antoine2@gmail.com>
2024-04-01 19:12:31 -0700

    wip

--- frontend/lib/api/analytics/analytics.ts
+++ frontend/lib/api/analytics/analytics.ts
@@ -3,12 +3,11 @@ import { AxiosInstance } from "axios";
 import { BrainsUsages } from "./types";
 
 export const getBrainsUsages = async (
-  axiosInstance: AxiosInstance,
-  brain_id?: string
+  axiosInstance: AxiosInstance
 ): Promise<BrainsUsages | undefined> => {
   const brainsUsages = (
     await axiosInstance.get<BrainsUsages | undefined>(
-      `/analytics/brains-usages/${brain_id}`
+      "/analytics/brains-usages"
     )
   ).data;
 
