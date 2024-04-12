d5a169775210dda01fff4466834d1c4cc09c18a0
Zewed <dewez.antoine2@gmail.com>
2024-04-12 19:23:47 +0200

    wip

---
+++ frontend/app/assistants/AssistantModal/InputsStep/InputsStep.tsx
@@ -0,0 +1,5 @@
+import styles from "./InputsStep.module.scss";
+
+export const InputsStep = (): JSX.Element => {
+  return <div className={styles.field_header_wrapper}></div>;
+};
---
+++ frontend/app/assistants/AssistantModal/OutputsStep/OutputsStep.tsx
@@ -0,0 +1,5 @@
+import styles from "./OutputsStep.module.scss";
+
+export const OutputsSTep = (): JSX.Element => {
+  return <div className={styles.field_header_wrapper}></div>;
+};
