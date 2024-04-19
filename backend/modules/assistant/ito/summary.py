5d92b98bcbce53acac9eb0d705f5d92d55dad92c
Stan Girard <girard.stanislas@gmail.com>
2024-04-19 11:53:45 +0200

    Update output description in summary_inputs function

--- backend/modules/assistant/ito/summary.py
+++ backend/modules/assistant/ito/summary.py
@@ -168,7 +168,7 @@ def summary_inputs():
         description="Summarize a set of documents",
         tags=["new"],
         input_description="One document to summarize",
-        output_description="A summary of the document",
+        output_description="A summary of the document with key points and main themes",
         icon_url="https://quivr-cms.s3.eu-west-3.amazonaws.com/report_94bea8b918.png",
         inputs=Inputs(
             files=[
