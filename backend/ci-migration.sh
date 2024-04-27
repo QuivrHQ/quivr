5baf2f02e5bc3170748aefe9f5bda7ccd5b6365b
Stan Girard <girard.stanislas@gmail.com>
2024-04-27 15:04:19 +0200

    Add supabase db password to ci-migration script

--- backend/ci-migration.sh
+++ backend/ci-migration.sh
@@ -6,6 +6,9 @@ PROJECT_ID=$PROJECT_ID
 echo "Setting the supabase token from environment variable"
 SUPABASE_ACCESS_TOKEN=$SUPABASE_ACCESS_TOKEN
 
+echo "Setting the supabase db password from environment variable"
+SUPABASE_DB_PASSWORD=$SUPABASE_DB_PASSWORD
+
 echo "Initializing supabase"
 supabase init
 
@@ -13,8 +16,8 @@ echo "Logging in to supabase"
 supabase login --token $SUPABASE_ACCESS_TOKEN
 
 echo "Running supabase link"
-supabase link --project-ref $PROJECT_ID
+supabase link --project-ref $PROJECT_ID --password $SUPABASE_DB_PASSWORD
 
 echo "Running supabase db push"
-supabase db push --linked
+supabase db push --linked 
 
