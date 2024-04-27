5c0e5db0d97e5d4cd7058bea2dbc8c2108e2e013
Stan Girard <girard.stanislas@gmail.com>
2024-04-27 05:49:15 -0700

    Feat/migration ci 2 (#2509)
    # Description
    
    Please include a summary of the changes and the related issue. Please
    also include relevant motivation and context.
    
    ## Checklist before requesting a review
    
    Please delete options that are not relevant.
    
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my code
    - [ ] I have commented hard-to-understand areas
    - [ ] I have ideally added tests that prove my fix is effective or that
    my feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] Any dependent changes have been merged
    
    ## Screenshots (if appropriate):

--- backend/ci-migration.sh
+++ backend/ci-migration.sh
@@ -3,6 +3,12 @@
 # Set the project ID from environment variable
 PROJECT_ID=$PROJECT_ID
 
+# Set the supabase token from environment variable
+SUPABASE_ACCESS_TOKEN=$SUPABASE_ACCESS_TOKEN
+
+# Login to supabase
+supabase login --token $SUPABASE_ACCESS_TOKEN
+
 # Run supabase link
 supabase link --project-ref $PROJECT_ID
 
