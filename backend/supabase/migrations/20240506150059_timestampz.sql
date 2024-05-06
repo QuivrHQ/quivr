bed01ed38932fe5742c4b48a712eccc0cd27eaa4
Zewed <dewez.antoine2@gmail.com>
2024-05-06 17:47:26 +0200

    wip

--- backend/supabase/migrations/20240506150059_timestampz.sql
+++ backend/supabase/migrations/20240506150059_timestampz.sql
@@ -2,4 +2,7 @@ alter table "public"."notifications" alter column "datetime" set default (now() 
 
 alter table "public"."notifications" alter column "datetime" set data type timestamp with time zone using "datetime"::timestamp with time zone;
 
+alter
+  publication supabase_realtime add table notifications
+
 
