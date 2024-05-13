3d4928871757ffa74859aee6b1248f5142cbd4d2
Zewed <dewez.antoine2@gmail.com>
2024-05-13 16:51:45 +0200

    wip

--- frontend/app/knowledge/page.tsx
+++ frontend/app/knowledge/page.tsx
@@ -1,7 +1,29 @@
 "use client";
 
+import PageHeader from "@/lib/components/PageHeader/PageHeader";
+import { ButtonType } from "@/lib/types/QuivrButton";
+
+import styles from "./page.module.scss";
+
 const Knowledge = (): JSX.Element => {
-  return <>Knowledge</>;
+  const buttons: ButtonType[] = [
+    {
+      label: "New",
+      color: "primary",
+      onClick: () => {
+        console.info("New");
+      },
+      iconName: "add",
+    },
+  ];
+
+  return (
+    <>
+      <div className={styles.page_header}>
+        <PageHeader iconName="book" label="Knowledge" buttons={buttons} />
+      </div>
+    </>
+  );
 };
 
 export default Knowledge;
