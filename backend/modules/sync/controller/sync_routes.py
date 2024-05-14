329b96940f39b8a9296a4e9f882f72e97119740d
Stan Girard <girard.stanislas@gmail.com>
2024-05-15 00:11:29 +0200

    Add Google Drive sync routes

--- backend/modules/sync/controller/sync_routes.py
+++ backend/modules/sync/controller/sync_routes.py
@@ -1,25 +1,32 @@
-import json
 import os
 from typing import List
 
-from fastapi import APIRouter, Depends, HTTPException, Request
-from google_auth_oauthlib.flow import Flow
+from fastapi import APIRouter, Depends
 from logger import get_logger
 from middlewares.auth import AuthBearer, get_current_user
+from modules.sync.controller.google_sync_routes import google_sync_router
 from modules.sync.dto import SyncsDescription
-from modules.sync.dto.inputs import SyncsUserInput, SyncUserUpdateInput
 from modules.sync.dto.outputs import AuthMethodEnum
 from modules.sync.service.sync_service import SyncService
 from modules.user.entity.user_identity import UserIdentity
 
+# Set environment variable for OAuthlib
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
+# Initialize logger
 logger = get_logger(__name__)
 
+# Initialize sync service
 sync_service = SyncService()
 
+# Initialize API router
 sync_router = APIRouter()
 
+# Add Google routes here
+sync_router.include_router(google_sync_router)
+
+
+# Google sync description
 google_sync = SyncsDescription(
     name="Google",
     description="Sync your Google Drive with Quivr",
@@ -34,7 +41,16 @@ google_sync = SyncsDescription(
     tags=["Sync"],
 )
 async def get_syncs(current_user: UserIdentity = Depends(get_current_user)):
+    """
+    Get all available sync descriptions.
 
+    Args:
+        current_user (UserIdentity): The current authenticated user.
+
+    Returns:
+        List[SyncsDescription]: A list of available sync descriptions.
+    """
+    logger.debug(f"Fetching all sync descriptions for user: {current_user.id}")
     return [google_sync]
 
 
@@ -44,68 +60,14 @@ async def get_syncs(current_user: UserIdentity = Depends(get_current_user)):
     tags=["Sync"],
 )
 async def get_user_syncs(current_user: UserIdentity = Depends(get_current_user)):
+    """
+    Get syncs for the current user.
 
+    Args:
+        current_user (UserIdentity): The current authenticated user.
+
+    Returns:
+        List: A list of syncs for the user.
+    """
+    logger.debug(f"Fetching user syncs for user: {current_user.id}")
     return sync_service.get_syncs_user(str(current_user.id))
-
-
-CLIENT_SECRETS_FILE = "modules/sync/controller/credentials.json"
-SCOPES = ["https://www.googleapis.com/auth/drive.metadata.readonly"]
-REDIRECT_URI = "http://localhost:5050/sync/google/oauth2callback"
-
-
-@sync_router.get(
-    "/sync/google/authorize",
-    dependencies=[Depends(AuthBearer())],
-    tags=["Sync"],
-)
-def authorize_google(
-    request: Request, current_user: UserIdentity = Depends(get_current_user)
-):
-    # Add to the redirect URI the user_id to be able to identify the user when the callback is called
-    REDIRECT_URI = (
-        f"http://localhost:5050/sync/google/oauth2callback?user_id={current_user.id}"
-    )
-    flow = Flow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE, scopes=SCOPES, redirect_uri=REDIRECT_URI
-    )
-    authorization_url, state = flow.authorization_url(
-        access_type="offline", include_granted_scopes="true"
-    )
-    sync_user_input = SyncsUserInput(
-        user_id=str(current_user.id),
-        sync_name="Google",
-        credentials={},
-        state={"state": state},
-    )
-    sync_service.create_sync_user(sync_user_input)
-    return {"authorization_url": authorization_url}
-
-
-@sync_router.get("/sync/google/oauth2callback")
-def oauth2callback_google(request: Request):
-    state = request.query_params.get("state")
-    state_dict = {"state": state}
-    current_user = request.query_params.get("user_id")
-    sync_user_state = sync_service.get_sync_user_by_state(state_dict)
-    logger.info(sync_user_state)
-    if state_dict != sync_user_state["state"]:
-        raise HTTPException(status_code=400, detail="Invalid state parameter")
-
-    if sync_user_state.get("user_id") != current_user:
-        raise HTTPException(status_code=400, detail="Invalid user")
-
-    REDIRECT_URI = (
-        f"http://localhost:5050/sync/google/oauth2callback?user_id={current_user}"
-    )
-    flow = Flow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE, scopes=SCOPES, state=state, redirect_uri=REDIRECT_URI
-    )
-    flow.fetch_token(authorization_response=str(request.url))
-    creds = flow.credentials
-
-    sync_user_input = SyncUserUpdateInput(
-        credentials=json.loads(creds.to_json()),
-        state={},
-    )
-    sync_service.update_sync_user(current_user, state_dict, sync_user_input)
-    return {"message": "Google Drive sync created successfully"}
