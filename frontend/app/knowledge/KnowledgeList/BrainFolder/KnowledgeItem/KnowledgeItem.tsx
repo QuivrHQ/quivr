0fd618ec273381295d2ba6ab9f8e153cc5edad8c
Zewed <dewez.antoine2@gmail.com>
2024-05-16 14:38:14 +0200

    work on reading doc

--- frontend/app/knowledge/KnowledgeList/BrainFolder/KnowledgeItem/KnowledgeItem.tsx
+++ frontend/app/knowledge/KnowledgeList/BrainFolder/KnowledgeItem/KnowledgeItem.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import axios from "axios";
+
+import { useKnowledgeApi } from "@/lib/api/knowledge/useKnowledgeApi";
 import { isUploadedKnowledge, Knowledge } from "@/lib/types/Knowledge";
 
 import styles from "./KnowledgeItem.module.scss";
@@ -9,10 +12,44 @@ type KnowledgeItemProps = {
 };
 
 const KnowledgeItem = ({ knowledge }: KnowledgeItemProps): JSX.Element => {
+  const { generateSignedUrlKnowledge } = useKnowledgeApi();
+
+  const logFileContent = async () => {
+    if (isUploadedKnowledge(knowledge)) {
+      let download_url = await generateSignedUrlKnowledge({
+        knowledgeId: knowledge.id,
+      });
+      console.info(download_url);
+      download_url = download_url.replace("host.docker.internal", "localhost");
+
+      try {
+        const response = await axios.get(download_url, {
+          responseType: "blob",
+        });
+
+        const reader = new FileReader();
+
+        reader.onload = (event) => {
+          console.info(event.target?.result);
+        };
+
+        reader.onerror = (event) => {
+          console.error("Error reading file:", event);
+        };
+
+        reader.readAsText(new Blob([response.data]));
+      } catch (error) {
+        console.error("Error downloading the file:", error);
+      }
+    }
+  };
+
   return (
     <div className={styles.knowledge_item_wrapper}>
       {isUploadedKnowledge(knowledge) ? (
-        <span className={styles.name}>{knowledge.fileName}</span>
+        <span className={styles.name} onClick={() => void logFileContent()}>
+          {knowledge.fileName}
+        </span>
       ) : (
         <a
           className={styles.name}
