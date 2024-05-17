890750daed639d856be0ba461257969844ce8117
Zewed <dewez.antoine2@gmail.com>
2024-05-17 19:46:24 +0200

    zindex and position stuff

--- frontend/app/knowledge/KnowledgeList/BrainFolder/KnowledgeItem/KnowledgeItem.tsx
+++ frontend/app/knowledge/KnowledgeList/BrainFolder/KnowledgeItem/KnowledgeItem.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import axios from "axios";
+import { useEffect, useRef, useState } from "react";
 
 import { useKnowledgeApi } from "@/lib/api/knowledge/useKnowledgeApi";
+import Icon from "@/lib/components/ui/Icon/Icon";
+import { OptionsModal } from "@/lib/components/ui/OptionsModal/OptionsModal";
 import { useNotesEditorContext } from "@/lib/context/NotesEditorProvider/hooks/useNotesEditorContext";
 import { isUploadedKnowledge, Knowledge } from "@/lib/types/Knowledge";
+import { Option } from "@/lib/types/Options";
 
 import styles from "./KnowledgeItem.module.scss";
 
@@ -15,6 +19,10 @@ type KnowledgeItemProps = {
 const KnowledgeItem = ({ knowledge }: KnowledgeItemProps): JSX.Element => {
   const { generateSignedUrlKnowledge } = useKnowledgeApi();
   const { updateContent } = useNotesEditorContext();
+  const [optionsOpened, setOptionsOpened] = useState<boolean>(false);
+
+  const iconRef = useRef<HTMLDivElement | null>(null);
+  const optionsRef = useRef<HTMLDivElement | null>(null);
 
   const canReadFile = (fileName: string): boolean => {
     const extension = fileName.split(".").pop();
@@ -24,6 +32,21 @@ const KnowledgeItem = ({ knowledge }: KnowledgeItemProps): JSX.Element => {
       isUploadedKnowledge(knowledge)
     );
   };
+
+  const options: Option[] = [
+    {
+      label: "Edit",
+      onClick: () => ({}),
+      iconName: "edit",
+      iconColor: "primary",
+    },
+    {
+      label: "Delete",
+      onClick: () => void true,
+      iconName: "delete",
+      iconColor: "dangerous",
+    },
+  ];
 
   const logFileContent = async () => {
     if (isUploadedKnowledge(knowledge)) {
@@ -56,32 +79,65 @@ const KnowledgeItem = ({ knowledge }: KnowledgeItemProps): JSX.Element => {
     }
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        iconRef.current &&
+        !iconRef.current.contains(event.target as Node) &&
+        optionsRef.current &&
+        !optionsRef.current.contains(event.target as Node)
+      ) {
+        setOptionsOpened(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div
-      className={`${styles.knowledge_item_wrapper} ${
-        !canReadFile(
-          isUploadedKnowledge(knowledge) ? knowledge.fileName : knowledge.url
-        )
-          ? styles.disabled
-          : ""
-      }`}
-    >
-      {isUploadedKnowledge(knowledge) ? (
-        <span className={styles.name} onClick={() => void logFileContent()}>
-          {knowledge.fileName}
-        </span>
-      ) : (
-        <a
-          className={styles.name}
-          href={knowledge.url}
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className={styles.knowledge_item_container}>
+      <div
+        className={`${styles.knowledge_item_wrapper} ${
+          !canReadFile(
+            isUploadedKnowledge(knowledge) ? knowledge.fileName : knowledge.url
+          )
+            ? styles.disabled
+            : ""
+        }`}
+      >
+        {isUploadedKnowledge(knowledge) ? (
+          <span className={styles.name} onClick={() => void logFileContent()}>
+            {knowledge.fileName}
+          </span>
+        ) : (
+          <a
+            className={styles.name}
+            href={knowledge.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {knowledge.url}
+          </a>
+        )}
+        <div
+          className={styles.icon_wrapper}
+          ref={iconRef}
+          onClick={(event: React.MouseEvent<HTMLElement>) => {
+            event.nativeEvent.stopImmediatePropagation();
+            event.stopPropagation();
+            setOptionsOpened(!optionsOpened);
+          }}
         >
-          {knowledge.url}
-        </a>
-      )}
-      <div className={styles.absolute_wrapper}>
-        <div className={styles.absolute}>Hey</div>
+          <Icon name="options" size="tiny" color="black" handleHover={true} />
+        </div>
+      </div>
+      <div className={styles.options_modal_wrapper}>
+        <div ref={optionsRef} className={styles.options_modal}>
+          {optionsOpened && <OptionsModal options={options} />}
+        </div>
       </div>
     </div>
   );
