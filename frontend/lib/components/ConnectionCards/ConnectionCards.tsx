fe9aad415103bbc553ecfd956003c798574f7fc7
Zewed <dewez.antoine2@gmail.com>
2024-05-27 14:41:41 +0200

    wip

---
+++ frontend/lib/components/ConnectionCards/ConnectionCards.tsx
@@ -0,0 +1,26 @@
+import { useSync } from "@/lib/api/sync/useSync";
+
+import { ConnectionSection } from "./ConnectionSection/ConnectionSection";
+import styles from "./Connections.module.scss";
+
+export const ConnectionCards = (): JSX.Element => {
+  const { syncGoogleDrive, syncSharepoint, iconUrls } = useSync();
+
+  return (
+    <div className={styles.connections_wrapper}>
+      <span className={styles.title}>Link apps you want to search across</span>
+      <div className={styles.connection_cards}>
+        <ConnectionSection
+          label="Google Drive"
+          iconUrl={iconUrls.Google}
+          callback={(name) => syncGoogleDrive(name)}
+        />
+        <ConnectionSection
+          label="Sharepoint"
+          iconUrl={iconUrls.Azure}
+          callback={(name) => syncSharepoint(name)}
+        />
+      </div>
+    </div>
+  );
+};
