00074f46fc01fba0f72e81b465074f30ea8900ce
Zewed <dewez.antoine2@gmail.com>
2024-05-27 14:52:13 +0200

    wip

--- frontend/lib/components/ConnectionCards/ConnectionCards.tsx
+++ frontend/lib/components/ConnectionCards/ConnectionCards.tsx
@@ -4,23 +4,20 @@ import styles from "./ConnectionCards.module.scss";
 import { ConnectionSection } from "./ConnectionSection/ConnectionSection";
 
 export const ConnectionCards = (): JSX.Element => {
-  const { syncGoogleDrive, syncSharepoint, iconUrls } = useSync();
+  const { syncGoogleDrive, syncSharepoint } = useSync();
 
   return (
-    <div className={styles.connections_wrapper}>
-      <span className={styles.title}>Link apps you want to search across</span>
-      <div className={styles.connection_cards}>
-        <ConnectionSection
-          label="Google Drive"
-          iconUrl={iconUrls.Google}
-          callback={(name) => syncGoogleDrive(name)}
-        />
-        <ConnectionSection
-          label="Sharepoint"
-          iconUrl={iconUrls.Azure}
-          callback={(name) => syncSharepoint(name)}
-        />
-      </div>
+    <div className={styles.connection_cards}>
+      <ConnectionSection
+        label="Google Drive"
+        provider="Google"
+        callback={(name) => syncGoogleDrive(name)}
+      />
+      <ConnectionSection
+        label="Sharepoint"
+        provider="Azure"
+        callback={(name) => syncSharepoint(name)}
+      />
     </div>
   );
 };
--- frontend/lib/components/ConnectionCards/ConnectionSection/ConnectionSection.tsx
+++ frontend/lib/components/ConnectionCards/ConnectionSection/ConnectionSection.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
+import { Provider, Sync } from "@/lib/api/sync/types";
+import { useSync } from "@/lib/api/sync/useSync";
 import QuivrButton from "@/lib/components/ui/QuivrButton/QuivrButton";
 
 import styles from "./ConnectionSection.module.scss";
@@ -9,28 +11,45 @@ import { ConnectionModal } from "../ConnectionModal/ConnectionModal";
 
 interface ConnectionSectionProps {
   label: string;
-  iconUrl: string;
+  provider: Provider;
   callback: (name: string) => Promise<{ authorization_url: string }>;
 }
 
 export const ConnectionSection = ({
   label,
-  iconUrl,
+  provider,
   callback,
 }: ConnectionSectionProps): JSX.Element => {
   const [connectionModalOpened, setConnectionModalOpened] =
     useState<boolean>(false);
+  const { iconUrls, getUserSyncs } = useSync();
+  const [existingConnections, setExistingConnections] = useState<Sync[]>([]);
+
+  useEffect(() => {
+    void (async () => {
+      try {
+        const res: Sync[] = await getUserSyncs();
+        setExistingConnections(
+          res.filter(
+            (sync) => !!sync.credentials.token && sync.provider === provider
+          )
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
 
   return (
     <>
       <div className={styles.connection_section_wrapper}>
         <div className={styles.connection_section_header}>
-          <Image src={iconUrl} alt={label} width={24} height={24} />
+          <Image src={iconUrls[provider]} alt={label} width={24} height={24} />
           <span className={styles.label}>{label}</span>
         </div>
         <QuivrButton
-          iconName="sync"
-          label="Connect"
+          iconName={existingConnections.length ? "add" : "sync"}
+          label={existingConnections.length ? "Add more" : "Connect"}
           color="primary"
           onClick={() => setConnectionModalOpened(true)}
           small={true}
@@ -40,7 +59,7 @@ export const ConnectionSection = ({
         modalOpened={connectionModalOpened}
         setModalOpened={setConnectionModalOpened}
         label={label}
-        iconUrl={iconUrl}
+        iconUrl={iconUrls[provider]}
         callback={(name) => callback(name)}
       />
     </>
