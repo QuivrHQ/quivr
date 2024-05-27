2ee9c80ff0783d06fc966c8bd439ff8073e1f51e
Zewed <dewez.antoine2@gmail.com>
2024-05-27 17:15:23 +0200

    cleaning

--- frontend/lib/components/AddBrainModal/components/CreateBrainStep/CreateBrainStep.tsx
+++ frontend/lib/components/AddBrainModal/components/CreateBrainStep/CreateBrainStep.tsx
@@ -1,11 +1,7 @@
-import { capitalCase } from "change-case";
-import { useEffect, useState } from "react";
-
 import { KnowledgeToFeed } from "@/app/chat/[chatId]/components/ActionsBar/components";
 import { useUserApi } from "@/lib/api/user/useUserApi";
 import { MessageInfoBox } from "@/lib/components/ui/MessageInfoBox/MessageInfoBox";
 import QuivrButton from "@/lib/components/ui/QuivrButton/QuivrButton";
-import { TextInput } from "@/lib/components/ui/TextInput/TextInput";
 import { useKnowledgeToFeedContext } from "@/lib/context/KnowledgeToFeedProvider/hooks/useKnowledgeToFeedContext";
 import { useUserData } from "@/lib/hooks/useUserData";
 
@@ -17,32 +13,11 @@ import { useBrainCreationSteps } from "../../hooks/useBrainCreationSteps";
 
 export const CreateBrainStep = (): JSX.Element => {
   const { currentStepIndex, goToPreviousStep } = useBrainCreationSteps();
-  const { createBrain, fields, setFields } = useBrainCreationApi();
-  const { creating, setCreating, currentSelectedBrain } =
-    useBrainCreationContext();
-  const [createBrainStepIndex, setCreateBrainStepIndex] = useState<number>(0);
+  const { createBrain } = useBrainCreationApi();
+  const { creating, setCreating } = useBrainCreationContext();
   const { knowledgeToFeed } = useKnowledgeToFeedContext();
   const { userIdentityData } = useUserData();
   const { updateUserIdentity } = useUserApi();
-
-  useEffect(() => {
-    if (currentSelectedBrain?.connection_settings) {
-      const newFields = Object.entries(
-        currentSelectedBrain.connection_settings
-      ).map(([key, type]) => {
-        return { name: key, type, value: "" };
-      });
-      setFields(newFields);
-    }
-
-    setCreateBrainStepIndex(Number(!currentSelectedBrain?.connection_settings));
-  }, [currentSelectedBrain?.connection_settings]);
-
-  const handleInputChange = (name: string, value: string) => {
-    setFields(
-      fields.map((field) => (field.name === name ? { ...field, value } : field))
-    );
-  };
 
   const previous = (): void => {
     goToPreviousStep();
@@ -58,24 +33,6 @@ export const CreateBrainStep = (): JSX.Element => {
     }
     setCreating(true);
     createBrain();
-  };
-
-  const renderSettings = () => {
-    return (
-      <>
-        <MessageInfoBox type="warning">
-          {currentSelectedBrain?.information}
-        </MessageInfoBox>
-        {fields.map(({ name, value }) => (
-          <TextInput
-            key={name}
-            inputValue={value}
-            setInputValue={(inputValue) => handleInputChange(name, inputValue)}
-            label={capitalCase(name)}
-          />
-        ))}
-      </>
-    );
   };
 
   const renderFeedBrain = () => {
@@ -96,24 +53,6 @@ export const CreateBrainStep = (): JSX.Element => {
     );
   };
 
-  const renderCreateButton = () => {
-    return (
-      <MessageInfoBox type="info">
-        <div className={styles.message_content}>
-          Click on
-          <QuivrButton
-            label="Create"
-            color="primary"
-            iconName="add"
-            onClick={feed}
-            isLoading={creating}
-          />
-          to finish your brain creation.
-        </div>
-      </MessageInfoBox>
-    );
-  };
-
   const renderButtons = () => {
     return (
       <div className={styles.buttons_wrapper}>
@@ -123,29 +62,17 @@ export const CreateBrainStep = (): JSX.Element => {
           iconName="chevronLeft"
           onClick={previous}
         />
-        {(!currentSelectedBrain?.max_files && !createBrainStepIndex) ||
-        createBrainStepIndex ? (
-          <QuivrButton
-            label="Create"
-            color="primary"
-            iconName="add"
-            onClick={feed}
-            disabled={
-              knowledgeToFeed.length === 0 && !userIdentityData?.onboarded
-            }
-            isLoading={creating}
-            important={true}
-          />
-        ) : (
-          <QuivrButton
-            label="Feed your brain"
-            color="primary"
-            iconName="add"
-            onClick={() => setCreateBrainStepIndex(1)}
-            isLoading={creating}
-            important={true}
-          />
-        )}
+        <QuivrButton
+          label="Create"
+          color="primary"
+          iconName="add"
+          onClick={feed}
+          disabled={
+            knowledgeToFeed.length === 0 && !userIdentityData?.onboarded
+          }
+          isLoading={creating}
+          important={true}
+        />
       </div>
     );
   };
@@ -156,14 +83,7 @@ export const CreateBrainStep = (): JSX.Element => {
 
   return (
     <div className={styles.brain_knowledge_wrapper}>
-      {!createBrainStepIndex && renderSettings()}
-      {!!currentSelectedBrain?.max_files &&
-        !!createBrainStepIndex &&
-        renderFeedBrain()}
-      {!currentSelectedBrain?.max_files &&
-        !currentSelectedBrain?.connection_settings &&
-        renderCreateButton()}
-
+      {renderFeedBrain()}
       {renderButtons()}
     </div>
   );
