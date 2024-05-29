615b7be2d87c4615cb7635573495bd8569f347fe
Zewed <dewez.antoine2@gmail.com>
2024-05-29 16:55:23 +0200

    wip

--- frontend/app/chat/[chatId]/components/ActionsBar/components/KnowledgeToFeed/components/FromConnections/FromConnections.tsx
+++ frontend/app/chat/[chatId]/components/ActionsBar/components/KnowledgeToFeed/components/FromConnections/FromConnections.tsx
@@ -1,20 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
+import { SyncElement } from "@/lib/api/sync/types";
 import { ConnectionCards } from "@/lib/components/ConnectionCards/ConnectionCards";
 import TextButton from "@/lib/components/ui/TextButton/TextButton";
 
 import { useFromConnectionsContext } from "./FromConnectionsProvider/hooks/useFromConnectionContext";
 
 export const FromConnections = (): JSX.Element => {
-  const {
-    currentSyncElements,
-    setCurrentSyncElements,
-    currentProvider,
-    setCurrentProvider,
-  } = useFromConnectionsContext();
+  const { currentSyncElements, setCurrentSyncElements } =
+    useFromConnectionsContext();
+  const [currentFiles, setCurrentFiles] = useState<SyncElement[]>([]);
+  const [currentFolders, setCurrentFolders] = useState<SyncElement[]>([]);
 
   useEffect(() => {
-    console.info(currentSyncElements);
+    setCurrentFiles(
+      currentSyncElements?.files.filter((file) => !file.is_folder) ?? []
+    );
+    setCurrentFolders(
+      currentSyncElements?.files.filter((file) => file.is_folder) ?? []
+    );
   }, [currentSyncElements]);
 
   return (
@@ -29,14 +33,16 @@ export const FromConnections = (): JSX.Element => {
             color="black"
             onClick={() => {
               setCurrentSyncElements({ files: [] });
-              setCurrentProvider(undefined);
             }}
             small={true}
           />
           <div>
-            <div>Synchronize {currentProvider} </div>
-            OR
-            <div></div>
+            {currentFolders.map((folder) => (
+              <div key={folder.id}>{folder.name}</div>
+            ))}
+            {currentFiles.map((file) => (
+              <div key={file.id}>{file.name}</div>
+            ))}
           </div>
         </div>
       )}
--- frontend/app/chat/[chatId]/components/ActionsBar/components/KnowledgeToFeed/components/FromConnections/FromConnectionsProvider/FromConnection-provider.tsx
+++ frontend/app/chat/[chatId]/components/ActionsBar/components/KnowledgeToFeed/components/FromConnections/FromConnectionsProvider/FromConnection-provider.tsx
@@ -1,15 +1,11 @@
 import { createContext, useState } from "react";
 
-import { Provider, SyncElements } from "@/lib/api/sync/types";
+import { SyncElements } from "@/lib/api/sync/types";
 
 export type FromConnectionsContextType = {
   currentSyncElements: SyncElements | undefined;
   setCurrentSyncElements: React.Dispatch<
     React.SetStateAction<SyncElements | undefined>
-  >;
-  currentProvider: Provider | undefined;
-  setCurrentProvider: React.Dispatch<
-    React.SetStateAction<Provider | undefined>
   >;
 };
 
@@ -25,17 +21,12 @@ export const FromConnectionsProvider = ({
   const [currentSyncElements, setCurrentSyncElements] = useState<
     SyncElements | undefined
   >(undefined);
-  const [currentProvider, setCurrentProvider] = useState<Provider | undefined>(
-    undefined
-  );
 
   return (
     <FromConnectionsContext.Provider
       value={{
         currentSyncElements,
         setCurrentSyncElements,
-        currentProvider,
-        setCurrentProvider,
       }}
     >
       {children}
