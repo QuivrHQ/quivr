5d2bc36f44e38635672dbbcce290066cb53f362f
Zewed <dewez.antoine2@gmail.com>
2024-06-17 19:15:41 +0200

    Update message row styles and spacing

--- frontend/app/chat/[chatId]/components/ChatDialogueArea/components/ChatDialogue/components/ChatItem/QADisplay/components/MessageRow/components/Source/Source.tsx
+++ frontend/app/chat/[chatId]/components/ChatDialogueArea/components/ChatDialogue/components/ChatItem/QADisplay/components/MessageRow/components/Source/Source.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import Icon from "@/lib/components/ui/Icon/Icon";
+
 import styles from "./Source.module.scss";
 
 import { SourceFile } from "../../types/types";
@@ -15,15 +17,25 @@ export const SourceCitations = ({
   const [isExpanded, setIsExpanded] = useState<boolean>(false);
 
   return (
-    <div
-      className={`${styles.source_wrapper} ${
-        isSelected ? styles.selected_source : ""
-      }`}
-      onClick={() => setIsExpanded(!isExpanded)}
-    >
-      <div className={styles.source_header}>
-        <span className={styles.filename}>{sourceFile.filename}</span>
+    <div className={styles.source_container}>
+      <div
+        className={`${styles.source_wrapper} ${
+          isSelected ? styles.selected_source : ""
+        }`}
+        onClick={() => setIsExpanded(!isExpanded)}
+      >
+        <div className={styles.source_header}>
+          <span className={styles.filename}>{sourceFile.filename}</span>
+        </div>
       </div>
+      <a
+        onClick={(event) => event.stopPropagation()}
+        href={sourceFile.file_url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Icon name="externLink" size="small" color="black" handleHover={true} />
+      </a>
     </div>
   );
 };
