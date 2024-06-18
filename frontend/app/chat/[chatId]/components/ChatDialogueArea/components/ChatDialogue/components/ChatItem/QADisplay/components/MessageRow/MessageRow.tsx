659f4aeea1fae6a94f493b09c26643c732c8796f
Zewed <dewez.antoine2@gmail.com>
2024-06-18 17:34:07 +0200

    Refactor QADisplay component to import MessageRow from the correct location

--- frontend/app/chat/[chatId]/components/ChatDialogueArea/components/ChatDialogue/components/ChatItem/QADisplay/components/MessageRow/MessageRow.tsx
+++ frontend/app/chat/[chatId]/components/ChatDialogueArea/components/ChatDialogue/components/ChatItem/QADisplay/components/MessageRow/MessageRow.tsx
@@ -32,218 +32,209 @@ type MessageRowProps = {
   lastMessage?: boolean;
 };
 
-const MessageRow = React.forwardRef(
-  (
-    {
-      speaker,
-      text,
-      brainName,
-      children,
-      brainId,
-      messageId,
-      thumbs: initialThumbs,
-      metadata,
-      lastMessage,
-    }: MessageRowProps,
-    ref: React.Ref<HTMLDivElement>
-  ) => {
-    const { handleCopy, isUserSpeaker } = useMessageRow({
-      speaker,
-      text,
-    });
-    const { updateChatMessage } = useChatApi();
-    const { chatId } = useChat();
-    const [thumbs, setThumbs] = useState<boolean | undefined | null>(
-      initialThumbs
+export const MessageRow = ({
+  speaker,
+  text,
+  brainName,
+  children,
+  brainId,
+  messageId,
+  thumbs: initialThumbs,
+  metadata,
+  lastMessage,
+}: MessageRowProps): JSX.Element => {
+  const { handleCopy, isUserSpeaker } = useMessageRow({
+    speaker,
+    text,
+  });
+  const { updateChatMessage } = useChatApi();
+  const { chatId } = useChat();
+  const [thumbs, setThumbs] = useState<boolean | undefined | null>(
+    initialThumbs
+  );
+  const [folded, setFolded] = useState<boolean>(false);
+  const [sourceFiles, setSourceFiles] = useState<SourceFile[]>([]);
+  const { submitQuestion } = useChatInput();
+
+  useEffect(() => {
+    setThumbs(initialThumbs);
+    setSourceFiles(
+      metadata?.sources?.reduce((acc, source) => {
+        const existingSource = acc.find((s) => s.filename === source.name);
+        if (existingSource) {
+          existingSource.citations.push(source.citation);
+        } else {
+          acc.push({
+            filename: source.name,
+            file_url: source.source_url,
+            citations: [source.citation],
+            selected: false,
+          });
+        }
+
+        return acc;
+      }, [] as SourceFile[]) ?? []
     );
-    const [folded, setFolded] = useState<boolean>(false);
-    const [sourceFiles, setSourceFiles] = useState<SourceFile[]>([]);
-    const { submitQuestion } = useChatInput();
+  }, [initialThumbs, metadata]);
 
-    useEffect(() => {
-      setThumbs(initialThumbs);
-      setSourceFiles(
-        metadata?.sources?.reduce((acc, source) => {
-          const existingSource = acc.find((s) => s.filename === source.name);
-          if (existingSource) {
-            existingSource.citations.push(source.citation);
-          } else {
-            acc.push({
-              filename: source.name,
-              file_url: source.source_url,
-              citations: [source.citation],
-              selected: false,
-            });
-          }
+  const messageContent = text ?? "";
 
-          return acc;
-        }, [] as SourceFile[]) ?? []
-      );
-    }, [initialThumbs, metadata]);
+  const thumbsUp = async () => {
+    if (chatId && messageId) {
+      await updateChatMessage(chatId, messageId, {
+        thumbs: thumbs ? null : true,
+      });
+      setThumbs(thumbs ? null : true);
+    }
+  };
 
-    const messageContent = text ?? "";
+  const thumbsDown = async () => {
+    if (chatId && messageId) {
+      await updateChatMessage(chatId, messageId, {
+        thumbs: thumbs === false ? null : false,
+      });
+      setThumbs(thumbs === false ? null : false);
+    }
+  };
 
-    const thumbsUp = async () => {
-      if (chatId && messageId) {
-        await updateChatMessage(chatId, messageId, {
-          thumbs: thumbs ? null : true,
-        });
-        setThumbs(thumbs ? null : true);
-      }
-    };
-
-    const thumbsDown = async () => {
-      if (chatId && messageId) {
-        await updateChatMessage(chatId, messageId, {
-          thumbs: thumbs === false ? null : false,
-        });
-        setThumbs(thumbs === false ? null : false);
-      }
-    };
-
-    const renderMessageHeader = () => {
-      if (!isUserSpeaker && !folded) {
-        return (
-          <div className={styles.message_header_wrapper}>
-            <div className={styles.message_header}>
-              <QuestionBrain brainName={brainName} brainId={brainId} />
-            </div>
-          </div>
-        );
-      }
-    };
-
-    const renderMetadata = () => {
-      if (!isUserSpeaker && messageContent !== "🧠") {
-        return (
-          <div className={styles.metadata_wrapper}>
-            <div
-              className={`${styles.icons_wrapper} ${
-                sourceFiles.length === 0 ? styles.with_border : ""
-              }`}
-            >
-              {metadata?.thoughts && metadata.thoughts.trim() !== "" && (
-                <ThoughtsButton text={metadata.thoughts} size="small" />
-              )}
-              <CopyButton handleCopy={handleCopy} size="small" />
-              <Icon
-                name="thumbsUp"
-                handleHover={true}
-                color={thumbs ? "primary" : "black"}
-                size="small"
-                onClick={async () => {
-                  await thumbsUp();
-                }}
-              />
-              <Icon
-                name="thumbsDown"
-                handleHover={true}
-                color={thumbs === false ? "primary" : "black"}
-                size="small"
-                onClick={async () => {
-                  await thumbsDown();
-                }}
-              />
-            </div>
-
-            {sourceFiles.length > 0 && (
-              <div className={styles.sources_and_citations_wrapper}>
-                <div className={styles.title_wrapper}>
-                  <Icon name="sources" size="normal" color="black" />
-                  <span className={styles.title}>Sources</span>
-                </div>
-                <div className={styles.sources}>
-                  {sourceFiles.map((sourceFile, i) => (
-                    <div key={i}>
-                      <SourceCitations sourceFile={sourceFile} />
-                    </div>
-                  ))}
-                </div>
-              </div>
-            )}
-          </div>
-        );
-      }
-    };
-
-    const renderRelatedQuestions = () => {
-      if (
-        !isUserSpeaker &&
-        !folded &&
-        (metadata?.followup_questions?.length ?? 0) > 0
-      ) {
-        return (
-          <div className={styles.related_questions_wrapper}>
-            <div className={styles.title_wrapper}>
-              <Icon name="search" color="black" size="normal" />
-              <span className={styles.title}>Follow up questions</span>
-            </div>
-            <div className={styles.questions_wrapper}>
-              {metadata?.followup_questions?.map((question, index) => (
-                <div
-                  className={styles.question}
-                  key={index}
-                  onClick={() => submitQuestion(question)}
-                >
-                  <Icon name="followUp" size="small" color="grey" />
-                  <span className={styles.text}>{question}</span>
-                </div>
-              ))}
-            </div>
-          </div>
-        );
-      }
-    };
-
-    const renderOtherSections = () => {
+  const renderMessageHeader = () => {
+    if (!isUserSpeaker && !folded) {
       return (
-        <>
-          {!folded && renderMetadata()}
-          {!folded && renderRelatedQuestions()}
-        </>
+        <div className={styles.message_header_wrapper}>
+          <div className={styles.message_header}>
+            <QuestionBrain brainName={brainName} brainId={brainId} />
+          </div>
+        </div>
       );
-    };
+    }
+  };
 
+  const renderMetadata = () => {
+    if (!isUserSpeaker && messageContent !== "🧠") {
+      return (
+        <div className={styles.metadata_wrapper}>
+          <div
+            className={`${styles.icons_wrapper} ${
+              sourceFiles.length === 0 ? styles.with_border : ""
+            }`}
+          >
+            {metadata?.thoughts && metadata.thoughts.trim() !== "" && (
+              <ThoughtsButton text={metadata.thoughts} size="small" />
+            )}
+            <CopyButton handleCopy={handleCopy} size="small" />
+            <Icon
+              name="thumbsUp"
+              handleHover={true}
+              color={thumbs ? "primary" : "black"}
+              size="small"
+              onClick={async () => {
+                await thumbsUp();
+              }}
+            />
+            <Icon
+              name="thumbsDown"
+              handleHover={true}
+              color={thumbs === false ? "primary" : "black"}
+              size="small"
+              onClick={async () => {
+                await thumbsDown();
+              }}
+            />
+          </div>
+
+          {sourceFiles.length > 0 && (
+            <div className={styles.sources_and_citations_wrapper}>
+              <div className={styles.title_wrapper}>
+                <Icon name="sources" size="normal" color="black" />
+                <span className={styles.title}>Sources</span>
+              </div>
+              <div className={styles.sources}>
+                {sourceFiles.map((sourceFile, i) => (
+                  <div key={i}>
+                    <SourceCitations sourceFile={sourceFile} />
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+      );
+    }
+  };
+
+  const renderRelatedQuestions = () => {
+    if (
+      !isUserSpeaker &&
+      !folded &&
+      (metadata?.followup_questions?.length ?? 0) > 0
+    ) {
+      return (
+        <div className={styles.related_questions_wrapper}>
+          <div className={styles.title_wrapper}>
+            <Icon name="search" color="black" size="normal" />
+            <span className={styles.title}>Follow up questions</span>
+          </div>
+          <div className={styles.questions_wrapper}>
+            {metadata?.followup_questions?.map((question, index) => (
+              <div
+                className={styles.question}
+                key={index}
+                onClick={() => submitQuestion(question)}
+              >
+                <Icon name="followUp" size="small" color="grey" />
+                <span className={styles.text}>{question}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+      );
+    }
+  };
+
+  const renderOtherSections = () => {
     return (
-      <div
-        className={`
+      <>
+        {!folded && renderMetadata()}
+        {!folded && renderRelatedQuestions()}
+      </>
+    );
+  };
+
+  return (
+    <div
+      className={`
       ${styles.message_row_container} 
       ${isUserSpeaker ? styles.user : styles.brain}
       ${messageContent.length > 100 && isUserSpeaker ? styles.smaller : ""}
       ${lastMessage ? styles.last : ""}
       `}
-      >
-        {!isUserSpeaker && messageContent !== "🧠" && (
-          <div onClick={() => setFolded(!folded)}>
-            <Icon
-              name="chevronDown"
-              color="black"
-              handleHover={true}
-              size="normal"
-              classname={`${styles.icon_rotate} ${
-                folded ? styles.icon_rotate_down : styles.icon_rotate_up
-              }`}
-            />
-          </div>
-        )}
-        {renderMessageHeader()}
-        <div ref={ref} className={styles.message_row_content}>
-          {children ?? (
-            <>
-              <MessageContent
-                text={messageContent}
-                isUser={isUserSpeaker}
-                hide={folded}
-              />
-            </>
-          )}
+    >
+      {!isUserSpeaker && messageContent !== "🧠" && (
+        <div onClick={() => setFolded(!folded)}>
+          <Icon
+            name="chevronDown"
+            color="black"
+            handleHover={true}
+            size="normal"
+            classname={`${styles.icon_rotate} ${
+              folded ? styles.icon_rotate_down : styles.icon_rotate_up
+            }`}
+          />
         </div>
-        {renderOtherSections()}
+      )}
+      {renderMessageHeader()}
+      <div className={styles.message_row_content}>
+        {children ?? (
+          <>
+            <MessageContent
+              text={messageContent}
+              isUser={isUserSpeaker}
+              hide={folded}
+            />
+          </>
+        )}
       </div>
-    );
-  }
-);
-
-MessageRow.displayName = "MessageRow";
-
-export default MessageRow;
+      {renderOtherSections()}
+    </div>
+  );
+};
