ec5c91d5add8223051f82c2485f4c8e6816e4f27
Zewed <dewez.antoine2@gmail.com>
2024-06-18 14:04:29 +0200

    Update message row styles and spacing

--- frontend/app/chat/[chatId]/components/ChatDialogueArea/components/ChatDialogue/components/ChatItem/QADisplay/components/MessageRow/components/Source/Source.tsx
+++ frontend/app/chat/[chatId]/components/ChatDialogueArea/components/ChatDialogue/components/ChatItem/QADisplay/components/MessageRow/components/Source/Source.tsx
@@ -12,12 +12,15 @@ type SourceProps = {
 };
 export const SourceCitations = ({ sourceFile }: SourceProps): JSX.Element => {
   const [isExpanded, setIsExpanded] = useState<boolean>(false);
+  const [hovered, isHovered] = useState<boolean>(false);
 
   return (
     <div className={styles.source_and_citations_container}>
       <div
         className={styles.source_wrapper}
         onClick={() => setIsExpanded(!isExpanded)}
+        onMouseEnter={() => isHovered(true)}
+        onMouseLeave={() => isHovered(false)}
       >
         <a
           onClick={(event) => event.stopPropagation()}
@@ -30,8 +33,7 @@ export const SourceCitations = ({ sourceFile }: SourceProps): JSX.Element => {
             <Icon
               name="externLink"
               size="small"
-              color="black"
-              handleHover={true}
+              color={hovered ? "primary" : "black"}
             />
           </div>
         </a>
