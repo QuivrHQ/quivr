4ee7e1737bef953b43209c8a315c0d5a7a9b8335
Zewed <dewez.antoine2@gmail.com>
2024-06-27 18:58:03 +0200

    chore: Add ci-migration.sh to Dockerfile
    feat(backend): quivr-monorepo and quivr-core package
    
    fix(frontend): settings button
    
    chore: Update docker-compose files to specify platform for backend services
    
    fix(frontend): fix table in frontend
    
    chore(main): release 0.0.274
    
    fix: Update email sender parameters in backend code
    
    closes #2756
    
    refacto(backend): poetry package manager and chat route refactoring
    
    chore(main): release 0.0.273

--- frontend/app/user/components/ApiKeyConfig/ApiKeyConfig.tsx
+++ frontend/app/user/components/ApiKeyConfig/ApiKeyConfig.tsx
@@ -1,30 +1,25 @@
 /* eslint-disable max-lines */
 "use client";
 
-import { useTranslation } from "react-i18next";
-
-import Button from "@/lib/components/ui/Button";
 import { CopyButton } from "@/lib/components/ui/CopyButton";
-import { FieldHeader } from "@/lib/components/ui/FieldHeader/FieldHeader";
+import QuivrButton from "@/lib/components/ui/QuivrButton/QuivrButton";
 
 import styles from "./ApiKeyConfig.module.scss";
 import { useApiKeyConfig } from "./hooks/useApiKeyConfig";
 
 export const ApiKeyConfig = (): JSX.Element => {
   const { apiKey, handleCopyClick, handleCreateClick } = useApiKeyConfig();
-  const { t } = useTranslation(["config"]);
 
   return (
     <div>
-      <FieldHeader iconName="key" label={`Quivr ${t("apiKey")}`} />
       {apiKey === "" ? (
-        <Button
-          data-testid="create-new-key"
-          variant="secondary"
-          onClick={() => void handleCreateClick()}
-        >
-          Create New Key
-        </Button>
+        <QuivrButton
+          iconName="key"
+          color="primary"
+          label="Create new key"
+          onClick={void handleCreateClick()}
+          small={true}
+        />
       ) : (
         <div className={styles.response_wrapper}>
           <span>{apiKey}</span>
--- frontend/app/user/components/Settings/Settings.tsx
+++ frontend/app/user/components/Settings/Settings.tsx
@@ -1,27 +1,67 @@
-import { InfoDisplayer } from "@/lib/components/ui/InfoDisplayer/InfoDisplayer";
+import Icon from "@/lib/components/ui/Icon/Icon";
 
+import { InfoSection } from "./InfoSection/InfoSection";
 import styles from "./Settings.module.scss";
 
 import { ApiKeyConfig } from "../ApiKeyConfig";
-import LanguageSelect from "../LanguageSelect/LanguageSelect";
 import { StripePricingOrManageButton } from "../StripePricingOrManageButton";
 
 type InfoDisplayerProps = {
   email: string;
+  username: string;
+  remainingCredits: number;
 };
 
-export const Settings = ({ email }: InfoDisplayerProps): JSX.Element => {
+export const Settings = ({
+  email,
+  username,
+  remainingCredits,
+}: InfoDisplayerProps): JSX.Element => {
   return (
     <div className={styles.settings_wrapper}>
       <span className={styles.title}>
         General settings and main information
       </span>
-      <InfoDisplayer label="Email" iconName="email">
-        <span>{email}</span>
-      </InfoDisplayer>
-      <LanguageSelect />
-      <ApiKeyConfig />
-      <StripePricingOrManageButton />
+      <div className={styles.infos_wrapper}>
+        <InfoSection iconName="email" title="Email">
+          <span className={styles.bold}>{email}</span>
+        </InfoSection>
+        <InfoSection iconName="user" title="Username">
+          <span className={styles.bold}>{username}</span>
+        </InfoSection>
+        <InfoSection iconName="coin" title="Remaining credits">
+          <div className={styles.remaining_credits}>
+            <span className={styles.credits}>{remainingCredits}</span>
+            <Icon name="coin" color="gold" size="normal" />
+          </div>
+        </InfoSection>
+        <InfoSection iconName="key" title="Quivr API Key">
+          <div className={styles.text_and_button}>
+            <span className={styles.text}>
+              The Quivr API key is a unique identifier that allows you to access
+              and interact with Quivr&apos;s API. It is used to authenticate
+              your requests, ensuring that only authorized users can utilize the
+              API&apos;s functionalities.
+            </span>
+            <div className={styles.button}>
+              <ApiKeyConfig />
+            </div>
+          </div>
+        </InfoSection>
+        <InfoSection iconName="star" title="My plan" last={true}>
+          <div className={styles.text_and_button}>
+            <span className={styles.text}>
+              Customize your subscription to best suit your needs. By upgrading
+              to a premium plan, you gain access to a host of additional
+              benefits, including significantly more chat credits and the
+              ability to create more Brains.
+            </span>
+            <div className={styles.button}>
+              <StripePricingOrManageButton small={true} />
+            </div>
+          </div>
+        </InfoSection>
+      </div>
     </div>
   );
 };
