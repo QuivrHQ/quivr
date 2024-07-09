5b4dddbcfd5b0d700fe17a006e661b30ca9ea7bd
aminediro <aminedirhoussi1@gmail.com>
2024-07-09 17:34:47 +0200

    added from chunk tests

--- backend/core/tests/test_brain.py
+++ backend/core/tests/test_brain.py
@@ -8,6 +8,7 @@ from langchain_core.language_models import FakeListChatModel
 from quivr_core.brain import Brain
 from quivr_core.config import LLMEndpointConfig
 from quivr_core.llm import LLMEndpoint
+from quivr_core.storage.local_storage import TransparentStorage
 
 
 @pytest.fixture
@@ -40,20 +41,40 @@ def test_brain_from_files_exception():
         Brain.from_files(name="test_brain", file_paths=[])
 
 
+def test_brain_from_files_success(llm: LLMEndpoint, embedder, temp_data_file):
+    brain = Brain.from_files(
+        name="test_brain", file_paths=[temp_data_file], embedder=embedder, llm=llm
+    )
+    assert brain.name == "test_brain"
+    assert brain.chat_history == []
+    assert brain.llm == llm
+    assert brain.vector_db.embeddings == embedder
+    # storage
+    assert isinstance(brain.storage, TransparentStorage)
+    assert len(brain.storage.get_files()) == 1
+
+
 def test_brain_ask_txt(llm: LLMEndpoint, embedder, temp_data_file, answers):
     brain = Brain.from_files(
         name="test_brain", file_paths=[temp_data_file], embedder=embedder, llm=llm
     )
-
-    assert brain.llm == llm
-    assert brain.vector_db.embeddings == embedder
-
     answer = brain.ask("question")
 
     assert answer.answer == answers[1]
     assert answer.metadata is not None
     assert answer.metadata.sources is not None
     assert answer.metadata.sources[0].metadata["source"] == temp_data_file
+
+
+@pytest.mark.asyncio
+async def test_brain_from_langchain_docs(llm, embedder):
+    chunk = Document("content_1", metadata={"id": uuid4()})
+    brain = await Brain.afrom_langchain_documents(
+        name="test", langchain_documents=[chunk], embedder=embedder
+    )
+    # No appended files
+    assert len(brain.storage.get_files()) == 0
+    assert len(brain.chat_history) == 0
 
 
 @pytest.mark.asyncio
