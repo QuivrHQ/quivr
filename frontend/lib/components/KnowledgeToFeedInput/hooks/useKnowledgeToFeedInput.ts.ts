2b3efb6c2d16bcd4b0de92a0d76d378fc74caedf
Zewed <dewez.antoine2@gmail.com>
2024-07-12 14:50:20 +0200

    wip

--- frontend/lib/components/KnowledgeToFeedInput/hooks/useKnowledgeToFeedInput.ts.ts
+++ frontend/lib/components/KnowledgeToFeedInput/hooks/useKnowledgeToFeedInput.ts.ts
@@ -62,6 +62,7 @@ export const useKnowledgeToFeedInput = () => {
           brainId,
           formData,
           chat_id,
+          bulk_id: brainId,
         });
       } catch (e: unknown) {
         const errorParams = getAxiosErrorParams(e);
