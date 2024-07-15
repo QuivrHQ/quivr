a2224ea3ee5a3651a2e77742e713739812cbd0ca
Zewed <dewez.antoine2@gmail.com>
2024-07-15 12:20:23 +0200

    wip

--- frontend/lib/components/Menu/components/Notifications/Notification/Notification.tsx
+++ frontend/lib/components/Menu/components/Notifications/Notification/Notification.tsx
@@ -1,3 +1,6 @@
+import Icon from "@/lib/components/ui/Icon/Icon";
+import { LoaderIcon } from "@/lib/components/ui/LoaderIcon/LoaderIcon";
+
 import styles from "./Notification.module.scss";
 
 import { BulkNotification } from "../../../types/types";
@@ -21,16 +24,42 @@ export const Notification = ({
     >
       <span></span>
       <div className={styles.loader_wrapper}>
-        <div className={styles.count}>
-          <span>
-            {
-              bulkNotification.notifications.filter(
+        <div className={styles.left}>
+          <div className={styles.icon_info}>
+            {bulkNotification.notifications.some(
+              (notif) => notif.status === "info"
+            ) ? (
+              <LoaderIcon size="small" color="primary" />
+            ) : bulkNotification.notifications.every(
+                (notif) => notif.status === "success"
+              ) ? (
+              <Icon color="success" name="check" size="small" />
+            ) : (
+              <Icon color="warning" name="warning" size="small" />
+            )}
+          </div>
+
+          <div
+            className={`${styles.count} ${
+              bulkNotification.notifications.every(
                 (notif) => notif.status !== "info"
-              ).length
-            }
-          </span>
-          <span> / </span>
-          <span>{bulkNotification.notifications.length}</span>
+              )
+                ? bulkNotification.notifications.some(
+                    (notif) => notif.status === "error"
+                  )
+                  ? styles.warning
+                  : styles.success
+                : ""
+            }`}
+          >
+            {`${Math.round(
+              (bulkNotification.notifications.filter(
+                (notif) => notif.status !== "info"
+              ).length /
+                bulkNotification.notifications.length) *
+                100
+            )}%`}
+          </div>
         </div>
         <NotificationLoadingBar bulkNotification={bulkNotification} />
       </div>
