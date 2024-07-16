2ad82f46c550a74dda3fcd88369fd7ccaeaf48d6
Stan Girard <stan@quivr.app>
2024-07-16 15:45:20 +0200

    feat: Import QuivrButton from correct path

--- frontend/lib/helpers/iconList.ts
+++ frontend/lib/helpers/iconList.ts
@@ -126,6 +126,7 @@ export const iconList: { [name: string]: IconType } = {
   custom: MdDashboardCustomize,
   delete: MdDeleteOutline,
   discord: FaDiscord,
+  doc: BsFiletypeDocx,
   docx: BsFiletypeDocx,
   download: IoCloudDownloadOutline,
   edit: MdOutlineModeEditOutline,
