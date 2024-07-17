ef4ad429cc2a58bf8f3da69735dc53fd5d2e5cee
aminediro <aminedirhoussi1@gmail.com>
2024-07-17 16:32:10 +0200

    registry files tests fixed

--- backend/core/quivr_core/processor/processor_base.py
+++ backend/core/quivr_core/processor/processor_base.py
@@ -5,8 +5,11 @@ from langchain_core.documents import Document
 from quivr_core.storage.file import FileExtension, QuivrFile
 
 
+# TODO: processors should be cached somewhere ?
+# The processor should be cached by processor type
+# The cache should use a single
 class ProcessorBase(ABC):
-    supported_extensions: list[FileExtension]
+    supported_extensions: list[FileExtension | str]
 
     @abstractmethod
     async def process_file(self, file: QuivrFile) -> list[Document]:
--- backend/core/tests/processor/test_registry.py
+++ backend/core/tests/processor/test_registry.py
@@ -1,9 +1,16 @@
 import pytest
+from langchain_core.documents import Document
 
-from quivr_core.processor.registry import _import_class, get_processor_class
+from quivr_core import registry
+from quivr_core.processor.processor_base import ProcessorBase
+from quivr_core.processor.registry import (
+    _import_class,
+    get_processor_class,
+    register_processor,
+)
 from quivr_core.processor.simple_txt_processor import SimpleTxtProcessor
 from quivr_core.processor.tika_processor import TikaProcessor
-from quivr_core.storage.file import FileExtension
+from quivr_core.storage.file import FileExtension, QuivrFile
 
 
 def test_get_processor_cls():
@@ -32,6 +39,29 @@ def test_get_processor_cls_error():
         get_processor_class(".docx")
 
 
-# def test_register():
-#     print()
-#     pass
+def test_register_new_proc():
+    nprocs = len(registry)
+
+    class TestProcessor(ProcessorBase):
+        supported_extensions = [".test"]
+
+        async def process_file(self, file: QuivrFile) -> list[Document]:
+            return []
+
+    register_processor(".test", TestProcessor)
+    assert len(registry) == nprocs + 1
+
+    cls = get_processor_class(".test")
+    assert cls == TestProcessor
+
+
+def test_register_override_proc():
+    class TestProcessor(ProcessorBase):
+        supported_extensions = [".pdf"]
+
+        async def process_file(self, file: QuivrFile) -> list[Document]:
+            return []
+
+    register_processor(".pdf", TestProcessor, override=True)
+    cls = get_processor_class(FileExtension.pdf)
+    assert cls == TestProcessor
