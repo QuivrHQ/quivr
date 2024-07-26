e7a985ba623caa951dec1c162a158e8e9e7540b5
Zewed <dewez.antoine2@gmail.com>
2024-07-26 16:27:07 +0200

    done

--- backend/api/quivr_api/modules/upload/controller/upload_routes.py
+++ backend/api/quivr_api/modules/upload/controller/upload_routes.py
@@ -82,19 +82,27 @@ async def upload_file(
     except Exception as e:
         print(e)
 
-        notification_service.update_notification_by_id(
-            upload_notification.id if upload_notification else None,
-            NotificationUpdatableProperties(
-                status=NotificationsStatusEnum.ERROR,
-                description=f"There was an error uploading the file: {e}",
-            ),
-        )
+
         if "The resource already exists" in str(e):
+            notification_service.update_notification_by_id(
+                upload_notification.id if upload_notification else None,
+                NotificationUpdatableProperties(
+                    status=NotificationsStatusEnum.ERROR,
+                    description=f"File {uploadFile.filename} already exists in storage.",
+                ),
+            )
             raise HTTPException(
                 status_code=403,
                 detail=f"File {uploadFile.filename} already exists in storage.",
             )
         else:
+            notification_service.update_notification_by_id(
+                upload_notification.id if upload_notification else None,
+                NotificationUpdatableProperties(
+                    status=NotificationsStatusEnum.ERROR,
+                    description=f"There was an error uploading the file",
+                ),
+            )
             raise HTTPException(
                 status_code=500, detail=f"Failed to upload file to storage. {e}"
             )
