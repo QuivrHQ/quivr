628d8c89ecc164547687a277d220fd303b5ba9b8
Zewed <dewez.antoine2@gmail.com>
2024-08-13 16:56:27 +0200

    wip

--- frontend/lib/components/HelpWindow/HelpWindow.tsx
+++ frontend/lib/components/HelpWindow/HelpWindow.tsx
@@ -1,13 +1,48 @@
+import { useEffect, useRef } from "react";
+
 import { useHelpContext } from "@/lib/context/HelpProvider/hooks/useHelpContext";
 
 import styles from "./HelpWindow.module.scss";
 
+import { Icon } from "../ui/Icon/Icon";
+
 export const HelpWindow = (): JSX.Element => {
-  const { isVisible } = useHelpContext();
+  const { isVisible, setIsVisible } = useHelpContext();
+  const helpWindowRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        helpWindowRef.current &&
+        !helpWindowRef.current.contains(event.target as Node)
+      ) {
+        setIsVisible(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [setIsVisible]);
 
   if (!isVisible) {
     return <></>;
   }
 
-  return <div className={styles.help_wrapper}>hey</div>;
+  return (
+    <div className={styles.help_wrapper} ref={helpWindowRef}>
+      <div className={styles.header}>
+        <span className={styles.title}>🧠 What is Quivr ?</span>
+        <Icon
+          name="close"
+          size="normal"
+          color="black"
+          handleHover={true}
+          onClick={() => setIsVisible(false)}
+        />
+      </div>
+    </div>
+  );
 };
