77fd2a42d1f2e769da83372e2b0106978e327c4b
Antoine Dewez <44063631+Zewed@users.noreply.github.com>
2024-08-13 14:52:57 +0200

    fix(frontend): remove onboarding modal (#3001)
    # Description
    
    Please include a summary of the changes and the related issue. Please
    also include relevant motivation and context.
    
    ## Checklist before requesting a review
    
    Please delete options that are not relevant.
    
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my code
    - [ ] I have commented hard-to-understand areas
    - [ ] I have ideally added tests that prove my fix is effective or that
    my feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] Any dependent changes have been merged
    
    ## Screenshots (if appropriate):

--- frontend/app/search/page.tsx
+++ frontend/app/search/page.tsx
@@ -8,12 +8,9 @@ import { useBrainCreationContext } from "@/lib/components/AddBrainModal/brainCre
 import { OnboardingModal } from "@/lib/components/OnboardingModal/OnboardingModal";
 import { PageHeader } from "@/lib/components/PageHeader/PageHeader";
 import { UploadDocumentModal } from "@/lib/components/UploadDocumentModal/UploadDocumentModal";
-import { MessageInfoBox } from "@/lib/components/ui/MessageInfoBox/MessageInfoBox";
-import { QuivrButton } from "@/lib/components/ui/QuivrButton/QuivrButton";
 import { SearchBar } from "@/lib/components/ui/SearchBar/SearchBar";
 import { SmallTabs } from "@/lib/components/ui/SmallTabs/SmallTabs";
 import { useBrainContext } from "@/lib/context/BrainProvider/hooks/useBrainContext";
-import { useOnboardingContext } from "@/lib/context/OnboardingProvider/hooks/useOnboardingContext";
 import { useSupabase } from "@/lib/context/SupabaseProvider";
 import { useUserSettingsContext } from "@/lib/context/UserSettingsProvider/hooks/useUserSettingsContext";
 import { useUserData } from "@/lib/hooks/useUserData";
@@ -26,17 +23,14 @@ import styles from "./page.module.scss";
 
 const Search = (): JSX.Element => {
   const [selectedTab, setSelectedTab] = useState("Models");
-  const [isUserDataFetched, setIsUserDataFetched] = useState(false);
   const [isNewBrain, setIsNewBrain] = useState(false);
   const brainsPerPage = 6;
 
   const pathname = usePathname();
   const { session } = useSupabase();
-  const { isBrainCreationModalOpened, setIsBrainCreationModalOpened } =
-    useBrainCreationContext();
-  const { userIdentityData, userData } = useUserData();
+  const { setIsBrainCreationModalOpened } = useBrainCreationContext();
+  const { userData } = useUserData();
   const { isDarkMode } = useUserSettingsContext();
-  const { isBrainCreated } = useOnboardingContext();
   const { allBrains } = useBrainContext();
 
   const [buttons, setButtons] = useState<ButtonType[]>([
@@ -81,12 +75,7 @@ const Search = (): JSX.Element => {
   };
 
   useEffect(() => {
-    if (userIdentityData) {
-      setIsUserDataFetched(true);
-    }
-  }, [userIdentityData]);
-
-  useEffect(() => {
+    console.info(allBrains.length);
     if (userData) {
       setButtons((prevButtons) => {
         return prevButtons.map((button) => {
@@ -112,73 +101,39 @@ const Search = (): JSX.Element => {
   }, [pathname, session]);
 
   return (
-    <>
-      <div className={styles.main_container}>
-        <div className={styles.page_header}>
-          <PageHeader iconName="home" label="Home" buttons={buttons} />
-        </div>
-        <div className={styles.search_page_container}>
-          <div className={styles.main_wrapper}>
-            <div className={styles.quivr_logo_wrapper}>
-              <QuivrLogo size={80} color={isDarkMode ? "white" : "black"} />
-              <div className={styles.quivr_text}>
-                <span>Talk to </span>
-                <span className={styles.quivr_text_primary}>Quivr</span>
-              </div>
-            </div>
-            <div className={styles.search_bar_wrapper}>
-              <SearchBar newBrain={isNewBrain} />
-            </div>
-            <div className={styles.assistants_container}>
-              <div className={styles.tabs}>
-                <SmallTabs tabList={assistantsTabs} />
-              </div>
-              <BrainsList
-                brains={allBrains}
-                selectedTab={selectedTab}
-                brainsPerPage={brainsPerPage}
-                newBrain={newBrain}
-              />
-            </div>
-          </div>
-        </div>
-        <UploadDocumentModal />
-        <AddBrainModal />
-        <OnboardingModal />
+    <div className={styles.main_container}>
+      <div className={styles.page_header}>
+        <PageHeader iconName="home" label="Home" buttons={buttons} />
       </div>
-      {!isBrainCreationModalOpened &&
-        !userIdentityData?.onboarded &&
-        !isBrainCreated &&
-        !!isUserDataFetched && (
-          <div className={styles.onboarding_overlay}>
-            <div className={styles.main_message_wrapper}>
-              <MessageInfoBox type="tutorial">
-                <div className={styles.main_message}>
-                  <span>Welcome {userIdentityData?.username}!</span>
-                  <span>
-                    We will guide you through your quivr adventure and the
-                    creation of your first brain.
-                  </span>
-                  <span className={styles.bolder}>
-                    First, Press the Create Brain button on the top right corner
-                    to create your first brain.
-                  </span>
-                </div>
-              </MessageInfoBox>
-            </div>
-            <div className={styles.first_brain_button}>
-              <QuivrButton
-                iconName="brain"
-                label="Create Brain"
-                color="primary"
-                onClick={() => {
-                  setIsBrainCreationModalOpened(true);
-                }}
-              />
+      <div className={styles.search_page_container}>
+        <div className={styles.main_wrapper}>
+          <div className={styles.quivr_logo_wrapper}>
+            <QuivrLogo size={80} color={isDarkMode ? "white" : "black"} />
+            <div className={styles.quivr_text}>
+              <span>Talk to </span>
+              <span className={styles.quivr_text_primary}>Quivr</span>
             </div>
           </div>
-        )}
-    </>
+          <div className={styles.search_bar_wrapper}>
+            <SearchBar newBrain={isNewBrain} />
+          </div>
+          <div className={styles.assistants_container}>
+            <div className={styles.tabs}>
+              <SmallTabs tabList={assistantsTabs} />
+            </div>
+            <BrainsList
+              brains={allBrains}
+              selectedTab={selectedTab}
+              brainsPerPage={brainsPerPage}
+              newBrain={newBrain}
+            />
+          </div>
+        </div>
+      </div>
+      <UploadDocumentModal />
+      <AddBrainModal />
+      <OnboardingModal />
+    </div>
   );
 };
 
