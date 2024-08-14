f4ba125b3d736d3444dc4f0b8645bed88a88b9e7
Zewed <dewez.antoine2@gmail.com>
2024-08-14 18:51:20 +0200

    wip

--- frontend/lib/components/HelpWindow/HelpWindow.tsx
+++ frontend/lib/components/HelpWindow/HelpWindow.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { useEffect, useRef } from "react";
 
 import { useHelpContext } from "@/lib/context/HelpProvider/hooks/useHelpContext";
@@ -41,6 +42,111 @@ export const HelpWindow = (): JSX.Element => {
           handleHover={true}
           onClick={() => setIsVisible(false)}
         />
+      </div>
+      <div className={styles.content}>
+        <div className={styles.section}>
+          <span className={styles.title}>🧱 Build your second brains</span>
+          <span className={styles.section_content}>
+            A <strong>Brain</strong> in Quivr is an advanced knowledge system
+            designed to integrate and leverage information from various sources.
+            <ul>
+              <li>
+                <strong>📁 Knowledge Integration</strong>
+                <br /> Connect to and pull data from various platforms like{" "}
+                <span className={styles.connection}>
+                  <strong>Google Drive</strong>{" "}
+                  <Image
+                    src="https://quivr-cms.s3.eu-west-3.amazonaws.com/gdrive_8316d080fd.png"
+                    alt="Google Drive"
+                    width={16}
+                    height={16}
+                  />
+                </span>
+                ,
+                <span className={styles.connection}>
+                  <strong>SharePoint</strong>{" "}
+                  <Image
+                    src="https://quivr-cms.s3.eu-west-3.amazonaws.com/sharepoint_8c41cfdb09.png"
+                    alt="SharePoint"
+                    width={16}
+                    height={16}
+                  />
+                </span>
+                , and{""}
+                <span className={styles.connection}>
+                  <strong className={styles.pre}> Dropbox</strong>{" "}
+                  <Image
+                    src="https://quivr-cms.s3.eu-west-3.amazonaws.com/dropbox_dce4f3d753.png"
+                    alt="Dropbox"
+                    width={16}
+                    height={16}
+                  />
+                </span>
+                . You can also incorporate data from <strong>URLs</strong> and{" "}
+                <strong>files</strong>.
+              </li>
+              <li>
+                <strong>🤖 AI Models</strong>
+                <br /> Utilize powerful models such as{" "}
+                <strong>
+                  <em>GPT</em>
+                </strong>{" "}
+                and{" "}
+                <strong>
+                  <em>Mistral</em>
+                </strong>{" "}
+                to process and understand the integrated knowledge.
+              </li>
+              <li>
+                <strong>🔧 Customization</strong>
+                <br /> Tailor the behavior of your Brain with{" "}
+                <em>custom prompts</em> and <em>settings</em>, such as{" "}
+                <strong>max tokens</strong>, to better suit your needs.
+              </li>
+            </ul>
+            <p>
+              You can also <strong>share</strong> your brains with other Quivr
+              users, allowing them to access and benefit from your knowledge
+              systems. 🤝
+            </p>
+          </span>
+        </div>
+        <div className={styles.section}>
+          <span className={styles.title}>🤖 Talk to AI Models</span>
+          <span className={styles.section_content}>
+            <p>
+              Quivr allows you to <strong>interact directly</strong> with AI
+              models such as{" "}
+              <strong>
+                <em>GPT-4</em>
+              </strong>{" "}
+              and{" "}
+              <strong>
+                <em>Mistral</em>
+              </strong>
+              . Simply start a conversation with the AI to get answers and
+              support based on a broad range of data and knowledge. 🤖✨
+            </p>
+          </span>
+        </div>
+        <div className={styles.section}>
+          <div className={styles.title}>🎯 Select assistant</div>
+          <span className={styles.section_content}>
+            <p>
+              Press
+              <strong> @</strong> to choose the AI model or the Brain you want
+              to interact with.
+            </p>
+          </span>
+          <div className={styles.image}>
+            <Image
+              src="https://quivr-cms.s3.eu-west-3.amazonaws.com/Screen_82ac3783aa.png"
+              width={500}
+              height={100}
+              alt="Quivr"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
