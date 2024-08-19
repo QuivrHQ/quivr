00b03a73d37058f18ebe65ba8f4f5d10b612be68
Zewed <dewez.antoine2@gmail.com>
2024-08-19 20:15:26 +0200

    start

---
+++ frontend/lib/components/BrainSnippet/BrainSnippet.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+export const BrainSnippet = (): JSX.Element => {
+  const [color, setColor] = useState("#aabbcc");
+  const [emoji, setEmoji] = useState("🧠");
+
+  return <div></div>;
+};
