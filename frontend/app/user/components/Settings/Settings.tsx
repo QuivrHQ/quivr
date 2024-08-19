d844f66e0d35d9388d2e76c984b28b67dfbbb943
Antoine Dewez <44063631+Zewed@users.noreply.github.com>
2024-08-19 19:22:56 +0200

    feat(frontend): show or hide tokens relative stuff (#3017)
    # Description
    
    Please include a summary of the changes and the related issue. Please
    also include relevant motivation and context.
    
    ## Checklist before requesting a review
    
    Please delete options that are not relevant.
    
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my code
    - [ ] I have commented hard-to-understand areas
    - [ ] I have ideally added tests that prove my fix is effective or that
    my feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] Any dependent changes have been merged
    
    ## Screenshots (if appropriate):

--- frontend/app/user/components/Settings/Settings.tsx
+++ frontend/app/user/components/Settings/Settings.tsx
@@ -6,6 +6,8 @@ import styles from "./Settings.module.scss";
 
 import { StripePricingOrManageButton } from "../StripePricingOrManageButton";
 
+const showTokensSettings = process.env.NEXT_PUBLIC_SHOW_TOKENS === "true";
+
 type InfoDisplayerProps = {
   email: string;
   username: string;
@@ -29,12 +31,14 @@ export const Settings = ({
         <InfoSection iconName="user" title="Username">
           <span className={styles.bold}>{username}</span>
         </InfoSection>
-        <InfoSection iconName="coin" title="Remaining credits">
-          <div className={styles.remaining_credits}>
-            <span className={styles.credits}>{remainingCredits}</span>
-            <Icon name="coin" color="gold" size="normal" />
-          </div>
-        </InfoSection>
+        {!!showTokensSettings && (
+          <InfoSection iconName="coin" title="Remaining credits">
+            <div className={styles.remaining_credits}>
+              <span className={styles.credits}>{remainingCredits}</span>
+              <Icon name="coin" color="gold" size="normal" />
+            </div>
+          </InfoSection>
+        )}
         <InfoSection iconName="key" title="Quivr API Key">
           <div className={styles.text_and_button}>
             <span className={styles.text}>
@@ -54,19 +58,21 @@ export const Settings = ({
             </div>
           </div>
         </InfoSection>
-        <InfoSection iconName="star" title="My plan" last={true}>
-          <div className={styles.text_and_button}>
-            <span className={styles.text}>
-              Customize your subscription to best suit your needs. By upgrading
-              to a premium plan, you gain access to a host of additional
-              benefits, including significantly more chat credits and the
-              ability to create more Brains.
-            </span>
-            <div className={styles.button}>
-              <StripePricingOrManageButton small={true} />
+        {!!showTokensSettings && (
+          <InfoSection iconName="star" title="My plan" last={true}>
+            <div className={styles.text_and_button}>
+              <span className={styles.text}>
+                Customize your subscription to best suit your needs. By
+                upgrading to a premium plan, you gain access to a host of
+                additional benefits, including significantly more chat credits
+                and the ability to create more Brains.
+              </span>
+              <div className={styles.button}>
+                <StripePricingOrManageButton small={true} />
+              </div>
             </div>
-          </div>
-        </InfoSection>
+          </InfoSection>
+        )}
       </div>
     </div>
   );
