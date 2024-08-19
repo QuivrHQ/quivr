5403017d245a8253779c07b02afe3c8ce3756ede
Zewed <dewez.antoine2@gmail.com>
2024-08-19 17:41:03 +0200

    done

--- frontend/lib/components/ui/EmojiSelector/EmojiSelector.tsx
+++ frontend/lib/components/ui/EmojiSelector/EmojiSelector.tsx
@@ -1,6 +1,10 @@
 // components/EmojiSelector.tsx
-import { EmojiClickData } from "emoji-picker-react";
+import { EmojiClickData, Theme } from "emoji-picker-react";
 import dynamic from "next/dynamic";
+
+import { useUserSettingsContext } from "@/lib/context/UserSettingsProvider/hooks/useUserSettingsContext";
+
+import styles from "./EmojiSelector.module.scss";
 
 const EmojiPicker = dynamic(() => import("emoji-picker-react"), { ssr: false });
 
@@ -9,13 +13,32 @@ const EmojiSelector = ({
 }: {
   onSelectEmoji?: (emoji: string) => void;
 }): JSX.Element => {
+  const { isDarkMode } = useUserSettingsContext();
+
   const onEmojiClick = (emojiObject: EmojiClickData) => {
     if (onSelectEmoji) {
       onSelectEmoji(emojiObject.emoji);
     }
   };
 
-  return <EmojiPicker onEmojiClick={onEmojiClick} />;
+  return (
+    <div className={styles.emoji_picker_wrapper}>
+      <EmojiPicker
+        onEmojiClick={onEmojiClick}
+        lazyLoadEmojis={true}
+        previewConfig={{
+          showPreview: false,
+          defaultCaption: "Your selected emoji",
+          defaultEmoji: "👀",
+        }}
+        theme={
+          isDarkMode
+            ? ("dark" as Theme | undefined)
+            : ("light" as Theme | undefined)
+        }
+      />
+    </div>
+  );
 };
 
 export default EmojiSelector;
