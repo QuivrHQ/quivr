13b0e9ea345b43b90740a9e351d5d588cb8b8cd7
aminediro <aminedirhoussi1@gmail.com>
2024-08-22 12:14:59 +0200

    cleaned up migrations

--- backend/supabase/migrations/20240808141057_knowledge_fix.sql
+++ backend/supabase/migrations/20240808141057_knowledge_fix.sql
@@ -1,6 +1,17 @@
-alter table "public"."knowledge" drop column "file_sha";
+ALTER TABLE "public"."knowledge" RENAME COLUMN "extension" TO "mime_type";
 
-alter table "public"."knowledge" add column "file_sha1" text;
+ALTER TABLE "public"."knowledge" RENAME COLUMN "integration" TO "source";
 
+ALTER TABLE "public"."knowledge" RENAME COLUMN "integration_link" TO "source_link";
+
+ALTER TABLE "public"."knowledge" RENAME COLUMN "file_sha" TO "file_sha1";
+
+alter table "public"."knowledge" add column "created_at" timestamp with time zone default now();
+
+alter table "public"."knowledge" add column "file_size" bigint;
+
+alter table "public"."knowledge" add column "metadata" jsonb;
+
+alter table "public"."knowledge" add column "updated_at" timestamp with time zone default now();
 
 CREATE INDEX knowledge_file_sha1_hash_idx ON knowledge USING hash (file_sha1);
