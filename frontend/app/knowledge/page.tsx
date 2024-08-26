e3585f87261ac79ef95be0cfef6cc0c178789135
Zewed <dewez.antoine2@gmail.com>
2024-08-26 17:42:26 +0200

    feat: Add lazy loading for images in Knowledge page

--- frontend/app/knowledge/page.tsx
+++ frontend/app/knowledge/page.tsx
@@ -1,10 +1,29 @@
 "use client";
 
+import { useEffect } from "react";
+
+import { Sync } from "@/lib/api/sync/types";
+import { useSync } from "@/lib/api/sync/useSync";
 import PageHeader from "@/lib/components/PageHeader/PageHeader";
 
 import styles from "./page.module.scss";
 
 const Knowledge = (): JSX.Element => {
+  const { getUserSyncs } = useSync();
+
+  const fetchUserSyncs = async () => {
+    try {
+      const res: Sync[] = await getUserSyncs();
+      console.info(res);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    void fetchUserSyncs();
+  }, []);
+
   return (
     <div className={styles.main_container}>
       <div className={styles.page_header}>
