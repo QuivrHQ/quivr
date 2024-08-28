513dba31268d8453ef23502891fb02612fc2048a
chloedia <chloedaems0@gmail.com>
2024-08-28 12:31:40 +0200

    add test similarity search

--- backend/api/quivr_api/vector/tests/test_vectors.py
+++ backend/api/quivr_api/vector/tests/test_vectors.py
@@ -1,63 +1,55 @@
-import asyncio
 import os
-import select
 from typing import List, Tuple
 
 import pytest
-import pytest_asyncio
-import sqlalchemy
-from pgvector.sqlalchemy import Vector as PGVector
+from quivr_api.modules.brain.entity.brain_entity import Brain, BrainType
+from quivr_api.modules.dependencies import get_embedding_client
 from quivr_api.modules.knowledge.entity.knowledge import KnowledgeDB
 from quivr_api.vector.entity.vector import Vector
+from quivr_api.vector.repository.vectors_repository import VectorRepository
+from sqlalchemy import create_engine
 from sqlmodel import Session, select
 
-pg_database_base_url = "postgres:postgres@localhost:54322/postgres"
+pg_database_base_url = "postgresql://postgres:postgres@localhost:54322/postgres"
 
-TestData = Tuple[List[Vector], KnowledgeDB]
+TestData = Tuple[List[Vector], KnowledgeDB, Brain]
 
 
 @pytest.fixture(scope="session")
-def event_loop(request: pytest.FixtureRequest):
-    loop = asyncio.get_event_loop_policy().new_event_loop()
-    yield loop
-    loop.close()
-
-
-@pytest_asyncio.fixture(scope="session")
-def async_engine():
-    engine = sqlalchemy.create_engine(
-        "postgresql://" + pg_database_base_url,
+def engine():
+    return create_engine(
+        pg_database_base_url,
         echo=True if os.getenv("ORM_DEBUG") else False,
-        future=True,
-        # NOTE: pessimistic bound on
-        pool_pre_ping=True,
-        pool_size=10,  # NOTE: no bouncer for now, if 6 process workers => 6
-        pool_recycle=1800,
     )
-    yield engine
 
 
-@pytest.fixture()
+@pytest.fixture(scope="function")
 def session(engine):
-    with engine.connect() as conn:
-        conn.begin()
-        conn.begin_nested()
-        session = Session(conn, expire_on_commit=False)
+    connection = engine.connect()
+    transaction = connection.begin()
+    session = Session(bind=connection)
 
-        @sqlalchemy.event.listens_for(session, "after_transaction_end")
-        def end_savepoint(session, transaction):
-            if conn.closed:
-                return
-            if not conn.in_nested_transaction():
-                conn.sync_connection.begin_nested()
+    yield session
 
-        yield session
+    session.close()
+    transaction.rollback()
+    connection.close()
 
 
-@pytest.fixture()
-def test_data(
-    session: Session,
-) -> TestData:
+@pytest.fixture
+def test_data(session: Session) -> TestData:
+    embedding = get_embedding_client()
+    vectors = embedding.embed_documents(
+        ["I love paste a la vongole", "I rather play chess than watch football"]
+    )
+
+    nested = session.begin_nested()  # Start a savepoint
+
+    brain_1 = Brain(
+        name="test_brain",
+        description="this is a test brain",
+        brain_type=BrainType.integration,
+    )
     knowledge_1 = KnowledgeDB(
         file_name="test_file_1",
         mime_type="txt",
@@ -66,39 +58,65 @@ def test_data(
         source_link="test_source_link",
         file_size=100,
         file_sha1="test_sha1",
-        brains=[],
+        brains=[brain_1],
     )
+    session.add(knowledge_1)
+    session.flush()  # This will assign an ID to knowledge_1
     assert knowledge_1.id, "Knowledge ID not generated"
 
     vector_1 = Vector(
         content="vector_1",
-        metadata_={"metadata": "metadata_1"},
-        embedding=PGVector([1, 2, 3]),
+        metadata_={"chunk_size": 16},
+        embedding=vectors[0],  # type: ignore
         knowledge_id=knowledge_1.id,
     )
 
     vector_2 = Vector(
         content="vector_2",
-        metadata_={"metadata": "metadata_2"},
-        embedding=PGVector([4, 5, 6]),
+        metadata_={"chunk_size": 16},
+        embedding=vectors[1],  # type: ignore
         knowledge_id=knowledge_1.id,
     )
 
-    session.add(knowledge_1)
     session.add(vector_1)
     session.add(vector_2)
 
-    session.commit()
-    return [vector_1, vector_2], knowledge_1
+    session.flush()
+
+    yield ([vector_1, vector_2], knowledge_1, brain_1)
+    # @Amine do not get the typing() error
+
+    nested.rollback()  # Roll back the savepoint after the test
 
 
 def test_get_vectors_by_knowledge_id(session: Session, test_data: TestData):
-    vectors, knowledge = test_data
+    vectors, knowledge, _ = test_data
     assert knowledge.id
 
     query = select(Vector).where(Vector.knowledge_id == knowledge.id)
-    results = session.exec(query).all()
+    results = session.execute(query).all()
 
-    assert len(results) == 2
+    assert len(results) == 2, f"Expected 2 vectors, got {len(results)}"
+    assert (
+        results[0][0].content == vectors[0].content
+    ), f"Expected {vectors[0].content}, got {results[0][0].content}"
+    assert (
+        results[1][0].content == vectors[1].content
+    ), f"Expected {vectors[1].content}, got {results[1][0].content}"
+
+
+def test_similarity_search(session: Session, test_data: TestData):
+    vectors, knowledge, brain = test_data
+    assert knowledge.id
+    assert brain.brain_id
+
+    repo = VectorRepository(session)
+
+    k = 2
+    results = repo.similarity_search(vectors[0].embedding, brain.brain_id, k=k)  # type: ignore
+    assert len(results) == k
     assert results[0].content == vectors[0].content
-    assert results[1].content == vectors[1].content
+
+    results = repo.similarity_search(vectors[1].embedding, brain.brain_id, k=k)  # type: ignore
+
+    assert results[0].content == vectors[1].content
