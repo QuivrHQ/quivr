bba725feec37ea66f3c5fbb4bf83581c06364341
chloedia <chloedaems0@gmail.com>
2024-08-30 16:26:38 +0200

    add service test

--- backend/api/quivr_api/vector/tests/test_vectors.py
+++ backend/api/quivr_api/vector/tests/test_vectors.py
@@ -2,11 +2,14 @@ import os
 from typing import List, Tuple
 
 import pytest
+import sqlalchemy
+from langchain.docstore.document import Document
+from langchain_core.embeddings import DeterministicFakeEmbedding
 from quivr_api.modules.brain.entity.brain_entity import Brain, BrainType
-from quivr_api.modules.dependencies import get_embedding_client
 from quivr_api.modules.knowledge.entity.knowledge import KnowledgeDB
 from quivr_api.vector.entity.vector import Vector
 from quivr_api.vector.repository.vectors_repository import VectorRepository
+from quivr_api.vector.service.vector_service import VectorService
 from sqlalchemy import create_engine
 from sqlmodel import Session, select
 
@@ -24,29 +27,35 @@ def engine():
 
 
 @pytest.fixture(scope="function")
+def embedder():
+    return DeterministicFakeEmbedding(size=1536)
+
+
+@pytest.fixture()
 def session(engine):
-    connection = engine.connect()
-    transaction = connection.begin()
-    session = Session(bind=connection)
+    with engine.connect() as conn:
+        conn.begin()
+        conn.begin_nested()
+        sync_session = Session(conn, expire_on_commit=False)
 
-    yield session
+        @sqlalchemy.event.listens_for(sync_session, "after_transaction_end")
+        def end_savepoint(session, transaction):
+            if conn.closed:
+                return
+            if not conn.in_nested_transaction():
+                conn.sync_connection.begin_nested()
 
-    session.close()
-    transaction.rollback()
-    connection.close()
+        yield sync_session
 
 
 @pytest.fixture
-def test_data(session: Session) -> TestData:
-    embedding = get_embedding_client()
-    vectors = embedding.embed_documents(
+def test_data(session: Session, embedder) -> TestData:
+    vectors = embedder.embed_documents(
         [
-            "I love pasta alla vongole because it reminds me of summer evenings by the sea. The combination of garlic, olive oil, white wine, and fresh clams creates a symphony of flavors that transports me to the Italian coast. The al dente pasta soaks up the briny, aromatic sauce, making each bite a delightful journey. It's a dish that embodies simplicity and elegance, perfect for sharing with loved ones over a glass of chilled Pinot Grigio.",
-            "I'd rather play chess than watch football because chess offers a unique blend of strategy, creativity, and intellectual challenge. Each move requires careful thought and planning, engaging my mind in a way that's both stimulating and satisfying. The quiet intensity of a chess match provides a stark contrast to the loud, fast-paced action of football. Plus, chess allows me to enjoy quality time with friends and family, fostering meaningful connections over a timeless game.",
+            "vector_1",
+            "vector_2",
         ]
     )
-
-    nested = session.begin_nested()  # Start a savepoint
 
     brain_1 = Brain(
         name="test_brain",
@@ -86,26 +95,89 @@ def test_data(session: Session) -> TestData:
 
     session.flush()
 
-    yield ([vector_1, vector_2], knowledge_1, brain_1)
-    # @Amine do not get the typing() error
+    return ([vector_1, vector_2], knowledge_1, brain_1)
 
-    nested.rollback()  # Roll back the savepoint after the test
+
+def test_create_vectors_service(session: Session, test_data: TestData, embedder):
+    _, knowledge, _ = test_data
+    assert knowledge.id
+    repo = VectorRepository(session)
+    service = VectorService(repo)
+    service._embedding = embedder
+
+    chunk_1 = Document(page_content="I love eating pasta with tomato sauce")
+    chunk_2 = Document(page_content="I love eating pizza with extra cheese")
+
+    # Create vectors from documents
+    new_vectors_id: List[int] = service.create_vectors([chunk_1, chunk_2], knowledge.id)  # type: ignore
+
+    # Verify the correct number of vectors were created
+    assert len(new_vectors_id) == 2, f"Expected 2 vectors, got {len(new_vectors_id)}"
+
+    # Verify the content of the first vector matches the corresponding document
+    vector_1_content = (
+        session.execute(select(Vector).where(Vector.id == new_vectors_id[0]))
+        .scalars()
+        .first()
+        .content
+    )
+    vector_2_content = (
+        session.execute(select(Vector).where(Vector.id == new_vectors_id[1]))
+        .scalars()
+        .first()
+        .content
+    )
+
+    assert (
+        vector_1_content == chunk_1.page_content
+    ), "The content of the first vector does not match"
+    assert (
+        vector_2_content == chunk_2.page_content
+    ), "The content of the second vector does not match"
 
 
 def test_get_vectors_by_knowledge_id(session: Session, test_data: TestData):
     vectors, knowledge, _ = test_data
     assert knowledge.id
 
-    query = select(Vector).where(Vector.knowledge_id == knowledge.id)
-    results = session.execute(query).all()
+    repo = VectorRepository(session)
+    results = repo.get_vectors_by_knowledge_id(knowledge.id)  # type: ignore
 
     assert len(results) == 2, f"Expected 2 vectors, got {len(results)}"
     assert (
-        results[0][0].content == vectors[0].content
-    ), f"Expected {vectors[0].content}, got {results[0][0].content}"
+        results[0].content == vectors[0].content
+    ), f"Expected {vectors[0].content}, got {results[0].content}"
     assert (
-        results[1][0].content == vectors[1].content
-    ), f"Expected {vectors[1].content}, got {results[1][0].content}"
+        results[1].content == vectors[1].content
+    ), f"Expected {vectors[1].content}, got {results[1].content}"
+
+
+def test_service_similarity_search(session: Session, test_data: TestData, embedder):
+    vectors, knowledge, brain = test_data
+    assert knowledge.id
+    assert brain.brain_id
+
+    repo = VectorRepository(session)
+    service = VectorService(repo)
+    service._embedding = embedder
+
+    k = 2
+    results = service.similarity_search(vectors[0].content, brain.brain_id, k=k)  # type: ignore
+    assert len(results) == k
+    assert results[0].page_content == vectors[0].content
+
+    results = service.similarity_search(vectors[1].content, brain.brain_id, k=k)  # type: ignore
+
+    assert results[0].page_content == vectors[1].content
+
+    k = 1
+    results = service.similarity_search(vectors[0].content, brain.brain_id, k=k)  # type: ignore
+    assert len(results) == k
+    assert results[0].page_content == vectors[0].content
+
+    results = service.similarity_search(vectors[1].content, brain.brain_id, k=k)  # type: ignore
+
+    assert results[0].page_content == vectors[1].content
 
 
 def test_similarity_search(session: Session, test_data: TestData):
