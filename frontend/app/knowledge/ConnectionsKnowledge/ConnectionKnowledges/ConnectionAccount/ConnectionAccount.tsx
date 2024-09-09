d30e386fc46d7ada3d844c868d66c8c52aaf8164
Zewed <dewez.antoine2@gmail.com>
2024-09-09 15:49:21 +0200

    wip

--- frontend/app/knowledge/ConnectionsKnowledge/ConnectionKnowledges/ConnectionAccount/ConnectionAccount.tsx
+++ frontend/app/knowledge/ConnectionsKnowledge/ConnectionKnowledges/ConnectionAccount/ConnectionAccount.tsx
@@ -4,6 +4,7 @@ import { Sync, SyncElements } from "@/lib/api/sync/types"; // Assurez-vous que S
 import { useSync } from "@/lib/api/sync/useSync";
 import { ConnectionIcon } from "@/lib/components/ui/ConnectionIcon/ConnectionIcon";
 import { Icon } from "@/lib/components/ui/Icon/Icon";
+import { LoaderIcon } from "@/lib/components/ui/LoaderIcon/LoaderIcon";
 
 import styles from "./ConnectionAccount.module.scss";
 
@@ -19,15 +20,17 @@ const ConnectionAccount = ({
   index,
 }: ConnectionAccountProps): JSX.Element => {
   const { getSyncFiles } = useSync();
+  const [loading, setLoading] = useState(false);
   const [syncElements, setSyncElements] = useState<SyncElements>();
   const [folded, setFolded] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     void (async () => {
       try {
         const res = await getSyncFiles(sync.id);
-        console.info(res);
         setSyncElements(res);
+        setLoading(false);
       } catch (error) {
         console.error("Failed to get sync files:", error);
       }
@@ -47,17 +50,28 @@ const ConnectionAccount = ({
         <ConnectionIcon letter={sync.email[0]} index={index} />
         <span>{sync.email}</span>
       </div>
-      {!folded && (
-        <div className={styles.sync_elements_wrapper}>
-          {syncElements?.files
-            .filter((file) => file.is_folder)
-            .map((element, id) => (
-              <div key={id}>
-                <SyncFolder element={element} indent={2} />
-              </div>
-            ))}
-        </div>
-      )}
+      {!folded &&
+        (loading ? (
+          <div className={styles.loader_icon}>
+            <LoaderIcon color="primary" size="small" />
+          </div>
+        ) : (
+          <div
+            className={`${styles.sync_elements_wrapper} ${
+              !syncElements?.files.filter((file) => file.is_folder).length
+                ? styles.empty
+                : ""
+            }`}
+          >
+            {syncElements?.files
+              .filter((file) => file.is_folder)
+              .map((element, id) => (
+                <div key={id}>
+                  <SyncFolder element={element} indent={2} />
+                </div>
+              ))}
+          </div>
+        ))}
     </div>
   );
 };
