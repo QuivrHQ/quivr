3034f041d8b231954742497442779f682a81c711
Zewed <dewez.antoine2@gmail.com>
2024-09-09 17:09:54 +0200

    wip

--- frontend/app/knowledge/ConnectionsKnowledge/ConnectionKnowledges/SyncFolder/SyncFolder.tsx
+++ frontend/app/knowledge/ConnectionsKnowledge/ConnectionKnowledges/SyncFolder/SyncFolder.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { SyncElement, SyncElements } from "@/lib/api/sync/types";
 import { useSync } from "@/lib/api/sync/useSync";
 import { Icon } from "@/lib/components/ui/Icon/Icon";
+import { LoaderIcon } from "@/lib/components/ui/LoaderIcon/LoaderIcon";
 
 import styles from "./SyncFolder.module.scss";
 
@@ -18,21 +19,25 @@ const SyncFolder = ({
   syncId,
 }: SyncFolderProps): JSX.Element => {
   const [folded, setFolded] = useState(true);
+  const [loading, setLoading] = useState(false);
   const { getSyncFiles } = useSync();
   const [syncElements, setSyncElements] = useState<SyncElements>();
   const paddingLeft = `${16 * indent}px`;
 
   useEffect(() => {
-    void (async () => {
-      try {
-        const res = await getSyncFiles(syncId, element.id);
-        console.info(res);
-        setSyncElements(res);
-      } catch (error) {
-        console.error("Failed to get sync files:", error);
-      }
-    })();
-  }, [element.id]);
+    if (!folded) {
+      setLoading(true);
+      void (async () => {
+        try {
+          const res = await getSyncFiles(syncId, element.id);
+          setSyncElements(res);
+          setLoading(false);
+        } catch (error) {
+          console.error("Failed to get sync files:", error);
+        }
+      })();
+    }
+  }, [folded]);
 
   return (
     <div className={styles.folder_wrapper} style={{ paddingLeft }}>
@@ -46,21 +51,32 @@ const SyncFolder = ({
         />
         <span>{element.name}</span>
       </div>
-      {!folded && (
-        <div className={styles.sync_elements_wrapper}>
-          {syncElements?.files
-            .filter((file) => file.is_folder)
-            .map((folder, id) => (
-              <div key={id}>
-                <SyncFolder
-                  element={folder}
-                  indent={indent + 1}
-                  syncId={syncId}
-                />
-              </div>
-            ))}
-        </div>
-      )}
+      {!folded &&
+        (loading ? (
+          <div className={styles.loader_icon}>
+            <LoaderIcon color="primary" size="small" />
+          </div>
+        ) : (
+          <div
+            className={`${styles.sync_elements_wrapper} ${
+              !syncElements?.files.filter((file) => file.is_folder).length
+                ? styles.empty
+                : ""
+            }`}
+          >
+            {syncElements?.files
+              .filter((file) => file.is_folder)
+              .map((folder, id) => (
+                <div key={id}>
+                  <SyncFolder
+                    element={folder}
+                    indent={indent}
+                    syncId={syncId}
+                  />
+                </div>
+              ))}
+          </div>
+        ))}
     </div>
   );
 };
