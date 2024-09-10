faff9891ad6935ed1bf0e562009a64909a933ba9
Jacopo Chevallard <jacopo.chevallard@mailfence.com>
2024-09-10 12:14:44 +0200

    add: added Pydantic class 'RerankerConfig' to manage the configuration of rerankers

--- backend/core/quivr_core/config.py
+++ backend/core/quivr_core/config.py
@@ -1,5 +1,10 @@
 from pydantic import BaseModel
 
+RERANKERS_DEFAULT_MODELS = {
+    'cohere': "rerank-multilingual-v3.0",
+    'jina': "jina-reranker-v2-base-multilingual",
+    # Add more suppliers as needed
+}
 
 class LLMEndpointConfig(BaseModel):
     model: str = "gpt-3.5-turbo-0125"
@@ -10,7 +15,25 @@ class LLMEndpointConfig(BaseModel):
     temperature: float = 0.7
     streaming: bool = True
 
+# Cannot use Pydantic v2 field_validator because of conflicts with pydantic v1 still in use in LangChain
+class RerankerConfig(BaseModel):
+    supplier: str
+    model: str | None = None
+    top_n: int = 5
 
+    def __init__(self, **data):
+        super().__init__(**data)  # Call Pydantic's BaseModel init
+        self.validate_model()  # Automatically call external validation
+
+    def validate_model(self):
+        # Custom external validation logic
+        if self.model is None:
+            # Set default model based on supplier if not provided
+            try:
+                self.model = RERANKERS_DEFAULT_MODELS[self.supplier]
+            except:
+                raise ValueError(f"Unknown supplier: {self.supplier}")
+        
 class RAGConfig(BaseModel):
     llm_config: LLMEndpointConfig = LLMEndpointConfig()
     max_history: int = 10
