ca492931b91570cb407f08a96a6ddcc1432e7e80
Zewed <dewez.antoine2@gmail.com>
2024-09-11 21:33:18 +0200

    small

--- frontend/app/knowledge/CurrentFolderExplorer/CurrentFolderExplorerLine/CurrentFolderExplorerLine.tsx
+++ frontend/app/knowledge/CurrentFolderExplorer/CurrentFolderExplorerLine/CurrentFolderExplorerLine.tsx
@@ -5,6 +5,8 @@ import { Icon } from "@/lib/components/ui/Icon/Icon";
 
 import styles from "./CurrentFolderExplorerLine.module.scss";
 
+import { useKnowledgeContext } from "../../KnowledgeProvider/hooks/useKnowledgeContext";
+
 // Fonction pour associer des extensions de fichier à des couleurs spécifiques
 const getColorByExtension = (extension: string): string => {
   const extensionColorMap: { [key: string]: string } = {
@@ -43,6 +45,8 @@ interface CurrentFolderExplorerLineProps {
 const CurrentFolderExplorerLine = ({
   element,
 }: CurrentFolderExplorerLineProps): JSX.Element => {
+  const { setCurrentFolder } = useKnowledgeContext();
+
   const extension = element.name?.split(".").pop()?.toLowerCase() ?? "file";
   const iconColor = getColorByExtension(extension);
 
@@ -51,6 +55,11 @@ const CurrentFolderExplorerLine = ({
       className={`${styles.folder_explorer_line_wrapper} ${
         element.is_folder ? styles.folder : ""
       }`}
+      onClick={() => {
+        if (element.is_folder) {
+          setCurrentFolder(element);
+        }
+      }}
     >
       <div className={styles.left}>
         {element.is_folder ? (
