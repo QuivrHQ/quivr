075f8c0837368a8da8c78b05ecfacf0777b3f589
Jacopo Chevallard <jacopo.chevallard@mailfence.com>
2024-09-11 17:55:56 +0200

    feat: adding configuration to Megaparse

---
+++ backend/core/MegaParse/megaparse/config.py
@@ -0,0 +1,22 @@
+from enum import Enum
+from pydantic import BaseModel
+import yaml
+class PdfParser(str, Enum):
+    LLAMA_PARSE = "llama_parse"
+    UNSTRUCTURED = "unstructured"
+    MEGAPARSE_VISION = "megaparse_vision"
+
+class MegaparseBaseConfig(BaseModel):
+    @classmethod
+    def from_yaml(cls, file_path: str):
+        # Load the YAML file
+        with open(file_path, 'r') as stream:
+            config_data = yaml.safe_load(stream)
+        
+        # Instantiate the class using the YAML data
+        return cls(**config_data)
+
+class MegaparseConfig(MegaparseBaseConfig):
+    strategy: str = "fast"
+    llama_parse_api_key: str | None = None
+    pdf_parser: PdfParser = PdfParser.LLAMA_PARSE
--- backend/core/quivr_core/processor/implementations/megaparse_config.py
+++
@@ -1,6 +0,0 @@
-from quivr_core.base_config import QuivrBaseConfig
-
-
-class MegaparseConfig(QuivrBaseConfig):
-    strategy: str = "fast"
-    llama_parse_api_key: str | None = None
