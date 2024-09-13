7d1c7789151a7a1db56ee143b65377a273743bd0
Zewed <dewez.antoine2@gmail.com>
2024-09-13 19:38:45 +0200

    wip

--- frontend/app/quality-assistant/page.tsx
+++ frontend/app/quality-assistant/page.tsx
@@ -1,7 +1,45 @@
 "use client";
 
+import { useState } from "react";
+
+import PageHeader from "@/lib/components/PageHeader/PageHeader";
+import { Tabs } from "@/lib/components/ui/Tabs/Tabs";
+import { Tab } from "@/lib/types/Tab";
+
+import styles from "./page.module.scss";
+
 const QualityAssistant = (): JSX.Element => {
-  return <div></div>;
+  const [selectedTab, setSelectedTab] = useState("Assistant");
+
+  const qualityAssistantTab: Tab[] = [
+    {
+      label: "Assistant",
+      isSelected: selectedTab === "Assistant",
+      onClick: () => setSelectedTab("Assistant"),
+      iconName: "edit",
+    },
+    {
+      label: "Process",
+      isSelected: selectedTab === "Process",
+      onClick: () => setSelectedTab("Process"),
+      iconName: "graph",
+    },
+  ];
+
+  return (
+    <div className={styles.page_wrapper}>
+      <div className={styles.page_header}>
+        <PageHeader
+          iconName="assistant"
+          label="Quality Assistant"
+          buttons={[]}
+        />
+      </div>
+      <div className={styles.content_wrapper}>
+        <Tabs tabList={qualityAssistantTab} />
+      </div>
+    </div>
+  );
 };
 
 export default QualityAssistant;
