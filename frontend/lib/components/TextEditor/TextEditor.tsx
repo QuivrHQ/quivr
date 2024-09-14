b5fcc45a16cc36491b4600d4c2562b50d8deb596
adityanandanx <nandanaditya985@gmail.com>
2024-09-14 13:52:53 +0530

    style: fix inline code

--- frontend/lib/components/TextEditor/TextEditor.tsx
+++ frontend/lib/components/TextEditor/TextEditor.tsx
@@ -4,6 +4,8 @@ import { Link } from "@tiptap/extension-link";
 import { EditorContent, useEditor } from "@tiptap/react";
 import { StarterKit } from "@tiptap/starter-kit";
 
+import { useBrainMention } from "@/app/chat/[chatId]/components/ActionsBar/components/ChatInput/components/ChatEditor/Editor/hooks/useBrainMention";
+
 import styles from "./TextEditor.module.scss";
 import { Toolbar } from "./components/Toolbar/Toolbar";
 
@@ -13,26 +15,31 @@ const defaultContent = `
   <h1>My Note</h1>
   <p>
     This is a note with the help of which you can jot down ideas and store them in your brains.<br>
-    Start by typing @ to select a brain!
+    Start by typing <code>@</code> to select a brain!
   </p>
 `;
 
 export const TextEditor = (): JSX.Element => {
-  const editor = useEditor({
-    extensions: [
-      StarterKit.configure({}),
-      Focus.configure({
-        className: styles.has_focus,
-        mode: "shallowest",
-      }),
-      Link.configure({
-        openOnClick: false,
-      }),
-    ],
-    content: defaultContent,
-    immediatelyRender: false,
-    autofocus: true,
-  });
+  const { BrainMention, items } = useBrainMention();
+  const editor = useEditor(
+    {
+      extensions: [
+        StarterKit.configure({}),
+        Focus.configure({
+          className: styles.has_focus,
+          mode: "shallowest",
+        }),
+        Link.configure({
+          openOnClick: false,
+        }),
+        BrainMention,
+      ],
+      content: defaultContent,
+      immediatelyRender: false,
+      autofocus: true,
+    },
+    [items.length]
+  );
 
   if (!editor) {
     return <></>;
