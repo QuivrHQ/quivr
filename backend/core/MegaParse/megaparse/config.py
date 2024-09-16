fe6b7d402ff50b749fdcb00ee263a01a9c179512
Jacopo Chevallard <jacopo.chevallard@mailfence.com>
2024-09-16 10:42:33 +0200

    feat: setting unstructured as the default parser

--- backend/core/MegaParse/megaparse/config.py
+++ backend/core/MegaParse/megaparse/config.py
@@ -1,22 +1,27 @@
 from enum import Enum
-from pydantic import BaseModel
+
 import yaml
+from pydantic import BaseModel
+
+
 class PdfParser(str, Enum):
     LLAMA_PARSE = "llama_parse"
     UNSTRUCTURED = "unstructured"
     MEGAPARSE_VISION = "megaparse_vision"
 
+
 class MegaparseBaseConfig(BaseModel):
     @classmethod
     def from_yaml(cls, file_path: str):
         # Load the YAML file
-        with open(file_path, 'r') as stream:
+        with open(file_path, "r") as stream:
             config_data = yaml.safe_load(stream)
-        
+
         # Instantiate the class using the YAML data
         return cls(**config_data)
+
 
 class MegaparseConfig(MegaparseBaseConfig):
     strategy: str = "fast"
     llama_parse_api_key: str | None = None
-    pdf_parser: PdfParser = PdfParser.LLAMA_PARSE
+    pdf_parser: PdfParser = PdfParser.UNSTRUCTURED
