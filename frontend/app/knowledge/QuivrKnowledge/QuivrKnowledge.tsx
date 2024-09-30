99aa39b243e2179c73ed156d9517e21e775b0715
Zewed <dewez.antoine2@gmail.com>
2024-09-30 15:33:50 +0200

    quivrKnowledge components

---
+++ frontend/app/knowledge/QuivrKnowledge/QuivrKnowledge.tsx
@@ -0,0 +1,7 @@
+import styles from "./QuivrKnowledge.module.scss";
+
+const QuivrKnowledge = (): JSX.Element => {
+  return <div className={styles.account_section_wrapper}></div>;
+};
+
+export default QuivrKnowledge;
