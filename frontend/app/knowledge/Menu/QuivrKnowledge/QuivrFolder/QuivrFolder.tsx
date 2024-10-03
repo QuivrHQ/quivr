6a47536a856d1831660cc86ccf883d5fc8aea264
Zewed <dewez.antoine2@gmail.com>
2024-10-03 14:45:37 +0200

    wip

---
+++ frontend/app/knowledge/Menu/QuivrKnowledge/QuivrFolder/QuivrFolder.tsx
@@ -0,0 +1,106 @@
+import { useEffect, useState } from "react";
+
+import { useKnowledgeContext } from "@/app/knowledge/KnowledgeProvider/hooks/useKnowledgeContext";
+import { useKnowledgeApi } from "@/lib/api/knowledge/useKnowledgeApi";
+import { KMSElement } from "@/lib/api/sync/types";
+import { Icon } from "@/lib/components/ui/Icon/Icon";
+import { LoaderIcon } from "@/lib/components/ui/LoaderIcon/LoaderIcon";
+
+import styles from "./QuivrFolder.module.scss";
+
+interface QuivrFolderProps {
+  element: KMSElement;
+}
+
+const QuivrFolder = ({ element }: QuivrFolderProps): JSX.Element => {
+  const [folded, setFolded] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const { getFiles } = useKnowledgeApi();
+  const [kmsElements, setKMSElements] = useState<KMSElement[]>();
+  const [selectedFolder, setSelectedFolder] = useState<boolean>(false);
+
+  const {
+    currentFolder,
+    setCurrentFolder,
+    setExploringQuivr,
+    setProviderRootSelected,
+  } = useKnowledgeContext();
+
+  useEffect(() => {
+    setSelectedFolder(currentFolder?.id === element.id);
+    setExploringQuivr(true);
+    setProviderRootSelected(undefined);
+  }, [currentFolder]);
+
+  useEffect(() => {
+    if (!folded) {
+      setLoading(true);
+      void (async () => {
+        try {
+          const res = await getFiles(element.id);
+          setKMSElements(res);
+          setLoading(false);
+        } catch (error) {
+          console.error("Failed to get sync files:", error);
+          setLoading(false);
+        }
+      })();
+    }
+  }, [folded]);
+
+  return (
+    <div
+      className={`${styles.folder_wrapper} ${
+        !kmsElements?.filter((file) => file.is_folder).length && !loading
+          ? styles.empty
+          : ""
+      }`}
+    >
+      <div className={styles.folder_line_wrapper}>
+        <Icon
+          name={folded ? "chevronRight" : "chevronDown"}
+          size="normal"
+          color="dark-grey"
+          handleHover={true}
+          onClick={() => setFolded(!folded)}
+        />
+        <span
+          className={`${styles.name} ${selectedFolder ? styles.selected : ""}`}
+          onClick={() => {
+            setCurrentFolder({
+              ...element,
+              parentKMSElement: element.parentKMSElement,
+            });
+          }}
+        >
+          {element.file_name?.includes(".")
+            ? element.file_name.split(".").slice(0, -1).join(".")
+            : element.file_name}
+        </span>
+      </div>
+      {!folded &&
+        (loading ? (
+          <div className={styles.loader_icon}>
+            <LoaderIcon color="primary" size="small" />
+          </div>
+        ) : (
+          <div className={styles.kms_elements_wrapper}>
+            {kmsElements
+              ?.filter((file) => file.is_folder)
+              .map((folder, id) => (
+                <div key={id}>
+                  <QuivrFolder
+                    element={{
+                      ...folder,
+                      parentKMSElement: element,
+                    }}
+                  />
+                </div>
+              ))}
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default QuivrFolder;
--- frontend/app/knowledge/Menu/QuivrKnowledge/QuivrKnowledges.tsx
+++ frontend/app/knowledge/Menu/QuivrKnowledge/QuivrKnowledges.tsx
@@ -1,47 +1,84 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { useKnowledgeApi } from "@/lib/api/knowledge/useKnowledgeApi";
+import { KMSElement } from "@/lib/api/sync/types";
 import { QuivrLogo } from "@/lib/assets/QuivrLogo";
 import { Icon } from "@/lib/components/ui/Icon/Icon";
+import { LoaderIcon } from "@/lib/components/ui/LoaderIcon/LoaderIcon";
 import { useUserSettingsContext } from "@/lib/context/UserSettingsProvider/hooks/useUserSettingsContext";
 
+import QuivrFolder from "./QuivrFolder/QuivrFolder";
 import styles from "./QuivrKnowledges.module.scss";
 
 import { useKnowledgeContext } from "../../KnowledgeProvider/hooks/useKnowledgeContext";
 
 const QuivrKnowledges = (): JSX.Element => {
   const [folded, setFolded] = useState(true);
+  const [kmsElements, setKMSElements] = useState<KMSElement[]>();
+  const [loading, setLoading] = useState(false);
   const { isDarkMode } = useUserSettingsContext();
   const { setExploringQuivr, setCurrentFolder, setProviderRootSelected } =
     useKnowledgeContext();
 
   const { getFiles } = useKnowledgeApi();
 
-  const fetchFiles = async () => {
-    try {
-      const res = await getFiles(null);
-      console.info(res);
-      setCurrentFolder(undefined);
-      setProviderRootSelected(undefined);
-      setExploringQuivr(true);
-    } catch (error) {
-      console.error("Failed to get files:", error);
-    }
+  const fetchFiles = () => {
+    setCurrentFolder(undefined);
+    setProviderRootSelected(undefined);
+    setExploringQuivr(true);
   };
 
+  useEffect(() => {
+    setLoading(true);
+    void (async () => {
+      try {
+        const res = await getFiles(null);
+        setKMSElements(res);
+        setLoading(false);
+      } catch (error) {
+        console.error("Failed to get sync files:", error);
+      }
+    })();
+  }, []);
+
   return (
-    <div className={styles.header_section_wrapper}>
-      <Icon
-        name={folded ? "chevronRight" : "chevronDown"}
-        size="normal"
-        color="dark-grey"
-        handleHover={true}
-        onClick={() => setFolded(!folded)}
-      />
-      <div className={styles.hoverable} onClick={() => void fetchFiles()}>
-        <QuivrLogo size={18} color={isDarkMode ? "white" : "black"} />
-        <span className={styles.provider_title}>Quivr</span>
+    <div className={styles.main_container}>
+      <div className={styles.header_section_wrapper}>
+        <Icon
+          name={folded ? "chevronRight" : "chevronDown"}
+          size="normal"
+          color="dark-grey"
+          handleHover={true}
+          onClick={() => setFolded(!folded)}
+        />
+        <div className={styles.hoverable} onClick={() => void fetchFiles()}>
+          <QuivrLogo size={18} color={isDarkMode ? "white" : "black"} />
+          <span className={styles.provider_title}>Quivr</span>
+        </div>
       </div>
+      {!folded ? (
+        loading ? (
+          <div className={styles.loader_icon}>
+            <LoaderIcon color="primary" size="small" />
+          </div>
+        ) : (
+          <div
+            className={`${styles.sync_elements_wrapper} ${
+              !kmsElements?.filter((file) => file.is_folder).length
+                ? styles.empty
+                : ""
+            } `}
+          >
+            {kmsElements
+              ?.filter((file) => file.is_folder)
+              .map((element, id) => (
+                <div key={id}>
+                  <QuivrFolder element={element} />
+                </div>
+              ))}
+          </div>
+        )
+      ) : null}
     </div>
   );
 };
