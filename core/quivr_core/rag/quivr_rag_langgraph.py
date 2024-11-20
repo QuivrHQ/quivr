d151e8b9d31586cadf86f10b0ca2ae2cc5b85c9c
Jacopo Chevallard <jacopo.chevallard@mailfence.com>
2024-11-20 17:22:50 +0100

    refacto: removing unused class

--- core/quivr_core/rag/quivr_rag_langgraph.py
+++ core/quivr_core/rag/quivr_rag_langgraph.py
@@ -68,35 +68,6 @@ class SplittedInput(BaseModel):
     )
 
 
-class TasksCompletion__(BaseModel):
-    completable_tasks_reasoning: Optional[str] = Field(
-        default=None,
-        description="The reasoning that leads to identifying the user tasks or questions that can be completed using the provided context and chat history.",
-    )
-    completable_tasks: Optional[List[str]] = Field(
-        default_factory=list,
-        description="The user tasks or questions that can be completed using the provided context and chat history.",
-    )
-
-    non_completable_tasks_reasoning: Optional[str] = Field(
-        default=None,
-        description="The reasoning that leads to identifying the user tasks or questions that cannot be completed using the provided context and chat history.",
-    )
-    non_completable_tasks: Optional[List[str]] = Field(
-        default_factory=list,
-        description="The user tasks or questions that need a tool to be completed.",
-    )
-
-    tool_reasoning: Optional[str] = Field(
-        default=None,
-        description="The reasoning that leads to identifying the tool that shall be used to complete the tasks.",
-    )
-    tool: Optional[str] = Field(
-        default_factory=list,
-        description="The tool that shall be used to complete the tasks.",
-    )
-
-
 class TasksCompletion(BaseModel):
     is_task_completable_reasoning: Optional[str] = Field(
         default=None,
