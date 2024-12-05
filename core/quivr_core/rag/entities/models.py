35105d622af89571dd0653d7b12885f8a51eb989
Jacopo Chevallard <jacopo.chevallard@mailfence.com>
2024-12-05 09:21:22 +0100

    feat: removing dependency on Pydantic V1

--- core/quivr_core/rag/entities/models.py
+++ core/quivr_core/rag/entities/models.py
@@ -5,31 +5,29 @@ from uuid import UUID
 
 from langchain_core.documents import Document
 from langchain_core.messages import AIMessage, HumanMessage
-from langchain_core.pydantic_v1 import BaseModel as BaseModelV1
-from langchain_core.pydantic_v1 import Field as FieldV1
 from pydantic import BaseModel, Field
 from typing_extensions import TypedDict
 
 
-class cited_answer(BaseModelV1):
+class cited_answer(BaseModel):
     """Answer the user question based only on the given sources, and cite the sources used."""
 
-    answer: str = FieldV1(
+    answer: str = Field(
         ...,
         description="The answer to the user question, which is based only on the given sources.",
     )
-    citations: list[int] = FieldV1(
+    citations: list[int] = Field(
         ...,
         description="The integer IDs of the SPECIFIC sources which justify the answer.",
     )
 
-    followup_questions: list[str] = FieldV1(
+    followup_questions: list[str] = Field(
         ...,
         description="Generate up to 3 follow-up questions that could be asked based on the answer given or context provided.",
     )
 
 
-class ChatMessage(BaseModelV1):
+class ChatMessage(BaseModel):
     chat_id: UUID
     message_id: UUID
     brain_id: UUID | None
@@ -108,7 +106,6 @@ class QuivrKnowledge(BaseModel):
     metadata: Optional[Dict[str, str]] = None
 
 
-# NOTE: for compatibility issues with langchain <-> PydanticV1
-class SearchResult(BaseModelV1):
+class SearchResult(BaseModel):
     chunk: Document
     distance: float
