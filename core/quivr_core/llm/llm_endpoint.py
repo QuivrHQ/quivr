de7e1905c7f0063e82cd823c3b3c2cb48a16b70b
Jacopo Chevallard <jacopo.chevallard@mailfence.com>
2024-12-16 17:46:49 +0100

    feat: using ChatMistralAI to avoid errors as in https://github.com/langchain-ai/langchainjs/issues/5144

--- core/quivr_core/llm/llm_endpoint.py
+++ core/quivr_core/llm/llm_endpoint.py
@@ -5,6 +5,7 @@ from urllib.parse import parse_qs, urlparse
 
 import tiktoken
 from langchain_anthropic import ChatAnthropic
+from langchain_mistralai import ChatMistralAI
 from langchain_core.language_models.chat_models import BaseChatModel
 from langchain_openai import AzureChatOpenAI, ChatOpenAI
 from pydantic import SecretStr
@@ -54,7 +55,7 @@ class LLMEndpoint:
 
     @classmethod
     def from_config(cls, config: LLMEndpointConfig = LLMEndpointConfig()):
-        _llm: Union[AzureChatOpenAI, ChatOpenAI, ChatAnthropic]
+        _llm: Union[AzureChatOpenAI, ChatOpenAI, ChatAnthropic, ChatMistralAI]
         try:
             if config.supplier == DefaultModelSuppliers.AZURE:
                 # Parse the URL
@@ -90,6 +91,15 @@ class LLMEndpoint:
                     else None,
                     base_url=config.llm_base_url,
                     max_tokens=config.max_output_tokens,
+                    temperature=config.temperature,
+                )
+            elif config.supplier == DefaultModelSuppliers.MISTRAL:
+                _llm = ChatMistralAI(
+                    model=config.model,
+                    api_key=SecretStr(config.llm_api_key)
+                    if config.llm_api_key
+                    else None,
+                    base_url=config.llm_base_url,
                     temperature=config.temperature,
                 )
             else:
