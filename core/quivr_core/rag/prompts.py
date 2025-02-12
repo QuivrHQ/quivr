699b5495f5519e79fd2a6d0e362402c3c77d06b8
Chloé Daems <73901882+chloedia@users.noreply.github.com>
2025-02-12 11:20:19 +0100

    fix: Zendesk system prompt (#3592)
    * Modify zendesk system prompt

--- core/quivr_core/rag/prompts.py
+++ core/quivr_core/rag/prompts.py
@@ -266,6 +266,16 @@ def _define_custom_prompts() -> CustomPromptsDict:
     - Give a the most complete answer to the client query and give relevant links if needed.
     - Based on the following similar client tickets, provide a response to the client query in the same format.
 
+    ------ Output Format ------
+    - You shouln't provide the greetings and the signature as those will be added automatically after generation.
+    - Avoid using lists or bullet points, or any other formatting that feels robotic in the response.
+    - Use paragraphs and sentences.
+    - Provide a neutral tone response, and a generalist answer, avoid specific.
+    - Keep the same lexical field as in the similar tickets agent responses.
+    - Always add the most relevant informations to the response, just like in similar tickets response so the user have all the informations needed.
+    - Stay as close as similar response as possible.
+
+
     ------ Zendesk Similar Tickets ------
     {similar_tickets}
     -------------------------------------
